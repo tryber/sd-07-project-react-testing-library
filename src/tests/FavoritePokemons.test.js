43f8e5b0e2dc8e802d82e94c008bd65fffcff9b8
cezar88augusto <cezar88augusto@gmail.com>
2020-12-04 18:57:13 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import data from '../data';
+
+describe('Requisito 3', () => {
+  test('renderiza nenhum pokemon favoritado', () => {
+    const { getByText, queryByText } = renderWithRouter(<FavoritePokemons />);
+    const text = getByText('No favorite pokemon found');
+    const query = queryByText('pokemon-name');
+    expect(query).not.toBeInTheDocument();
+    expect(text).toBeInTheDocument();
+  });
+  test('renderiza os pokemons favoritados', () => {
+    const favoritesPokemons = data;
+    const { getAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ favoritesPokemons } />,
+    );
+    const favoritesPokemonsQuantity = getAllByTestId('pokemon-name').length;
+    expect(favoritesPokemons.length).toBe(favoritesPokemonsQuantity);
+  });
+});
