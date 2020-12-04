9f211aa3b4a0b90f2b7cdfce9071290d7b63e5a7
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-12-04 16:00:43 -0300

    primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRoute from '../components/renderWithRoute';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('testando o componente App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testando se o caminho / é renderizado', () => {
+    const { getByText } = renderWithRoute(<App />);
+    const testandoHome = getByText('Home');
+    expect(testandoHome).toBeInTheDocument();
+    const testandoAbout = getByText('About');
+    expect(testandoAbout).toBeInTheDocument();
+    const testandoFavoritePokémons = getByText('Favorite Pokémons');
+    expect(testandoFavoritePokémons).toBeInTheDocument();
+  });
+  it('Testando redirecionamento ao clicar no home', () => {
+    const { getByText } = renderWithRoute(<App />);
+    fireEvent.click(getByText('Home'));
+    const testandoHome = getByText('Home');
+    expect(testandoHome).toBeInTheDocument();
+    const testandoAbout = getByText('About');
+    expect(testandoAbout).toBeInTheDocument();
+    const testandoFavoritePokémons = getByText('Favorite Pokémons');
+    expect(testandoFavoritePokémons).toBeInTheDocument();
+  });
+  it('Testando redirecionamento ao clicar no about', () => {
+    const { getByText } = renderWithRoute(<App />);
+    fireEvent.click(getByText('About'));
+    const testandoAbout = getByText('About Pokédex');
+    expect(testandoAbout).toBeInTheDocument();
+  });
+  it('Testando redirecionamento ao clicar no Favorit Pokemons', () => {
+    const { getByText } = renderWithRoute(<App />);
+    fireEvent.click(getByText('Favorite Pokémons'));
+    const testandoAbout = getByText('Favorite pokémons');
+    expect(testandoAbout).toBeInTheDocument();
+  });
+  it('Testando redirecionamento para página não encontrada', () => {
+    const { getByAltText, history } = renderWithRoute(<App />);
+    history.push('/xablau');
+    const testandoNotFound = getByAltText('Pikachu crying because the page requested was not found');
+    expect(testandoNotFound).toBeInTheDocument();
+  });
 });
