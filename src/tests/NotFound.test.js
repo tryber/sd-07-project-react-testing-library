95b703f11bd6ed32a68464422bdfb05f897977ea
alvaro <alvarovascon2@gmail.com>
2020-12-04 16:36:01 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+
+test('if notFound text is present', () => {
+  const { getByText } = render(<NotFound />);
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+});
+
+test('if notFound text is present', () => {
+  const { getAllByRole } = render(<NotFound />);
+  expect(getAllByRole('img')[1]).toHaveAttribute(
+    'src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'
+  );
+});
