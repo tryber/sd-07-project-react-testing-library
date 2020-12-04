9285a930894cc1ab568a445a85135adb6b3ecaa0
Carlos Souza <carlos.pessoal@hotmail.com>
2020-12-04 15:50:42 -0300

    Segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('renders the About screen', () => {
+  it('renders the pokedex info', () => {
+    render(<About />, { wrapper: MemoryRouter });
+
+    expect(screen.getByText('About Pokédex').tagName.toLowerCase()).toBe('h2');
+    expect(screen.getByText(/This application simulates a Pokédex/i)).toBeInTheDocument();
+    expect(screen.getByText(/One can filter Pokémons by type, and/i)).toBeInTheDocument();
+
+    const srcImage = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(screen.getByAltText('Pokédex').src).toBe(srcImage);
+  });
+});
