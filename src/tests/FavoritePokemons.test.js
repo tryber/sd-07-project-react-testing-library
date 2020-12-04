7f542b626ee8325ff70c0c383119aa993466c014
Strongreen <strongreen@live.com>
2020-12-04 13:07:44 -0300

    Iniciando o projeto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRender from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o arquivo FavoritePokemons', () => {
+
+    it('Testando o arquivo FavoritePokemons', () => {
+  
+      const { getByText } = renderWithRender(<App />);
+    
+      const favoritepoke = getByText(/Favorite pokémons/i);
+      expect(favoritepoke).toBeInTheDocument();
+  
+    });
+});
