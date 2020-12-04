e0a408a433cdc6d073fd467d4bb0749d540edb28
Raphael Caputo <faelelf2@gmail.com>
2020-12-04 20:43:48 -0300

    Requisito 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -13,7 +14,7 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
+it('mostra o caminho da pokedex é `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={ ['/'] }>
       <App />
@@ -21,4 +22,40 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('Testa caminho, home, about e Pokemon favorito', () => {
+  it('testar se o link Home encaminha para `/home`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText(/Home/i)).toBeInTheDocument();
+    expect(getByText(/Home/i).href).toEqual('http://localhost/');
+  });
+  it('testar se o link About encaminha para `/About`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText(/About/i)).toBeInTheDocument();
+    expect(getByText(/About/i).href).toBe('http://localhost/about');
+  });
+  it('testar se o link Favorite Pokémon encaminha para `/favorites`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+    expect(getByText(/Favorite Pokémons/i).href).toBe('http://localhost/favorites');
+  });
+  it('Testar se o link é redirecionado para `not found`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const route = 'http://localhost/xablau';
+    history.push(route);
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
 });
