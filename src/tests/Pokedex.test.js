0f18c6ee25606f76ba5b1488ab61e15e321b330f
Strongreen <strongreen@live.com>
2020-12-04 17:19:27 -0300

    requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -12,45 +12,27 @@ describe('Testando o arquivo Pokedex', () => {
 
   it('Informações Botoes Pokedex', () => {
     const { getByText, getByTestId } = renderWithRender(<App />);
-
     const buttonNext = getByText(/Próximo pokémon/i);
-
     fireEvent.click(buttonNext);
-
     const PokedexNextChamp = getByTestId('pokemon-name');
-
     expect(PokedexNextChamp.innerHTML).toBe('Charmander');
-
     fireEvent.click(buttonNext);
-
     const PokedexNextChamp2 = getByText(/Caterpie/i);
-
     expect(PokedexNextChamp2).toBeInTheDocument();
   });
 
   it('Informações Filter Pokedex', () => {
     const { getByText, getAllByTestId } = renderWithRender(<App />);
-
     const buttonPoke = getAllByTestId('pokemon-type-button');
-
     buttonPoke.map((buton) => expect(buton).toBeInTheDocument());
-
     fireEvent.click(getByText(/Psychic/i));
-
     const PokedexFilter = getByText(/Alakazam/i);
-
     expect(PokedexFilter).toBeInTheDocument();
-
     fireEvent.click(getByText(/Bug/i));
-
     const PokedexFilter2 = getByText(/Caterpie/i);
-
     expect(PokedexFilter2).toBeInTheDocument();
-
     fireEvent.click(getByText(/All/i));
-
     const PokedexFilter3 = getByText(/Pikachu/i);
-
     expect(PokedexFilter3).toBeInTheDocument();
   });
 });
