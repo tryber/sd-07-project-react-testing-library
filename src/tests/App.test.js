715e546ffa354484cdb97f59cc79ea044fa91180
Pedro Marques <pedromarques1011@gmail.com>
2020-12-04 15:03:43 -0300

    app.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -13,12 +14,34 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+describe('1. App.js file', () => {
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('render initial page `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const title = getByText(/Pokédex/i);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('test if there are a set of navigation links ', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    const about = getByText(/About/i);
+    const fav = getByText(/Favorite Pokémons/i);
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(fav).toBeInTheDocument();
+  });
 });
