73658cd6ec4751e9b44228190f164f85ea10a637
mbjunqueiraweb <mbjunqueira@gmail.com>
2020-12-04 15:44:11 -0300

    tests for pokedex

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('tests of NotFound component', () => {
+  it('render a heading `h2` with de text `Page requested not found`', () => {
+    const { getByRole, getByText } = render(<NotFound />);
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2).toBeInTheDocument();
+    const text = getByText(/Page requested not found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('render a image `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`', () => {
+    const { getByAltText } = render(<NotFound />);
+    const image = getByAltText(/Pikachu/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
