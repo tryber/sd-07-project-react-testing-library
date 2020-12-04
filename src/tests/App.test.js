144035753f613e7870d96d06726342c38e901f4a
moraisgabri <gabriel8azevedo@gmail.com>
2020-12-04 19:44:50 -0300

    concertando issues agora vai

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,9 +3,18 @@ import { cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
+afterEach(cleanup);
+
+test('Verificando se ao entrar na página nos encontramos na home', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const pageText = getByText(/about/i);
+  const pathName = history.location.pathname;
+  expect(pageText).toBeInTheDocument();
+  expect(pathName).toBe('/');
+});
+
 describe('Verificando se os links redirecionam corretamente.', () => {
-  afterEach(cleanup);
-  test('Testando o link Home', () => {
+  it('Testando o link Home', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const linkText = getByText(/home/i);
     fireEvent.click(linkText);
@@ -14,7 +23,7 @@ describe('Verificando se os links redirecionam corretamente.', () => {
     expect(pathName).toBe('/');
   });
 
-  test('Verficando o About', () => {
+  it('Verficando o About', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const linkText = getByText(/About/i);
     fireEvent.click(linkText);
@@ -23,12 +32,19 @@ describe('Verificando se os links redirecionam corretamente.', () => {
     expect(pathName).toBe('/about');
   });
 
-  test('Testando Favorite.', () => {
+  it('Testando Favorite.', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const linkText = getByText(/Favorite Pokémons/i);
+    const linkText = getByText(/Favorite pokémons/i);
     fireEvent.click(linkText);
-    const textInPage = (/Favorite pokémons/i);
-    expect((textInPage).toBeInTheDocument());
-    expect(history.location.pathname).toBe('/favorites');
+    const pathName = history.location.pathname;
+    expect(linkText).toBeInTheDocument();
+    expect(pathName).toBe('/favorites');
   });
+});
+
+test('Deverá ir para "Page Not Found" quando tentar acessar um link inválido', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/teste');
+  const pageText = getByText(/page requested not found/i);
+  expect(pageText).toBeInTheDocument();
 });
