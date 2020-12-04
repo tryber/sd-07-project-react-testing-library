d301363136becd8671ddd546b7f95bd1ef19c274
luc-zago <luc.zago@hotmail.com>
2020-12-04 18:34:39 -0300

    Finalizados 3 primeiros testes

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('check if `About` component is working correctly', () => {
+  it('check if `About` has text `About Pokédex`', () => {
+    const { getByText } = render(<About />);
+    const aboutPokedex = getByText(/About Pokédex/);
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  it('check if `About Pokédex` is a `h2`', () => {
+    const { getByText } = render(<About />);
+    const aboutPokedex = getByText(/About Pokédex/);
+    expect(aboutPokedex.tagName).toBe('H2');
+  });
+
+  it('check if `About Pokédex` has two `p` elements explaining the pokedéx', () => {
+    const { getAllByText } = render(<About />);
+    const paragraphs = getAllByText(/Pokémons/i);
+    expect(paragraphs).toHaveLength(2);
+    expect(paragraphs[0].tagName).toBe('P');
+    expect(paragraphs[1].tagName).toBe('P');
+  });
+
+  it('check pokédex image link', () => {
+    const { getByAltText } = render(<About />);
+    const image = getByAltText(/Pokédex/);
+    const imageLink = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    expect(image.src).toBe(imageLink);
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('', () => {
+  it('check if the text `No favorite pokémon found` if no pokémon was favored', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/favorites');
+    const favoritePokemons = getByText(/No favorite pokemon found/);
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  it('check if no pokémon is shown in `Favorite Pokémons` if no pokémon was favored', () => {
+    const { getByText, queryAllByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/favorites');
+    const noFavoritePokemons = queryAllByText(/Average/i);
+    expect(noFavoritePokemons).toHaveLength(0);
+  });
+
+  it('check if favored pokémons are shown in `Favorite Pokémons`', () => {
+    const { getByText, getByRole, queryAllByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    fireEvent.click(getByText(/More details/i));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByText(/Home/i));
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    fireEvent.click(getByText(/More details/i));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/favorites');
+    const firstFavoritePokemon = getByText(/Caterpie/);
+    expect(firstFavoritePokemon).toBeInTheDocument();
+    const secondFavoritePokemon = getByText(/Charmander/);
+    expect(secondFavoritePokemon).toBeInTheDocument();
+  });
+});
