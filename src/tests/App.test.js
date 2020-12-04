3e484b5a015e8ff373042e53a28b4f99cd27eed3
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-12-04 17:55:05 -0300

    issues

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import renderWithRouter from './renderWithRouter';
 import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('Application when first rendered', () => {
@@ -9,14 +9,12 @@ describe('Application when first rendered', () => {
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
-
   test('renders home page when URL is "/"', () => {
     const { history } = renderWithRouter(<App />);
     const { pathname } = history.location;
     expect(pathname).toBe('/');
-  })
-})
-
+  });
+});
 describe('Navigation Bar', () => {
   test('renders working link to "Home" page', () => {
     const { history, getByText } = renderWithRouter(<App />);
@@ -27,8 +25,7 @@ describe('Navigation Bar', () => {
     userEvent.click(home);
     expect(history.location.pathname).toBe('/');
     expect(getByText(/Encountered pokémons/i)).toBeInTheDocument();
-  })
-
+  });
   test('renders working link to "About" page', () => {
     const { history, getByText } = renderWithRouter(<App />);
     const about = getByText(/About/i);
@@ -37,8 +34,7 @@ describe('Navigation Bar', () => {
     userEvent.click(about);
     expect(history.location.pathname).toBe('/about');
     expect(getByText(/About Pokédex/i)).toBeInTheDocument();
-  })
-
+  });
   test('renders working link to "Favorites" page', () => {
     const { history, getByText } = renderWithRouter(<App />);
     const favorites = getByText(/Favorite Pokémons/i);
@@ -46,13 +42,12 @@ describe('Navigation Bar', () => {
     expect(favorites).toBeInTheDocument();
     userEvent.click(favorites);
     expect(history.location.pathname).toBe('/favorites');
-  })
-  
+  });
   test('renders "NotFound" page when redirected to unknown URL', () => {
     const { history, getByText } = renderWithRouter(<App />);
     expect(history.location.pathname).toBe('/');
     history.push('/xablau');
     expect(history.location.pathname).toBe('/xablau');
-    expect(getByText(/Page requested not found/i)).toBeInTheDocument;
-  })
-})
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+});
