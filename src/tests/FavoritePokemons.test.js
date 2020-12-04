adb0c02958bc0625fd406f3f810cf06746392565
Andre Serudo <andreserudo@gmail.com>
2020-12-04 17:05:52 -0400

    requisito 03 - arquivo FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../components/FavoritePokemons';
+
+it('should show a message when the user dont have favorite pokemons ', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemon />);
+
+  const heading = getByText(/No favorite pokemon found/);
+  expect(heading).toBeInTheDocument();
+});
+
+it('should show a message when the user dont have favorite pokemons ', () => {
+  const pokemon = [{
+    averageWeight: 2,
+    id: '1x021',
+    image: 'http://',
+    name: 'pikachu',
+    type: 'lightning',
+  }];
+  const { getByText } = renderWithRouter(<FavoritePokemon pokemon={ pokemon } />);
+
+  const heading = getByText(/No favorite pokemon found/);
+  expect(heading).toBeInTheDocument();
+});
