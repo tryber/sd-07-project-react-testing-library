a9c2d619a2f289a5cffecba1ca84ca65f8778a6f
Bruno <bruno_grb@hotmail.com>
2020-12-04 18:43:40 -0300

    implementacao do test 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('The message No favorite pokemon found is displayed on the screen', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const text = getByText(/No favorite pokemon found/i);
+  expect(text).toBeInTheDocument();
+});
