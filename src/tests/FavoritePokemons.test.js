326a0faabe4909cb734ca9e0abe29c1741d91d65
Vitor Rodrigues <vitor_rc@outlook.com>
2020-12-04 20:40:23 -0300

    Inicio do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+test("is displayed on the screen 'No favorite pokemon'", () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const favoritePokemonsButton = getByText('Favorite Pokémons');
+  fireEvent.click(favoritePokemonsButton);
+
+  const noFavoriteText = getByText('No favorite pokemon found');
+  expect(noFavoriteText).toBeInTheDocument();
+});
