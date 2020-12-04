94a719604ce965b0651ecdfd71fdac3fb74aec95
Eduardo Souza <dado.eletrons@gmail.com>
2020-12-04 14:24:19 -0300

    fix5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('1. Testing the App.js file', () => {
+  it('The main Pokédex page is rendered', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('The top of the application contains a fixed set of links', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+
+    const about = getByText(/About/i);
+    expect(about).toBeInTheDocument();
+
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+
+    const favorite = getByText(/Favorite Pokémons/i);
+    expect(favorite).toBeInTheDocument();
+
+    fireEvent.click(favorite);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('The application is redirected to the home page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('The application is redirected to the about page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText(/About/i);
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('The application is redirected to the favorite Pokémon page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favorite = getByText(/Favorite Pokémons/i);
+    fireEvent.click(favorite);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('The application is redirected to the not found page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/notfound');
+    expect(history.location.pathname).toBe('/notfound');
+
+    const notFound = getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,82 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import PokemonDetails from '../components/PokemonDetails';
+import pokemons from './helpers/pokemonData';
+import favorite from './helpers/pokemonFavorite';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('7. Testing the PokemonDetails.js file', () => {
+  it('Detailed Pokemon information is shown', () => {
+    const {
+      getByRole,
+      queryByRole,
+      getByText,
+      history,
+    } = renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ favorite }
+      pokemons={ pokemons }
+      match={ { params: { id: '4' } } }
+      onUpdateFavoritePokemons={ () => {} }
+    />);
+
+    history.push('/pokemons/4');
+    const pokeTitle = getByText(/Charmander Details/i);
+    expect(pokeTitle).toBeInTheDocument();
+
+    const linkToDetails = queryByRole('link', { href: '/pokemons/4' });
+    expect(linkToDetails).toBeNull();
+
+    const heading = getByRole('heading', { name: 'Summary' });
+    expect(heading).toBeInTheDocument();
+
+    const summary = getByText(/The flame on its tail shows/i);
+    expect(summary).toBeInTheDocument();
+  });
+
+  it('Section with maps and locations', () => {
+    const {
+      getAllByRole,
+      getByText,
+    } = renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ favorite }
+      pokemons={ pokemons }
+      match={ { params: { id: '4' } } }
+      onUpdateFavoritePokemons={ () => {} }
+    />);
+    const heading = getByText(/Game Locations of Charmander/i);
+    expect(heading).toBeInTheDocument();
+
+    const pokeLocations = getAllByRole('img', { name: 'Charmander location' });
+    expect(pokeLocations.length.toString()).toBe('4');
+
+    pokeLocations.forEach((poke, index) => {
+      expect(poke.nextElementSibling)
+        .toHaveTextContent(pokemons[0].foundAt[index].location);
+      expect(poke).toHaveAttribute('alt', 'Charmander location');
+      expect(poke).toHaveAttribute(
+        'src',
+        pokemons[0].foundAt[index].map,
+      );
+    });
+  });
+
+  it('User can favorite a pokemon', () => {
+    const {
+      getByLabelText,
+    } = renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ favorite }
+      pokemons={ pokemons }
+      match={ { params: { id: '4' } } }
+      onUpdateFavoritePokemons={ () => {} }
+    />);
+    const checkbox = getByLabelText('Pokémon favoritado?');
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox.checked).toBeTruthy();
+
+    fireEvent.change(checkbox, { target: { checked: false } });
+    expect(checkbox.checked).toBeFalsy();
+
+    fireEvent.change(checkbox, { target: { checked: true } });
+    expect(checkbox.checked).toBeTruthy();
+  });
+});
