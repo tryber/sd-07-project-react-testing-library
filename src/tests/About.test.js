3cc52641f2ded5360f442e9a852c6380ea80ca21
ericvini <ericcobain3@gmail.com>
2020-12-04 15:30:41 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+describe('Testando o arquivo About.js', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByText } = render(<About />);
+    const h2 = getByText('About Pokédex');
+    expect(h2).toBeInTheDocument();
+  });
+});
