f296f38f74c67e9d56231d4741e69d31d9ada725
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-12-04 20:25:10 -0300

    Ultimas aleterações no Pokedex.test.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,4 +1,4 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Pokedex from '../components/Pokedex';
 import renderWithRouter from '../helpers/renderWithRouter';
@@ -7,14 +7,65 @@ import pokemons from '../data';
 afterEach(cleanup);
 
 describe('Pokedex - teste de Conteúdo', () => {
+
+  const isPokemonFavoriteById = {
+    4: false,
+    10: false,
+    23: false,
+    25: false,
+    65: false,
+    78: false,
+    143: false,
+    148: false,
+    151: true,
+  };
+
+  const typesOfPokemons = 7;
+
   it('Deve renderizar um h2 com o texto "Encountered pokémons"', () => {
     const { getByRole, getByText } = renderWithRouter(<Pokedex
       pokemons={ pokemons }
-      isPokemonFavoriteById={ {} }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
     />);
     const h2Element = getByRole('heading', { level: 2 });
     const h2Content = getByText('Encountered pokémons');
     expect(h2Element).toBeInTheDocument();
     expect(h2Content).toBeInTheDocument();
+  });
+
+  it('Deve renderizar o próximo pokemon no clique do botão', () => {
+    const { getByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+
+    let actualPokemon = getByTestId('pokemon-name').innerHTML;
+    expect(actualPokemon).toBe('Pikachu');
+
+    const nextBtn = getByTestId('next-pokemon');
+    expect(nextBtn.innerHTML).toBe('Próximo pokémon');
+    fireEvent.click(nextBtn);
+
+    actualPokemon = getByTestId('pokemon-name').innerHTML;
+    expect(actualPokemon).not.toBe(pokemons[0].name);
+    expect(actualPokemon).toBe(pokemons[1].name);
+  });
+
+  it('Deve mostrar um pokemon por vez', () => {
+    const { getAllByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+    const pokemonsList = getAllByTestId('pokemon-name');
+    expect(pokemonsList.length).toBe(1);
+  });
+
+  it('Deve haver 7 botões de tipo', () => {
+    const { getAllByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+    const pokemonsType = getAllByTestId('pokemon-type-button');
+    expect(pokemonsType.length).toBe(typesOfPokemons);
   });
 });
