d1823db9ed9c4e955e27d6a2205d599558e9ce42
Lfocosi <luccafocosi@gmail.com>
2020-12-04 17:43:59 -0300

    requisito 2-1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -13,21 +13,23 @@ describe('links', () => {
   it('test paragrafo 1', () => {
     const { getByText, history } = renderWithRouter(<App />);
     history.push('/about');
-    const about = getByText(/This application simulates/i);
+    const about = getByText(/This application simulates a Pokédex, a digital/i);
     expect(about).toBeInTheDocument();
   });
 
-  it('test paragrafo 1', () => {
+  it('test paragrafo 2', () => {
     const { getByText, history } = renderWithRouter(<App />);
     history.push('/about');
-    const about = getByText(/One can filter Pokémons/i);
+    const about = getByText(/One can filter Pokémons by type, and see more details/i);
     expect(about).toBeInTheDocument();
   });
 
   it('test img', () => {
     const { getByAltText, history } = renderWithRouter(<App />);
+    const srcimagem = 'https://cdn.bulbagarden.net/upload/thumb/8/86/'
+    + 'Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     history.push('/about');
     const img = getByAltText('Pokédex');
-    expect(img).toBeInTheDocument();
+    expect(img.src).toEqual(srcimagem);
   });
 });
