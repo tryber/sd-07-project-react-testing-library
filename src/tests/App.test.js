1506d3640a36120dadf000f1ac686f28789f4724
moacyrrln <moacyrrln@gmail.com>
2020-12-04 17:05:07 -0300

    requisito1.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -22,3 +22,25 @@ test('shows the Pokédex when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('1) Teste se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Homee/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
