ee7f053e7844cd3b7e85a30b045a95d29a795586
Luise Rios <luisemiranda@gmail.com>
2020-12-04 16:08:11 -0300

    testando App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -15,10 +16,60 @@ test('renders a reading with the text `Pokédex`', () => {
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('Testa o conjunto fixo de links de navegação', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    const { getByText } = render(<App />);
+    const linkHome = getByText(/Home/i);
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto About', () => {
+    const { getByText } = render(<App />);
+    const linkAbout = getByText(/About/i);
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getByText } = render(<App />);
+    const linkFavorite = getByText(/Favorite Pokémons/i);
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
+
+describe('Testa os links da barra de navegação.', () => {
+  it('Testa se o link Home redireciona para /', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa se o link About redireciona para /about', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testa se o link Favorite Pokémons redireciona para /favorites', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
+
+test('deve testar um caminho não existente e a renderização do Not Found', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/pagina/que-nao-existe/');
+  const noMatch = getByText(/not found/i);
+  expect(noMatch).toBeInTheDocument();
 });
