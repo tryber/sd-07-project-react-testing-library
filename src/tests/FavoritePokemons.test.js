2a188941479da63037213e262388f08d49b22c7d
Carlos Souza <carlos.pessoal@hotmail.com>
2020-12-04 16:16:28 -0300

    Terceiro requisito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import mockedData from './mockedFavoritesPokemons';
+
+describe('renders the FavoritePokemons screen', () => {
+  it('renders no favorites pokemons', () => {
+    render(<FavoritePokemons pokemons={ [] } />, { wrapper: MemoryRouter });
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+    expect(screen.queryByText('pokemon-name')).not.toBeInTheDocument();
+  });
+
+  it('renders 2 favorites pokemons', () => {
+    const { pokemons } = mockedData;
+    render(
+      <FavoritePokemons pokemons={ pokemons } />,
+      { wrapper: MemoryRouter },
+    );
+
+    expect(screen.getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+  });
+});
