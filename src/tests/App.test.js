d2695269b0f7edf821030f4e5376bbb710a44f79
Felipe Nascimento <flpnascto@gmail.com>
2020-12-04 15:59:15 -0300

    Test dos links de navegação.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,20 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('testa conjunto de links de navegação', () => {
+  const { getAllByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const homeLink = getAllByRole('link')[0];
+  expect(homeLink).toHaveTextContent(/Home/i);
+
+  const aboutLink = getAllByRole('link')[1];
+  expect(aboutLink).toHaveTextContent(/About/i);
+
+  const favouriteLink = getAllByRole('link')[2];
+  expect(favouriteLink).toHaveTextContent(/Favorite Pokémons/i);
+});
