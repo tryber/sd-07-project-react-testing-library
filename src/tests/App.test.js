63c9631a33bf11f11c25aa9c36fc892e67d84aa3
Lfocosi <luccafocosi@gmail.com>
2020-12-04 14:38:39 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+// oi
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
