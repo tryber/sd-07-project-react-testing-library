201c47d5f96c7489550d1e85c2cf24c47cf9583a
ericvini <ericcobain3@gmail.com>
2020-12-04 17:37:14 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import About from '../components/About';
+import renderWithRouter from '../helpers/renderWithRouter';
 
 afterEach(cleanup);
 
-describe('Testando o arquivo About.js', () => {
-  test('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
-    const { getByText } = render(<About />);
-    const h2 = getByText('About Pokédex');
-    expect(h2).toBeInTheDocument();
+describe('Testing the About.js file', () => {
+  it('Tests whether the page contains the following image of a Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+
+    expect(image).toHaveAttribute(
+      'src',
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+  it('Test if the page contains two paragraphs with text about Pokédex.', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const text = getAllByText(/Pokémons/i);
+    expect(text.length.toString()).toBe('2');
+  });
+  it('The page contains information about Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const text = getByText('About Pokédex');
+    expect(text).toBeInTheDocument();
   });
 });
