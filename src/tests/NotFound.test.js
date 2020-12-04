fc8d3cc9ac61e1f5acd2aa82a381887da731c675
cezar88augusto <cezar88augusto@gmail.com>
2020-12-04 17:54:17 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Requisito 4', () => {
+    test('verifica se há um h2 com o texto especificado', () => {
+        const { getByText } = renderWithRouter(<NotFound />);
+        const text = getByText('Page requested not found');
+        expect(text).toBeInTheDocument();
+        expect(text.tagName.toLowerCase()).toBe('h2');
+    });
+    test('testa se a página mostra uma imagem específica', () => {
+        const { getByAltText } = renderWithRouter(<NotFound />);
+        const image = getByAltText('Pikachu crying because the page requested was not found');
+        expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
