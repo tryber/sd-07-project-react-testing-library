61c3fcfda117f50fefc68b02a683c5cc04702bc3
Andre Serudo <andreserudo@gmail.com>
2020-12-04 17:13:12 -0400

    requisito 04 - arquivo NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+it('should contain a h2 heading with the text: Page requested not found ', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+
+  const heading = getByText(/Page requested not found/);
+  expect(heading).toBeInTheDocument();
+});
+
+it('should contain an image representing a missing link', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const srcImgToFind = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const image = getAllByRole('img');
+
+  expect(image[1].src).toBe(srcImgToFind);
+});
