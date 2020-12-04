310fdeac0d5e92150995f52799329f40a62265d9
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-12-04 18:10:56 -0300

    Adicionando exercício 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando página not  found', () => {
+  it('Testando se a página contém texto Page requested not found ', () => {
+    const { getByText } = render(<NotFound />);
+    const notFound = getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+  it('Testando se a imagem aparece', () => {
+    render(<NotFound />);
+    const img = document.querySelector('img');
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
