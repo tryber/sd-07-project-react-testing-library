fc896335f111ec740a20b85f64e7520aef0ba407
cezar88augusto <cezar88augusto@gmail.com>
2020-12-04 17:55:03 -0300

    Requisito 4 Fix

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import renderWithRouter from '../renderWithRouter';
-import FavoritePokemons from '../components/FavoritePokemons';
-
-describe("Requisito 3", () => {
-    test('renderiza a msg No favorite pokemon found', () => {
-        
-     });
-});
-
-
+test('', () => {});
