ff5194d9b1899b0e197248ad411a8fb77004930a
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-12-04 17:49:39 -0300

    about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
-import App from '../App';
-import renderWithRouter from '../renderWithRouter';
+import { render, cleanup } from '@testing-library/react';
+import About from '../components/About';
 
 afterEach(cleanup);
 
-describe('Teste da tela App', () => {
-    it('',()=>{
-    const { getByText, history } = renderWithRouter(<App />);
-    })
+describe('Teste da tela About', () => {
+  it('test that there is a rendered h2 title', () => {
+    const { getByRole } = render(<About />);
+
+    const title = getByRole('heading');
+
+    expect(title).toBeInTheDocument();
+    expect(title.tagName).toBe('H2');
+    expect(title.textContent).toBe('About Pokédex');
+  });
+
+  it('tests that it has two paragraphs with information', () => {
+    const { container } = render(<About />);
+    const p = container.querySelectorAll('p');
+    const quantidadeP = 2;
+
+    expect(p.length).toBe(quantidadeP);
+    expect(p[0].textContent).toBe(
+      'This application simulates a Pokédex, a digital encliclopedia containing all Pokémons'
+    );
+    expect(p[1].textContent).toBe(
+      'One can filter Pokémons by type, and see more details for each one of them'
+    );
+  });
+  it('test that there is a rendered an image of Pokedex', () => {
+    const { getByRole } = render(<About />);
+
+    const image = getByRole('img');
+
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -8,41 +8,38 @@ afterEach(cleanup);
 describe('Teste da tela App', () => {
   it('renders a reading with the text `Pokédex`', () => {
     const { getByText, history } = renderWithRouter(<App />);
-  
+
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
-  
-    const {pathname} = history.location;
-    expect(pathname).toBe('/')
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
 
-
-  it('renders the Links: Home, About, Favorite Pokémon.And these links redirect the respective pages', () => {
+  it('renders Home, About, Favorite Pokémon.And it redirect the respective pages', () => {
     const { getByText, history } = renderWithRouter(<App />);
 
-      const home = getByText(/Home/i);
-      const about = getByText(/About/i);
-      const favoritePokemons = getByText(/Favorite Pokémons/i);
+    const home = getByText(/Home/i);
+    const about = getByText(/About/i);
+    const favoritePokemons = getByText(/Favorite Pokémons/i);
 
-      expect(home).toBeInTheDocument();
-      expect(about).toBeInTheDocument();
-      expect(favoritePokemons).toBeInTheDocument();
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
 
-      fireEvent.click(about);
-      expect(history.location.pathname).toBe('/about');
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
 
-      fireEvent.click(favoritePokemons);
-      expect(history.location.pathname).toBe('/favorites');
+    fireEvent.click(favoritePokemons);
+    expect(history.location.pathname).toBe('/favorites');
 
-      fireEvent.click(home);
-      expect(history.location.pathname).toBe('/');
-  })
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
   it('renders Not Found page', () => {
     const { getByText, history } = renderWithRouter(<App />);
 
     history.push('teste');
-    expect(getByText('Page requested not found')).toBeInTheDocument()
-  })
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
 });
-
-
