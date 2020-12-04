e45020f5512ae41e40a6d346787fa2a2828223d8
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-12-04 15:53:43 -0300

    testes do app.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+afterEach(cleanup);
+
+describe('Teste da tela App', () => {
+    it('',()=>{
+    const { getByText, history } = renderWithRouter(<App />);
+    })
+});
