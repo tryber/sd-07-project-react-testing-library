4918838e440c37b48654b05c941bc144da9f979a
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-04 17:10:47 -0300

    ajusting requirement 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -25,46 +25,29 @@ describe('testing the file App.js', () => {
     expect(getByText('Encountered pokémons')).toBeInTheDocument();
   });
 
-  test('the first link has the text "Home" and redirects to the initial page with the "/" in the URL path', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-
+  test('the link "Home" exists redirecting to the initial page on URL "/"', () => {
+    const { getByText } = renderWithRouter(<App />);
     const homeLink = getByText(/\bHome\b/);
     expect(homeLink).toBeInTheDocument();
-
     fireEvent.click(getByText(/\bHome\b/));
     const homePage = getByText(/\bEncountered pokémons\b/);
     expect(homePage).toBeInTheDocument();
   });
 
-  test('the second link has the text "About" and redirects to the about page with the "/about" in the URL path', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-
+  test('the link "About" exists redirecting to the initial page on URL "/about"', () => {
+    const { getByText } = renderWithRouter(<App />);
     const aboutLink = getByText(/\bAbout\b/i);
     expect(aboutLink).toBeInTheDocument();
-
     fireEvent.click(getByText(/\bAbout\b/));
     const aboutPage = getByText(/\bAbout Pokédex\b/);
     expect(aboutPage).toBeInTheDocument();
   });
 
-  test('the third link has the text "Favorite Pokémons" and redirects to the Pokémons Favoritados page with the "/favorites" in the URL path', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-
+  test(`the link "Favorite Pokémons" exists redirecting to the initial 
+  page on URL "/favorites"`, () => {
+    const { getByText } = renderWithRouter(<App />);
     const favoriteLink = getByText(/\bFavorite Pokémons\b/);
     expect(favoriteLink).toBeInTheDocument();
-
     fireEvent.click(getByText(/\bFavorite Pokémons\b/));
     const favoritePage = getByText(/\bFavorite pokémons\b/);
     expect(favoritePage).toBeInTheDocument();
@@ -72,7 +55,6 @@ describe('testing the file App.js', () => {
 
   test('the page is redirected to the "Not Found" page when used an unknown URL', () => {
     const { getByText, history } = renderWithRouter(<App />);
-
     history.push('/pagina/que-nao-existe/');
     const noMatch = getByText(/\bPage requested not found\b/);
     expect(noMatch).toBeInTheDocument();
