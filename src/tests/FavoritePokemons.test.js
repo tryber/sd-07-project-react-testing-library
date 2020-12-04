e0bac46d91e8ef882325a4e116c82eb75e57dfdd
Strongreen <strongreen@live.com>
2020-12-04 13:13:45 -0300

    correçao lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,13 +4,10 @@ import renderWithRender from '../renderWithRouter';
 import App from '../App';
 
 describe('Testando o arquivo FavoritePokemons', () => {
+  it('Testando o arquivo FavoritePokemons', () => {
+    const { getByText } = renderWithRender(<App />);
 
-    it('Testando o arquivo FavoritePokemons', () => {
-  
-      const { getByText } = renderWithRender(<App />);
-    
-      const favoritepoke = getByText(/Favorite pokémons/i);
-      expect(favoritepoke).toBeInTheDocument();
-  
-    });
+    const favoritepoke = getByText(/Favorite pokémons/i);
+    expect(favoritepoke).toBeInTheDocument();
+  });
 });
