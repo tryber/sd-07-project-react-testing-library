4ae2d8ac03a682dcf1155d35c9f90311bddaba88
Andre Serudo <andreserudo@gmail.com>
2020-12-04 16:49:05 -0400

    requisito 02 - arquivo About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+it('should contain a h2 heading with the text: About Pokédex ', () => {
+  const { getByText } = renderWithRouter(<About />);
+
+  const heading = getByText(/About Pokédex/);
+  expect(heading).toBeInTheDocument();
+});
+
+it('should contain an image of a pokédex', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const srcImgToFind = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const image = getByRole('img');
+
+  expect(image.src).toBe(srcImgToFind);
+});
