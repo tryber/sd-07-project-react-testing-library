ee1c6a03c44e43f9a0ac9a49f1012fc971d0a8a3
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-12-04 18:15:26 -0300

    not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+describe('Teste da tela NotFound', () => {
+  it('test that there is a rendered h2 title', () => {
+    const { getByRole } = render(<NotFound />);
+
+    const title = getByRole('heading');
+
+    expect(title).toBeInTheDocument();
+    expect(title.tagName).toBe('H2');
+    expect(title.textContent).toBe('Page requested not found 😭');
+  });
+  it('test that there is a rendered an image of Not Found page', () => {
+    const { getAllByRole } = render(<NotFound />);
+
+    const image = getAllByRole('img');
+
+    expect(image[1]).toBeInTheDocument();
+    expect(image[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
