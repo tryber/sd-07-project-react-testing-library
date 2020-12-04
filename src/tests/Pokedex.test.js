5102f5100e89316b25cd1b6cb8b85d107f868ebe
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-12-04 17:40:20 -0300

    Implementando primeiro teste do requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { cleanup } from '@testing-library/react';
+import React from 'react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from '../helpers/renderWithRouter';
+import pokemons from '../data';
+
+afterEach(cleanup);
+
+describe('Pokedex - teste de Conteúdo', () => {
+  it('Deve renderizar um h2 com o texto "Encountered pokémons"', () => {
+    const { getByRole, getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const h2Element = getByRole('heading', { level: 2 });
+    const h2Content = getByText('Encountered pokémons');
+    expect(h2Element).toBeInTheDocument();
+    expect(h2Content).toBeInTheDocument();
+  });
+});
