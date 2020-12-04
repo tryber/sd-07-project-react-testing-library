555d26b91979613bd6ca6b010233f430d6f26a2a
Pedro Marques <pedromarques1011@gmail.com>
2020-12-04 17:10:02 -0300

    feito notfound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+
+import { render } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+// import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('', () => {
+  const { container } = render(<NotFound />);
+  const img = container.querySelector(
+    '[src="https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"]',
+  );
+
+  expect(img).toBeInTheDocument();
+});
