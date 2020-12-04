cb29cf5a1b0c2a254ffbb4d7d3a80f437c05b1a7
Lfocosi <luccafocosi@gmail.com>
2020-12-04 16:47:28 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('links', () => {
+  it('test About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const about = getByText('About Pokédex');
+    expect(about).toBeInTheDocument();
+  });
+
+  it('test paragrafo 1', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const about = getByText(/This application simulates/i);
+    expect(about).toBeInTheDocument();
+  });
+
+  it('test paragrafo 1', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const about = getByText(/One can filter Pokémons/i);
+    expect(about).toBeInTheDocument();
+  });
+
+  it('test img', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const img = getByAltText('Pokédex');
+    expect(img).toBeInTheDocument();
+  });
+});
