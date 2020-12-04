5070fd09aed23415dd2f1320e8f8a7b6d5845442
Filipi Firmino <contatoic3web@gmail.com>
2020-12-04 19:43:46 -0300

    Initial commit :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,35 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('Check if you render the `Home` link on the header page', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkHome = getByText(/Home/i);
+  expect(linkHome).toBeInTheDocument();
+});
+
+test('Check if you render the `About` link on the header page', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkAbout = getByText(/About/i);
+  expect(linkAbout).toBeInTheDocument();
+});
+
+test('Check if you render the `Favorite Pokémon` link on the header page', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkFavoritePokémons = getByText(/Favorite Pokémons/i);
+  expect(linkFavoritePokémons).toBeInTheDocument();
+});
