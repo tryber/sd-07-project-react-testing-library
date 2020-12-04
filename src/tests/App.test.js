bf883088a505a71f4caa9190809d75588961edbe
moacyrrln <moacyrrln@gmail.com>
2020-12-04 17:45:45 -0300

    requisito 1 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent} from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../../src/renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -33,8 +34,10 @@ test('shows the Pokédex when the route is `/`', () => {
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
-describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
-  it('O primeiro link deve possuir o texto Home', () => {
+let message = `Teste se o topo da aplicação
+ contém um conjunto fixo de links de navegação.`;
+describe(message, () => {
+  it('O primeiro link deve possuir o texto Home, About e Favorite Pokémons/', () => {
     const { getByText } = render(
       <MemoryRouter>
         <App />
@@ -47,4 +50,39 @@ describe('Teste se o topo da aplicação contém um conjunto fixo de links de na
     const favorite = getByText(/Favorite Pokémons/i);
     expect(favorite).toBeInTheDocument();
   });
+});
+message = `Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar
+ no link Home da barra de navegação.`;
+it(message, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Home/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+message = `Teste se a aplicação é redirecionada para a página de About, na URL /about, ao
+ clicar no link About da barra de navegação.`;
+it(message, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/About/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+message = `Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na 
+URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.`;
+it(message, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+message = `Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma 
+URL desconhecida.`;
+it(message, () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/pagina-que-nao-existe/');
+  const noMatch = getByText(/Page requested not found/i);
+  expect(noMatch).toBeInTheDocument();
 });
