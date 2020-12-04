21492f4db7ae66f465de941b8059b5d717f9ca60
Bruno <bruno_grb@hotmail.com>
2020-12-04 15:53:23 -0300

    implmentando teste 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
+it('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -13,12 +14,63 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
+it('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+it('The first link must have the text "Home"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText(/Home/i)).toBeInTheDocument();
+});
+
+it('The first link must have the text "About"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText(/About/i)).toBeInTheDocument();
+});
+
+it('The first link must have the text "Favorite Pokémons"', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+});
+
+it('Test if the application is redirected to the home page', () => {
+  const history = createMemoryHistory();
+  const route = '/';
+  history.push(route);
+  render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  expect(screen.getByText(/Home/i)).toBeInTheDocument();
+});
+
+it('Test if the application is redirected to the About page', () => {
+  const history = createMemoryHistory();
+  const route = '/about';
+  history.push(route);
+  render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  expect(screen.getByText(/About Pokédex/i)).toBeInTheDocument();
 });
