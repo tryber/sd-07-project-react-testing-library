9dd0ddd3267800af9cb2940f56681b6aec1882eb
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-12-04 16:25:50 -0300

    Requisito 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('whether the page contains information about Pokédex.', () => {
+  renderWithRouter(<About />);
+  const textAbout = screen.getByText(/This application simulates a Pokédex/i);
+  const secondText = screen.getByText(/One can filter Pokémons by type/i);
+  expect(textAbout).toBeInTheDocument();
+  expect(secondText).toBeInTheDocument();
+});
+
+test('se a página contém um heading h2 com o texto About Pokédex', () => {
+  renderWithRouter(<About />);
+  const titleAbout = screen.getByText(/About Pokédex/i);
+  expect(titleAbout).toBeInTheDocument();
+});
+
+test('se a página contém a seguinte imagem de uma Pokédex', () => {
+  renderWithRouter(<About />);
+  const image = screen.getByAltText(/Pokédex/i);
+  expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
