d2f8ff9b4fc868d53f3fb0a685715b8362baa698
vitorAmorims <vtamorims@gmail.com>
2020-12-05 12:24:35 -0300

    correcoes sintaxe Pokedex.test.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -13,7 +13,7 @@ test('A página contém um heading h2 com o texto Encountered pokémons?', async
       + 'electricity to make them tender enough to eat.',
       type: 'Electric' }];
   const { container } = renderWithRouter(
-    <Pokedex pokemons={pokemon} isPokemonFavoriteById={25} />
+    <Pokedex pokemons={ pokemon } isPokemonFavoriteById={ 25 } />
   );
   await container.querySelector('h2');
   expect(container.querySelector('h2')).toHaveTextContent('Encountered pokémons');
@@ -21,54 +21,51 @@ test('A página contém um heading h2 com o texto Encountered pokémons?', async
 
 describe('Teste se é exibido o próximo Pokémon da lista quando'
 + 'o botão Próximo pokémon é clicado.', () => {
-    const pokemon = [
-      {
-        averageWeight: { value: '6.0', measurementUnit: 'kg' },
-        image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-        name: 'Pikachu',
-        summary: 'This intelligent Pokémon roasts hard berries with electricity'
-        + 'to make them tender enough to eat.',
-        type: 'Electric',
-      },
-      {
-        averageWeight: { value: '8.5', measurementUnit: 'kg' },
-        image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
-        name: 'Charmander',
-        summary: 'The flame on its tail shows the strength of its life force.'
-        + 'If it is weak, the flame also burns weakly.',
-        type: 'Fire',
-      },
-      {
-        averageWeight: { value: '48.0', measurementUnit: 'kg' },
-        image: 'https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
-        name: 'Alakazam',
-        summary: 'Closing both its eyes heightens all its other senses.'
-        + 'This enables it to use its abilities to their extremes.',
-        type: 'Psychic',
-      },
-    ];
-    test('O botão deve conter o texto Próximo pokémon', async () => {
-      const { getByText } = renderWithRouter(
-        <Pokedex pokemons={ pokemon } isPokemonFavoriteById={ 25 } />
-      );
-      await getByText('Próximo pokémon');
-      expect(getByText(/Próximo pokémon/i)).toBeInTheDocument();
-    });
-    test('Os próximos Pokémons da lista devem ser mostrados, um a um,'
-    + 'ao clicar sucessivamente no botão',
-      async () => {
-        const { getByText } = renderWithRouter(
-          <Pokedex pokemons={ pokemon } isPokemonFavoriteById={ 25 } />
-        );
-        await getByText('Próximo pokémon');
-        fireEvent.click(getByText(/Próximo pokémon/i));
-        await getByText('Charmander');
-        fireEvent.click(getByText(/Próximo pokémon/i));
-        await getByText('Alakazam');
-      }
+  const pokemon = [
+    {
+      averageWeight: { value: '6.0', measurementUnit: 'kg' },
+      image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      name: 'Pikachu',
+      summary: 'This intelligent Pokémon roasts hard berries with electricity'
+      + 'to make them tender enough to eat.',
+      type: 'Electric',
+    },
+    {
+      averageWeight: { value: '8.5', measurementUnit: 'kg' },
+      image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+      name: 'Charmander',
+      summary: 'The flame on its tail shows the strength of its life force.'
+      + 'If it is weak, the flame also burns weakly.',
+      type: 'Fire',
+    },
+    {
+      averageWeight: { value: '48.0', measurementUnit: 'kg' },
+      image: 'https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+      name: 'Alakazam',
+      summary: 'Closing both its eyes heightens all its other senses.'
+      + 'This enables it to use its abilities to their extremes.',
+      type: 'Psychic',
+    },
+  ];
+  test('O botão deve conter o texto Próximo pokémon', async () => {
+    const { getByText } = renderWithRouter(
+    <Pokedex pokemons={ pokemon } isPokemonFavoriteById={ 25 } />
     );
-  }
-);
+    await getByText('Próximo pokémon');
+    expect(getByText(/Próximo pokémon/i)).toBeInTheDocument();
+  });
+  test('Os próximos Pokémons da lista devem ser mostrados, um a um,'
+  + 'ao clicar sucessivamente no botão', async () => {
+    const { getByText } = renderWithRouter(
+        <Pokedex pokemons={ pokemon } isPokemonFavoriteById={ 25 } />
+    );
+    await getByText('Próximo pokémon');
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    await getByText('Charmander');
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    await getByText('Alakazam');
+  });
+});
 // Teste se é exibido o próximo Pokémon da lista quando o botão Próximo pokémon é clicado.
 
 // O botão deve conter o texto Próximo pokémon;
