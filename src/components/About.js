d5727330fd2797c40b50b4b08171482d05498e2a
danielmlduarte <danielduarte.tryber@gmail.com>
2020-12-05 01:11:50 -0300

    Requisito 2 - Testes do compomente About

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './about.css';
 
 const About = () => (
-  <section>
+  <section data-testid="about-section">
     <h2>{`About ` + `Pokédex`}</h2>
     <section>
-      <p>
+      <p data-testid="about-info">
         This application simulates a Pokédex, a
         digital encliclopedia containing all Pokémons
       </p>
-      <p>One can filter Pokémons by type, and see more details for each one of them</p>
+      <p data-testid="about-info">One can filter Pokémons by type, and see more details for each one of them</p>
       <img
         className="pokedex-image"
         src={`https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`}
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test if About component', () => {
+  test('renders contents about Pokédex', () => {
+    const { getByTestId } = renderWithRouter(<About />);
+    const aboutSection = getByTestId('about-section');
+    expect(aboutSection).toBeInTheDocument();
+  });
+
+  test('renders a info about Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const aboutTitle = getByText('About Pokédex');
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('renders two paragraphs with text about Pokédex', () => {
+    const { getAllByTestId } = renderWithRouter(<About />);
+    const aboutParagraphs = getAllByTestId('about-info');
+    const paragraphsInPage = 2;
+    expect(aboutParagraphs).toHaveLength(paragraphsInPage);
+  });
+
+  test('renders a Pokédex image', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const { src } = getByAltText('Pokédex');
+    const imageUrl = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(src).toBe(imageUrl);
+  });
+});
