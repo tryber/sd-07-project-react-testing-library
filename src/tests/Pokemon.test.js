845197936c88e54ae1d88a99f902299c33949a3c
Raphael Caputo <faelelf2@gmail.com>
2020-12-05 02:19:16 -0300

    Adicionando mais subRequisitos para o requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,5 +9,12 @@ describe('Testando o arquivo Pokemon.js', () => {
     renderWithRouter(<Pokemon pokemon={ pokemons[1] } isFavorite={ false } />);
     expect(screen.getByTestId('pokemon-weight'))
       .toHaveTextContent(/average weight: 8.5 kg/i);
+    const srcExpected = 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png';
+    expect(screen.getByRole('img', { name: /charmander sprite/i }).src).toBe(srcExpected);
   });
+  test('Teste se o card do Pokémon contém um link para exibir detalhes deste Pokémon',
+    () => {
+      renderWithRouter(<Pokemon pokemon={ pokemons[1] } isFavorite={ false } />);
+      expect(screen.getByText(/more details/i).href).toBe('http://localhost/pokemons/4');
+    });
 });
