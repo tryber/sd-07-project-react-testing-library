02d857a5614c8db7c62af2fc7ad4d1c93ebb7583
Pedro Marques <pedromarques1011@gmail.com>
2020-12-05 14:43:57 -0300

    corrigido NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,11 +4,18 @@ import { render } from '@testing-library/react';
 // import renderWithRouter from './renderWithRouter';
 import NotFound from '../components/NotFound';
 
-test('', () => {
-  const { container } = render(<NotFound />);
-  const img = container.querySelector(
-    '[src="https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"]',
-  );
+describe('NotFound.js tests', () => {
+  test('test if not found text is rendered', () => {
+    const { getByText } = render(<NotFound />);
 
-  expect(img).toBeInTheDocument();
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+  test('', () => {
+    const { container } = render(<NotFound />);
+    const img = container.querySelector(
+      '[src="https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"]',
+    );
+
+    expect(img).toBeInTheDocument();
+  });
 });
