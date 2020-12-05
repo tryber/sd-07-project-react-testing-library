3bdcb693d915eaa0548231c0849b1632e190a12a
vitorAmorims <vtamorims@gmail.com>
2020-12-05 11:31:07 -0300

    correcoes sintaxe FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -44,7 +44,7 @@ test('nenhum card de pokémon é exibido, se ele não estiver favoritado.', asyn
     <Router history={ history }>
       <FavoritePokemons pokemons={ pokemons } />
     </Router>
-    );
+  );
   await getByText('No favorite pokemon found');
   expect(getByText('No favorite pokemon found')).toBeInTheDocument();
 });
