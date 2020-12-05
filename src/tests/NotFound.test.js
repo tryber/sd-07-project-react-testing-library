f6ed91633fa277309da739c6c7a09a95e922f41e
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-05 18:29:05 -0300

    feat: requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+describe('testing the NotFound page', () => {
+  test(`the page has a h2 heading with the text 
+    "Page requested not found 😭"`, () => {
+    render(<NotFound />,
+      { wrapper: MemoryRouter });
+    const heading = screen.getByText(/Page requested not found/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('the page has a "crying Pikachu image"', () => {
+    const { container } = render(<NotFound />);
+    const image = container.querySelector(
+      '[src = "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"]',
+    );
+    expect(image).toBeInTheDocument();
+  });
+});
