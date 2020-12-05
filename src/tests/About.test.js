c3b7cce42f3d6dc8676dd4cf46c5f1a601a51913
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-12-05 11:42:57 -0300

    feat: ajusting the image check test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
-import { screen } from '@testing-library/dom';
+import { cleanup, render, fireEvent } from '@testing-library/react';
 import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
 afterEach(cleanup);
 
 describe('testing the About page', () => {
   test('the page has a h2 heading with the text "About Pokédex"', () => {
-    const { getByText } = render(<About />);
-    const heading = screen.getByTestId('h2-heading');
-    expect(heading).toBeInTheDocument();
-    const headingText = getByText(/\bAbout Pokédex\b/);
-    expect(headingText).toBeInTheDocument();
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
   });
 
   test('the page has two paragraphs with Pokédex information', () => {
