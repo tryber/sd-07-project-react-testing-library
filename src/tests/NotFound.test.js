096098ac7e5b49cd829d23dc71da50f02827a3ae
vitorAmorims <vtamorims@gmail.com>
2020-12-05 09:37:13 -0300

    correcoes sintaxe NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import NotFound from './../components/NotFound'
+import NotFound from '../components/NotFound';
 
 test('Teste se página contém um heading h2 com o texto Page requested not found 😭', () => {
     const { container } = render(<NotFound />);
@@ -8,9 +8,9 @@ test('Teste se página contém um heading h2 com o texto Page requested not foun
     expect(tagH2).toHaveTextContent('Page requested not found 😭');
 });
 
-test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
-    const { container } = render(<NotFound />);
-    const tagImg =  container.querySelector('img');
-    expect(tagImg).toBeInTheDocument()
-    expect(tagImg.getAttribute('src')).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+test('se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+  const { container } = render(<NotFound />);
+  const tagImg =  container.querySelector('img');
+  expect(tagImg).toBeInTheDocument();
+  expect(tagImg.getAttribute('src')).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
