2ac8553a22ffa164a38b72449bee59973e94f898
Raphael Caputo <faelelf2@gmail.com>
2020-12-05 02:32:13 -0300

    Mais um subRequisito

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import Pokemon from '../components/Pokemon';
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
+import App from '../App';
 
 describe('Testando o arquivo Pokemon.js', () => {
   test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
@@ -16,5 +17,11 @@ describe('Testando o arquivo Pokemon.js', () => {
     () => {
       renderWithRouter(<Pokemon pokemon={ pokemons[1] } isFavorite={ false } />);
       expect(screen.getByText(/more details/i).href).toBe('http://localhost/pokemons/4');
+    });
+  test('Teste se o link de navegação do Pokémon é feito para a página de detalhes',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      fireEvent.click(getByText(/more details/i));
+      expect(history.location.pathname).toBe('/pokemons/25');
     });
 });
