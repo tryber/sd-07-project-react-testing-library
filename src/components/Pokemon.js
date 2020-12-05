db80959ddb4c2785dc479859a7e2aeb5370865c9
Fernando <nando.neves@gmail.com>
2020-12-05 00:52:29 -0300

    pokedex incomplete

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
 import { pokemonType } from '../types';
-
 import './pokemon.css';
 
 const Pokemon = ({ pokemon, showDetailsLink, isFavorite }) => {
