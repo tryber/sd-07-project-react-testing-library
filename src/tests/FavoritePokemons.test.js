6a25dda62eeb5289f06d6864b3c2afa976892c62
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-12-05 13:27:08 -0300

    test do FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Testando o arquivo FavoritePokemons.js', () => {
+  it('check if No favorite pokemon found is displayed, if no favorite pokemon.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const message = getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  it('check to see if all favorite Pokémon cards are displayed.', () => {
+    const favorites = [pokemons[0], pokemons[1]];
+    const { getByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ favorites } />,
+    );
+
+    expect(getByText(favorites[0].name)).toBeInTheDocument();
+    expect(getByText(favorites[1].name)).toBeInTheDocument();
+  });
+
+  it('deve não encontrar um pokemon não favoritado', () => {
+    const favorites = [pokemons[0], pokemons[1]];
+
+    const { queryByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ favorites } />,
+    );
+
+    expect(queryByText(pokemons[2].name)).toBe(null);
+  });
+});
