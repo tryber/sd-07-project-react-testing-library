c101a5f6b9b1262206e8e2ff09a8450df978f053
Jr3564 <rodolfojr94@gmail.com>
2020-12-04 22:55:38 -0300

    Caçando mutantes. Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Requisito 1. Testando o arquivo App.js', () => {
+  afterEach(cleanup);
+  it('Teste se a página principal da Pokédex é renderizada.', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+  it('Aplicação contém um conjunto fixo de links de navegação.', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const nav = getByRole(/nav/i);
+    expect(nav).toBeInTheDocument();
+    expect(getByText(/Home/i)).not.toBeNull();
+    expect(getByText(/About/i)).not.toBeNull();
+    expect(getByText(/Favorite Pokémons/i)).not.toBeNull();
+  });
+  it('Redirecionada à Home.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('Redirecionada para a página de About.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('Redirecionada para a página de Pokémons Favoritados.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+  it('Redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const notFound = getByText(/Not Found/i);
+    expect(notFound).toBeInTheDocument();
+  });
 });
