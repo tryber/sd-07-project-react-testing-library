495c78147cf182df674e696aa1694d82629ea6f8
Raphael Caputo <faelelf2@gmail.com>
2020-12-04 21:24:25 -0300

    Terceiro subRequisito do requisito 2, about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -16,4 +16,15 @@ describe('Testando o arquivo About.js', () => {
     expect(getByText(/About pokédex/i)).toBeInTheDocument();
     expect(getByText(/About pokédex/i).tagName).toBe('H2');
   });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(getByText(/this application simulates a pokédex,/i)).toBeInTheDocument();
+    expect(getByText(/One can filter Pokémons by type,/i)).toBeInTheDocument();
+    expect(getByText(/this application simulates a pokédex,/i).tagName).toBe('P');
+    expect(getByText(/One can filter Pokémons by type,/i).tagName).toBe('P');
+  });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    expect(getByRole('img').src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
