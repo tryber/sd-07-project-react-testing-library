ba46fe9d548af814eb841c21a8e9af41a1ce8c0d
Raphael Caputo <faelelf2@gmail.com>
2020-12-05 01:55:23 -0300

    Adicionando Primeiro subRequisito do requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import Pokemon from '../components/Pokemon';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Testando o arquivo Pokemon.js', () => {
+  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemons[1] } isFavorite={ false } />);
+    expect(screen.getByTestId('pokemon-weight'))
+      .toHaveTextContent(/average weight: 8.5 kg/i);
+  });
+});
