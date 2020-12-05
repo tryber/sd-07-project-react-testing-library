a9b777478c39e878500ebb282dbb5cf37ae63b54
Raphael Caputo <faelelf2@gmail.com>
2020-12-04 21:00:42 -0300

    Primeiro subRequisito do requisito 2, about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testando o arquivo About.js', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(
+      getByText(
+        /this application simulates a pokédex, a digital encliclopedia containing all /i,
+      ),
+    ).toBeInTheDocument();
+  });
+});
