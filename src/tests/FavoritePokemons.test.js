a07bad371d72e1c905a7405358677dcae7f0e70f
moacyrrln <moacyrrln@gmail.com>
2020-12-05 12:49:26 -0300

    requisito3+Lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
-import { render, fireEvent, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 describe('Testando o arquivo FavoritePokemons.js', () => {
   it(`Teste se é exibido na tela a mensagem No favorite pokemon found, 
@@ -16,13 +15,12 @@ describe('Testando o arquivo FavoritePokemons.js', () => {
 
 describe('Testando o arquivo FavoritePokemons.js', () => {
   it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
+    const { getByText } = renderWithRouter(<App />);
     fireEvent.click(getByText(/More details/i));
     const favorite = getByText(/Pokémon favoritado/);
     fireEvent.click(favorite);
     fireEvent.click(getByText(/Favorite Pokémons/i));
     const weight = getByText(/Average weight: 6.0 kg/);
     expect(weight).toBeInTheDocument();
-
   });
 });
