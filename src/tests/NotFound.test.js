9e36662f274b61ee5330778b73d6de33306d8629
vitorAmorims <vtamorims@gmail.com>
2020-12-05 10:19:46 -0300

    correcoes de sintaxe NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,13 +4,13 @@ import NotFound from '../components/NotFound';
 
 test('A página contém um heading h2 com o texto Page requested not found 😭', () => {
   const { container } = render(<NotFound />);
-  const tagH2 =  container.querySelector('h2');
+  const tagH2 = container.querySelector('h2');
   expect(tagH2).toHaveTextContent('Page requested not found 😭');
 });
 
 test('Página mostra https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
   const { container } = render(<NotFound />);
-  const tagImg =  container.querySelector('img');
+  const tagImg = container.querySelector('img');
   expect(tagImg).toBeInTheDocument();
   expect(tagImg.getAttribute('src')).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
