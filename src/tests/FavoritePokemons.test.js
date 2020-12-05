5bd5c3a22d9c408435bea5d7f34b7b3bf6aab789
Raphael Caputo <faelelf2@gmail.com>
2020-12-04 22:13:03 -0300

    Colocando os dois primeiros subRequisitos do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import pokemons from '../data';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Testando o arquivo FavoritePokemons.js', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/no favorite pokemon found/i)).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    expect(getByText(/Caterpie/i)).toBeInTheDocument();
+    expect(getByText(/Ekans/i)).toBeInTheDocument();
+  });
+  test('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+    const { queryByText } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    expect(queryByText(/pikachu/i)).toBeNull();
+  });
+});
