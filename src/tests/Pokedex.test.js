c1e858cf0e0305206018d060fec2eacf50f29618
Rogerio-feitor <roger.feitor@gmail.com>
2020-12-06 19:41:28 -0300

    Expecting everything to work

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
@@ -48,6 +48,70 @@ describe('Testing the Pokedex.js file', () => {
     expect(firstPokemon).toBeInTheDocument();
   });
   it('Should to test if the Pokédex has the filter buttons.', () => {
-    
+    const { getByText, getAllByTestId } = renderWithRouter(<App />);
+    const firstPokemon = getByText(/Pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
+    const typeButton = getAllByTestId('pokemon-type-button');
+    expect(typeButton[0]).toBeInTheDocument();
+    fireEvent.click(typeButton[1]);
+    const secondPokemon = getByText(/Charmander/i);
+    expect(secondPokemon).toBeInTheDocument();
+    fireEvent.click(typeButton[2]);
+    const thirdPokemon = getByText(/Caterpie/i);
+    expect(thirdPokemon).toBeInTheDocument();
+    fireEvent.click(typeButton[3]);
+    const fourthPokemon = getByText(/Ekans/i);
+    expect(fourthPokemon).toBeInTheDocument();
+    fireEvent.click(typeButton[4]);
+    const fifthPokemon = getByText(/Alakazam/i);
+    expect(fifthPokemon).toBeInTheDocument();
+    fireEvent.click(typeButton[5]);
+    const sixthPokemon = getByText(/Snorlax/i);
+    expect(sixthPokemon).toBeInTheDocument();
+    fireEvent.click(typeButton[6]);
+    const seventhPokemon = getByText(/Dragonair/i);
+    expect(seventhPokemon).toBeInTheDocument();
+  });
+  it('Should to test if the Pokédex contains a button All to reset the filter', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const filterButton = getByText('All');
+    fireEvent.select(filterButton);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+    expect(filterButton).toBeInTheDocument();
+    fireEvent.click(filterButton);
+    const firstPokemon = getByText(/Pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
+  });
+  it('Should to test if buttons are created dynamically for each type of Pokémon.', () => {
+    const { getByText, getAllByTestId } = renderWithRouter(<App />);
+    const filterButton = getByText('All');
+    fireEvent.select(filterButton);
+    const allTypesButtons = getAllByTestId('pokemon-type-button');
+    expect(allTypesButtons[0].innerHTML).toBe('Electric');
+    fireEvent.click(allTypesButtons[0]);
+    expect(filterButton).toBeInTheDocument();
+    expect(allTypesButtons[1].innerHTML).toBe('Fire');
+    fireEvent.click(allTypesButtons[1]);
+    expect(filterButton).toBeInTheDocument();
+    expect(allTypesButtons[2].innerHTML).toBe('Bug');
+    fireEvent.click(allTypesButtons[2]);
+    expect(filterButton).toBeInTheDocument();
+    expect(allTypesButtons[3].innerHTML).toBe('Poison');
+    fireEvent.click(allTypesButtons[3]);
+    expect(filterButton).toBeInTheDocument();
+    expect(allTypesButtons[4].innerHTML).toBe('Psychic');
+    fireEvent.click(allTypesButtons[4]);
+    expect(filterButton).toBeInTheDocument();
+    expect(allTypesButtons[5].innerHTML).toBe('Normal');
+    fireEvent.click(allTypesButtons[5]);
+    expect(filterButton).toBeInTheDocument();
+    expect(allTypesButtons[6].innerHTML).toBe('Dragon');
+    fireEvent.click(allTypesButtons[6]);
+    expect(filterButton).toBeInTheDocument();
   });
 });
