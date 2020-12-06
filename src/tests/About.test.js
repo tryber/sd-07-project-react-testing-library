a3011227f6a9cc2fec9a3152c3b2f20cd38decfe
dennerpl <denner.souzapl@gmail.com>
2020-12-06 12:40:27 -0300

    missing trailing comma

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -34,7 +34,7 @@ describe('testing file About.js', () => {
     });
     expect(pokedexImg).toHaveAttribute(
       'src',
-      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
     );
   });
 });
