336cca3882bb43fd6d249484488bbc9fc987b5d8
vitorAmorims <vtamorims@gmail.com>
2020-12-06 12:20:01 -0300

    Ao carregar a página, o filtro selecionado deverá ser All

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,6 +2,34 @@ import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import Pokedex from '../components/Pokedex';
 import renderWithRouter from '../renderWithRouter';
+// import Button from '../components/Button';
+
+const pokemon = [
+  {
+    averageWeight: { value: '6.0', measurementUnit: 'kg' },
+    image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    name: 'Pikachu',
+    summary: 'This intelligent Pokémon roasts hard berries with electricity'
+    + 'to make them tender enough to eat.',
+    type: 'Electric',
+  },
+  {
+    averageWeight: { value: '8.5', measurementUnit: 'kg' },
+    image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    name: 'Charmander',
+    summary: 'The flame on its tail shows the strength of its life force.'
+    + 'If it is weak, the flame also burns weakly.',
+    type: 'Fire',
+  },
+  {
+    averageWeight: { value: '48.0', measurementUnit: 'kg' },
+    image: 'https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+    name: 'Alakazam',
+    summary: 'Closing both its eyes heightens all its other senses.'
+    + 'This enables it to use its abilities to their extremes.',
+    type: 'Psychic',
+  },
+];
 
 test('A página contém um heading h2 com o texto Encountered pokémons?', async () => {
   const pokemon = [
@@ -105,32 +133,6 @@ test('Teste se é mostrado apenas um Pokémon por vez.', async () => {
 });
 
 describe('Teste se a Pokédex tem os botões de filtro.', () => {
-  const pokemon = [
-    {
-      averageWeight: { value: '6.0', measurementUnit: 'kg' },
-      image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-      name: 'Pikachu',
-      summary: 'This intelligent Pokémon roasts hard berries with electricity'
-      + 'to make them tender enough to eat.',
-      type: 'Electric',
-    },
-    {
-      averageWeight: { value: '8.5', measurementUnit: 'kg' },
-      image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
-      name: 'Charmander',
-      summary: 'The flame on its tail shows the strength of its life force.'
-      + 'If it is weak, the flame also burns weakly.',
-      type: 'Fire',
-    },
-    {
-      averageWeight: { value: '48.0', measurementUnit: 'kg' },
-      image: 'https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
-      name: 'Alakazam',
-      summary: 'Closing both its eyes heightens all its other senses.'
-      + 'This enables it to use its abilities to their extremes.',
-      type: 'Psychic',
-    },
-  ];
   test('A partir da seleção de um botão de tipo, a Pokédex deve circular'
   + 'somente pelos pokémons daquele tipo;', async () => {
     const { getByText } = renderWithRouter(
@@ -155,19 +157,33 @@ describe('Teste se a Pokédex tem os botões de filtro.', () => {
   });
 });
 
-// Teste se a Pokédex tem os botões de filtro.
+describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+  test('O texto do botão deve ser All', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex pokemons={ pokemon } isPokemonFavoriteById={ 25 } />,
+    );
+    const btnAll = getByText(/All/i);
+    expect(btnAll).toBeInTheDocument();
+  });
+  test('A Pokedéx deverá mostrar os Pokémons normalmente'
+  + '(sem filtros) quando o botão All for clicado;', () => {
 
-// A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo;
-
-// O texto do botão deve corresponder ao nome do tipo, ex. Psychic;
-
-// Teste se a Pokédex contém um botão para resetar o filtro
-
-// O texto do botão deve ser All;
-
-// A Pokedéx deverá mostrar os Pokémons normalmente (sem filtros) quando o botão All for clicado;
-
-// Ao carregar a página, o filtro selecionado deverá ser All;
+  });
+  test('Ao carregar a página, o filtro selecionado deverá ser All', () => {
+    const clickBtnAll = jest.fn();
+    const { queryByTestId } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemon }
+        onClick={ clickBtnAll }
+        isPokemonFavoriteById={ 25 }
+      />,
+    );
+    const btn = queryByTestId('');
+    if (btn.textContent = 'All') btn.onClick;
+    fireEvent.load(queryByTestId(''));
+    expect(queryByTestId('').textContent).toBe('All');
+  });
+});
 
 // Teste se é criado, dinamicamente, um botão de filtro para cada tipo de Pokémon.
 
