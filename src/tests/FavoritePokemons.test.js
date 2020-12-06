8938b064de43265ab7738cefc4b92e36a9276b7c
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-12-06 10:36:38 -0300

    Implementando Testes do App, About, Favorite, NotFound - 06/12/2020

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import RenderWithRouter from './RenderWithRouter';
+import App from '../App';
+import Favorite from '../components/FavoritePokemons';
+
+afterEach(cleanup);
+
+describe('EX03 - Testando o arquivo FavoritePokemons.js', () => {
+  test('mensagem "No favorite pokemon found", se não tiver pokémons favoritos', () => {
+    const { getByText, container } = RenderWithRouter(<Favorite />);
+
+    const favoritePokemon = container.getElementsByClassName('favorite-pokemon');
+
+    if(favoritePokemon.length === 0) {
+      const messageNotFound = getByText(/No favorite pokemon found/i);
+      expect(messageNotFound).toBeInTheDocument();
+    }
+
+  });
+
+  test('exibido todos os cards de pokémons favoritados.', () => {
+    const {
+      getByTestId,
+      getByText,
+      getByLabelText,
+      container,
+    } = RenderWithRouter(<App />);
+
+    const detailsPokemon = getByText('More details');
+    fireEvent.click(detailsPokemon);
+
+    const inputFavorite = getByLabelText('Pokémon favoritado?');
+    fireEvent.click(inputFavorite);
+
+    const linkHome = getByText('Home');
+    fireEvent.click(linkHome);
+
+    const buttonNext = getByTestId('next-pokemon');
+    fireEvent.click(buttonNext);
+
+    const detailsPokemon2 = getByText('More details');
+    fireEvent.click(detailsPokemon2);
+
+    const inputFavorite2 = getByLabelText('Pokémon favoritado?');
+    fireEvent.click(inputFavorite2);
+
+    const linkFavorite = getByText('Favorite Pokémons');
+    fireEvent.click(linkFavorite);
+
+    const favoritePokemons = container.getElementsByClassName('favorite-pokemon');
+    expect(favoritePokemons.length).toBe(2);
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import RenderWithRouter from './RenderWithRouter';
+import Pokedex from '../components/Pokedex';
+import Data from '../data';
+
+afterEach(cleanup);
+
+describe('EX05 - Testando o arquivo Pokedex.js', () => {
+  test('heading h2 com o texto Encountered pokémons', () => {
+    const { getByText } = RenderWithRouter(<Pokedex pokemons={ Data } isPokemonFavoriteById={{}} />);
+
+    const subtitle = getByText(/Encountered pokémons/i);
+
+    expect(subtitle.tagName).toBe('H2');
+    expect(subtitle).toBeInTheDocument();
+  })
+});
