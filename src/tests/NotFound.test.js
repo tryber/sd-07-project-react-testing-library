fc0b25877fb1e1ca18df3045281a7cffe0b8fb35
moises <moisaant@gmail.com>
2020-12-06 00:59:15 -0300

    test 1/2

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Requiriment 04', () => {
+  test('1/2', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTitle = screen.getByText(/page requested not found/i);
+
+    expect(notFoundTitle).toBeInTheDocument();
+    expect(notFoundTitle.tagName).toBe('H2');
+  });
+
+});
