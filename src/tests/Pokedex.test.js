ade11735ab13ac199c4e75f977a81d606d365604
Rogerio-feitor <roger.feitor@gmail.com>
2020-12-06 17:59:34 -0300

    Some requirement 5 tests

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -17,4 +17,37 @@ describe('Testing the Pokedex.js file', () => {
     const tagH2 = container.querySelector('h2').innerHTML;
     expect(tagH2).toBe('Encountered pokémons');
   });
+  it('Should if the next Pokémon is displayed when the same button is clicked.', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    const firstPokemon = getByText(/Pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
+    const btnNextPokemon = getByTestId('next-pokemon');
+    expect(btnNextPokemon.innerHTML).toBe('Próximo pokémon');
+    fireEvent.click(btnNextPokemon);
+    const secondPokemon = getByText(/Charmander/i);
+    expect(secondPokemon).toBeInTheDocument();
+  });
+  it('Should to show only one Pokémon is shown at a time.', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    const firstPokemon = getByText(/Pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
+    const btnNextPokemon = getByTestId('next-pokemon');
+    fireEvent.click(btnNextPokemon);
+    const secondPokemon = getByText(/Charmander/i);
+    expect(secondPokemon).toBeInTheDocument();
+    fireEvent.click(btnNextPokemon);
+    const thirthPokemon = getByText(/Caterpie/i);
+    expect(thirthPokemon).toBeInTheDocument();
+    fireEvent.click(btnNextPokemon);
+    fireEvent.click(btnNextPokemon);
+    fireEvent.click(btnNextPokemon);
+    fireEvent.click(btnNextPokemon);
+    fireEvent.click(btnNextPokemon);
+    fireEvent.click(btnNextPokemon);
+    fireEvent.click(btnNextPokemon);
+    expect(firstPokemon).toBeInTheDocument();
+  });
+  it('Should to test if the Pokédex has the filter buttons.', () => {
+    
+  });
 });
