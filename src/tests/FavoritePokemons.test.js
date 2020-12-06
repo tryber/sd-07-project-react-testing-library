92504e40379c7b5f6631ac9d6aedda26bc7e0fa4
dennerpl <denner.souzapl@gmail.com>
2020-12-06 13:11:12 -0300

    fazendo um teste 2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('testing file FavoritePokemons.js', () => {
-    test('if favorite pokemons render No favorite pokemon found', () => {
-        renderWithRouter(<App />);
-        fireEvent.click(screen.getByText(/Favorite Pokémons/i));
-        expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
-    })
+  test('if favorite pokemons render No favorite pokemon found', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Favorite Pokémons/i));
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
-  
+});
