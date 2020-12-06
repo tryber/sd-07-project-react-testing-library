fb392649110111be797065fc09b1355f40091836
Fernando <nando.neves@gmail.com>
2020-12-05 23:51:48 -0300

    pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -23,11 +24,32 @@ test('if pokemon contains correct link', () => {
   expect(link.href).toBe('http://localhost/pokemons/25');
 });
 // Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento da aplicação para a página de detalhes de Pokémon.
-
+test('if link takes to correct page', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const button = getByText(/more details/i);
+  fireEvent.click(button);
+  const text = getByText(/Pikachu Details/i);
+  expect(text).toBeInTheDocument();
+});
 // Teste também se a URL exibida no navegador muda para /pokemon/<id>, onde <id> é o id do Pokémon cujos detalhes se deseja ver;
-
+test('if link takes to correct page', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const button = getByText(/more details/i);
+  fireEvent.click(button);
+  const homePathname = history.location.pathname;
+  expect(homePathname).toBe('/pokemons/25');
+});
 // Teste se existe um ícone de estrela nos Pokémons favoritados.
-
 //        O ícone deve ser uma imagem com o atributo src contendo o caminho /star-icon.svg;
-
 //        A imagem deve ter o atributo alt igual a <pokemon> is marked as favorite, onde <pokemon> é o nome do Pokémon exibido.
+test('if star icon appears on favorited pokemon', () => {
+  const { getByText, container } = renderWithRouter(<App />);
+  const button = getByText(/more details/i);
+  fireEvent.click(button);
+  const addFavorite = getByText(/Pokémon favoritado/i);
+  fireEvent.click(addFavorite);
+  const findStar = container.getElementsByClassName('favorite-icon')[0];
+  expect(findStar.src).toBe('http://localhost/star-icon.svg');
+  expect(findStar.alt).toBe('Pikachu is marked as favorite');
+  expect(findStar).toBeInTheDocument();
+});
