cfa15e23f554e94a4588d9ce0bf4e0e0f7cd34ec
moises <moisaant@gmail.com>
2020-12-05 22:36:12 -0300

    test 1/4

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Requiriment 02', () => {
+  test('1/4', () => {
+    renderWithRouter(<About />);
+
+    const aboutMessage = screen.getByText(
+      /this application simulates a pokédex, a digital encliclopedia containing all pokémons/i
+    );
+
+    expect(aboutMessage).toBeInTheDocument();
+  });
+
+});
