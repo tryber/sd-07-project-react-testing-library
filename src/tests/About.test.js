05630785dd2f031ec9562be88cb99e3650c96cb4
Jr3564 <rodolfojr94@gmail.com>
2020-12-05 21:45:20 -0300

    Caçando mutantes. Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, cleanup, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('2. Testando o arquivo About.js', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/About/i));
+  });
+  afterEach(cleanup);
+  it('A página contém as informações sobre a Pokédex.', () => {
+    const pokedex = screen.queryAllByText(/Pokédex/i);
+    expect(pokedex).not.toBeNull();
+  });
+  it('A página contém um heading h2 com o texto About Pokédex.', () => {
+    const h2 = screen.getByRole('heading', { name: /About/i }, { level: 2 });
+    expect(h2).toBeInTheDocument();
+  });
+  it('A página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const p1 = screen.getByText(/one can filter pokémons by type,/i);
+    const p2 = screen.getByText(/encliclopedia containing all Pokémons/i);
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const imgSrc = 'https://cdn.bulbagarden.net/upload/thumb'
+    + '/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img', { name: /pokédex/i });
+    expect(img.src).toBe(imgSrc);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,45 +1,45 @@
 import React from 'react';
-import { fireEvent, cleanup } from '@testing-library/react';
+import { fireEvent, cleanup, screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Requisito 1. Testando o arquivo App.js', () => {
   afterEach(cleanup);
   it('Teste se a página principal da Pokédex é renderizada.', () => {
-    const { getByText } = renderWithRouter(<App />);
-    const heading = getByText(/Pokédex/i);
+    renderWithRouter(<App />);
+    const heading = screen.getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
   it('Aplicação contém um conjunto fixo de links de navegação.', () => {
-    const { getByRole, getByText } = renderWithRouter(<App />);
-    const nav = getByRole(/nav/i);
+    renderWithRouter(<App />);
+    const nav = screen.getByRole(/nav/i);
     expect(nav).toBeInTheDocument();
-    expect(getByText(/Home/i)).not.toBeNull();
-    expect(getByText(/About/i)).not.toBeNull();
-    expect(getByText(/Favorite Pokémons/i)).not.toBeNull();
+    expect(screen.queryByText(/Home/i)).not.toBeNull();
+    expect(screen.queryByText(/About/i)).not.toBeNull();
+    expect(screen.queryByText(/Favorite Pokémons/i)).not.toBeNull();
   });
   it('Redirecionada à Home.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/Home/i));
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Home/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
   it('Redirecionada para a página de About.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/About/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/about');
   });
   it('Redirecionada para a página de Pokémons Favoritados.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Favorite Pokémons/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/favorites');
   });
   it('Redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
     history.push('/xablau');
-    const notFound = getByText(/Not Found/i);
+    const notFound = screen.getByText(/Not Found/i);
     expect(notFound).toBeInTheDocument();
   });
 });
