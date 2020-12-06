07b44abe128ea0f60e910f1c441a031377a28eb9
moises <moisaant@gmail.com>
2020-12-05 22:58:40 -0300

    test 4/4

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -36,4 +36,13 @@ describe('Requiriment 02', () => {
     expect(aboutParagraphTwo.tagName).toBe('P');
   });
 
+  test('4/4', () => {
+    renderWithRouter(<About />);
+
+    const aboutImage = screen.getByRole('img');
+
+    expect(aboutImage.src).toBe(
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    );
+  });
 });
