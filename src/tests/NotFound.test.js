0c3307340bf26cd110b4bb3e4029c6af579085ce
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-05 23:53:44 -0300

    Test 4 ready

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('Test 4 - NotFound.js', () => {
+  it('Should to contain a h2 tag with \'Page requested not found 😭\'', () => {
+    const { container } = RenderWithRouter(<NotFound />);
+    const tagH2Title = container.querySelector('h2');
+    const textH2 = tagH2Title.innerHTML;
+    expect(textH2).toContain('Page requested not found');
+  });
+  it('Should to contain an expecifc imag', () => {
+    const { container } = RenderWithRouter(<NotFound />);
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imagTag = container.querySelector('img');
+    expect(imagTag.src).toBe(imgSrc);
+  });
+});
