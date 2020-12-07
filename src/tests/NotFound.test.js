04268aa7421f2c666e441b6109cf1115cf794791
Lucas Andrade <lafdesouza2002@outlook.com>
2020-12-06 23:50:26 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('not found works correctly', () => {
+  test('heading renderer correctly', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const text = getByText(/Page requested not found/i);
+    expect(text).toBeInTheDocument();
+  });
+  test('image renderer correctly', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
