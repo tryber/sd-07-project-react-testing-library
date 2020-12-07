59286070a6b0c5db123dc2f34f149cf577d4897e
Luise Rios <luisemiranda@gmail.com>
2020-12-07 14:47:11 -0300

    Requisito 2 - correçao lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import About from '../components/About';
 import { render } from '@testing-library/react';
+import About from '../components/About';
 
 test('Testa se a página contém h2 com About Pokédex', () => {
-    const { getByTestId, getByText } = render(<About />);
-    const h2Content = getByText(/About Pokédex/i);
-    expect(h2Content).toBeInTheDocument();
+  const { getByText } = render(<About />);
+  const h2Content = getByText(/About Pokédex/i);
+  expect(h2Content).toBeInTheDocument();
 });
 
 test('Testa se a página contém a imagem correta', () => {
-    const { getByAltText } = render(<About />);
-    const img = getByAltText('Pokédex');
-    expect(img).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  const { getByAltText } = render(<About />);
+  const img = getByAltText('Pokédex');
+  expect(img).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
-
