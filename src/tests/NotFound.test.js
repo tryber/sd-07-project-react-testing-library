8ab525c10c65055f306cb0ad21b2768541ee74fa
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-12-07 14:57:41 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRoute from '../components/renderWithRoute';
+
+describe('Testando o componente NotFound', () => {
+  it('Testando se a página contém heading h2', () => {
+    const { getByText, history } = renderWithRoute(<App />);
+    history.push('/xablau');
+    const pageNaoEncontrada = getByText(/Page requested not found/i);
+    expect(pageNaoEncontrada).toBeInTheDocument();
+  });
+});
