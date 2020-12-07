6262408fbf0171183e47b4ea25e8d78a9e3e6c29
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-12-07 13:15:07 -0300

    Requirement 1 gh-actions issues

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, cleanup } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
@@ -19,16 +19,15 @@ afterEach(cleanup);
 describe('Requirement 1: Testing App.js', () => {
   test('if the Pokedex is displayed when the URL is "/"', () => {
     const { getByText } = renderWithRouter(<App />);
-  
     expect(getByText(/Encountered pokémons/i)).toBeInTheDocument();
   });
-  
+
   test('if there are navigation links at the top of the page', () => {
     const { getByRole } = renderWithRouter(<App />);
 
-    const homeLink = getByRole('link', {name: /home/i});
-    const aboutLink = getByRole('link', {name: /about/i});
-    const favPokeLink = getByRole('link', {name: /favorite pokémons/i});
+    const homeLink = getByRole('link', { name: /home/i });
+    const aboutLink = getByRole('link', { name: /about/i });
+    const favPokeLink = getByRole('link', { name: /favorite pokémons/i });
 
     expect(homeLink).toBeInTheDocument();
     expect(aboutLink).toBeInTheDocument();
@@ -36,26 +35,26 @@ describe('Requirement 1: Testing App.js', () => {
   });
 
   test('if "Home" redirects to "/" after clicking', () => {
-    const { getByRole, history } = renderWithRouter(<App />)
-    const homeLink = getByRole('link', {name: /home/i});
-    const { pathname } = history.location
+    const { getByRole, history } = renderWithRouter(<App />);
+    const homeLink = getByRole('link', { name: /home/i });
+    const { pathname } = history.location;
 
     fireEvent.click(homeLink);
     expect(pathname).toBe('/');
   });
 
   test('if "About" redirects to "/about" after clicking', () => {
-    const { getByRole, history } = renderWithRouter(<App />)
-    const aboutLink = getByRole('link', {name: /about/i});
-    
+    const { getByRole, history } = renderWithRouter(<App />);
+    const aboutLink = getByRole('link', { name: /about/i });
+
     fireEvent.click(aboutLink);
-    const { pathname } = history.location
+    const { pathname } = history.location;
     expect(pathname).toBe('/about');
   });
 
   test('if "Favorite Pokémons" redirects to "/favorites" after clicking', () => {
     const { getByRole, history } = renderWithRouter(<App />);
-    const favPokeLink = getByRole('link', {name: /favorite pokémons/i});
+    const favPokeLink = getByRole('link', { name: /favorite pokémons/i });
 
     fireEvent.click(favPokeLink);
     const { pathname } = history.location;
