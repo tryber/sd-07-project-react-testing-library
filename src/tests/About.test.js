21657e2bcc2ddba8ae3d7d5115d4c959784a2527
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-12-07 18:23:33 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import About from '../components/About';
+
+beforeEach(() => {
+  renderWithRouter(<About />);
+});
+
+test('if has tha heading "About Pokedex"', () => {
+  const heading = screen.getByRole('heading');
+
+  expect(heading).toBeInTheDocument();
+  expect(heading).toHaveTextContent('About Pokédex');
+});
+
+test('if page contains two paragraphs about Pokedex', () => {
+  const firstText = 'This application simulates a Pokédex, '
+  + 'a digital encliclopedia containing all Pokémons';
+  const secondText = 'One can filter Pokémons by type, '
+   + 'and see more details for each one of them';
+  const firstParagraph = screen.getByText(firstText);
+  const secondParagraph = screen.getByText(secondText);
+
+  expect(firstParagraph).toBeInTheDocument();
+  expect(secondParagraph).toBeInTheDocument();
+});
+
+test('if image is renderes', () => {
+  const image = screen.getByAltText('Pokédex');
+
+  expect(image).toBeInTheDocument();
+  expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -27,4 +28,77 @@ test('if render the links in the home page', () => {
   expect(homeLink).toBeInTheDocument();
   expect(aboutLink).toBeInTheDocument();
   expect(favoriteLink).toBeInTheDocument();
+});
+
+test('if Pokedex main page is rendered', () => {
+  renderWithRouter(<App />);
+  const encountered = screen.getByText(/encountered pokémons/i);
+
+  expect(encountered).toBeInTheDocument();
+});
+
+describe('test if the links and the paths are valid', () => {
+  it('should contain the links and the "/" path in home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByText(/home/i);
+    const aboutLink = screen.getByText(/about/i);
+    const favoriteLink = screen.getByText(/favorite pokémons/i);
+
+    fireEvent.click(homeLink);
+    const homePath = history.location.pathname;
+
+    expect(homePath).toBe('/');
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('should contain the links and the "/about" path in about', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByText(/home/i);
+    const aboutLink = screen.getByText(/about/i);
+    const favoriteLink = screen.getByText(/favorite pokémons/i);
+
+    fireEvent.click(aboutLink);
+    const aboutPath = history.location.pathname;
+
+    expect(aboutPath).toBe('/about');
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('should contain the links and the "/about" path in about', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByText(/home/i);
+    const aboutLink = screen.getByText(/about/i);
+    const favoriteLink = screen.getByText(/favorite pokémons/i);
+
+    fireEvent.click(favoriteLink);
+    const favoritePath = history.location.pathname;
+
+    expect(favoritePath).toBe('/favorites');
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('should render not found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByText(/home/i);
+    const aboutLink = screen.getByText(/about/i);
+    const favoriteLink = screen.getByText(/favorite pokémons/i);
+
+    history.push('/whatever');
+    const notFoundPage = screen.getByText(/Page requested not found/i);
+
+    expect(notFoundPage).toBeInTheDocument();
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
 });
