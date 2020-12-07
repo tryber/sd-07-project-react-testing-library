c3de3701cfa9835a93cc2078cab7113ae4ece269
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-12-07 17:11:46 -0300

    requeriment 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent, getByText } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -13,24 +14,49 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+test('verifying if there are links in the app', () => {
+  const { getByText } = renderWithRouter(<App />);
+  
+  const verifyingHome = getByText('Home');
+  const verifyingAbout = getByText('About');
+  const verifyingFavoritePokemons = getByText('Favorite Pokémons');
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(verifyingHome).toBeInTheDocument();  
+  expect(verifyingAbout).toBeInTheDocument();
+  expect(verifyingFavoritePokemons).toBeInTheDocument();
 });
 
-test('verifying if there are links in the app', () => {
-  const { getByText } = render(<App />);
-  const verifyingHome = getByText('Home');
-  expect(verifyingHome).toBeInTheDocument();
+test("redirects to '/' page when clicking 'Home' link", ()=> {
+  const { getByText, history } = renderWithRouter(<App />);
+  
+  const home = getByText('Home');
+  fireEvent.click(home);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
 
-  const verifyingAbout = getByText('About');
-  expect(verifyingAbout).toBeInTheDocument();
+test("redirects to '/about' page when clicking 'About' link", () => {
+  const { getByText, history } = renderWithRouter(<App />);
 
-  const verifyingFavoritePokemons = getByText('Favorite Pokémons');
-  expect(verifyingFavoritePokemons).toBeInTheDocument();
+  const about = getByText('About');
+  fireEvent.click(about);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test("redirects to '/favorites' page when clicking 'Favorites pokemons' link", () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  const favoritesPokemons = getByText('Favorite Pokémons');
+  fireEvent.click(favoritesPokemons);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('if app is redirected to the Not Found page when entering an unknown URL.', () =>{
+  const { getByText, history } = renderWithRouter(<App />);
+
+  history.push('/pagina-nao-existe');
+  const notFound = getByText('Pikachu crying because the page requested was not found');
+  expect(notFound).toBeInTheDocument();
 });
