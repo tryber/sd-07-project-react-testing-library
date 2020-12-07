a4bf5dfd64d2a873a0b74a6f83226f7cbd60ab8b
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-12-07 16:19:56 -0300

    Zero To Hero: Requirement 4 - NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+describe('Zero to Hero: Requirement 4 - NotFound.js', () => {
+  test('a h2 tag with the text "Page requested not found"', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const notFoundText = getByText(/page requested not found/i);
+    expect(notFoundText).toBeInTheDocument();
+    expect(notFoundText.tagName).toBe('H2');
+  });
+
+  test('if the page has the specified image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText(/pikachu/i);
+    const expectedImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(expectedImage);
+  });
+});
