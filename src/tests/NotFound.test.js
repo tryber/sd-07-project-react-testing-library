802c194430ac0e8b9f5db94c41f1ada7dedabf80
Galaraz <jvictor.si.2013@gmail.com>
2020-12-07 20:38:04 -0300

    rq 2 e 3

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('4.Testando o arquivo NotFound.js', () => {
+  it('Teste se página contém um heading h2', () => {
+    const { getByText, getByRole } = render(<NotFound />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toBeInTheDocument();
+    const text = getByText(/Page requested not found/i);
+    expect(text).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem', () => {
+    const { getByAltText } = render(<NotFound />);
+    const image = getByAltText(/Pikachu/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
