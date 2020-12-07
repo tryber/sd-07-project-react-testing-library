e94ef70b83be1d028982fc6b62a258ffb9212539
Carol Bezerra <carol.arbe@gmail.com>
2020-12-07 17:09:28 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import NotFound from '../components/NotFound';
+
+describe('quarto requisito', () => {
+  test('se página contém um heading h2 com o texto Page requested not found', () => {
+    const history = createMemoryHistory();
+    const { getByText, getByRole } = render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+
+    const h2Tag = getByRole('heading', { level: 2 });
+    const h2Text = getByText('Page requested not found');
+    expect(h2Tag).toBeInTheDocument();
+    expect(h2Text).toBeInTheDocument();
+  });
+
+  test('se mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const history = createMemoryHistory();
+    const { getAllByRole, getByAltText } = render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+
+    const imgAlt = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imgAlt).toBeInTheDocument();
+    expect(getAllByRole('img')[1].src).toBe(imgSrc);
+  });
+});
