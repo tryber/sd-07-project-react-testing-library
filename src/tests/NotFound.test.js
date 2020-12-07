e1ee26e3542da7ab6b5b84dc0cf7a80d7fb8442d
Luise Rios <luisemiranda@gmail.com>
2020-12-07 15:05:42 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('testa se a página contem um h2 com o texto Page requested not found 😭', () => {
+  const { getByText, getByTestId } = render(<NotFound />);
+  const text = getByText(/Page requested not found/i);
+  const emoji = getByTestId(/emoji/i);
+  expect(text).toBeInTheDocument();
+  expect(emoji).toBeInTheDocument();
+});
+
+test('Testa se a página mostra a imagem correta', () => {
+  const { getByAltText } = render(<NotFound />);
+  const img = getByAltText(/Pikachu crying because the page requested was not found/i);
+  expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
