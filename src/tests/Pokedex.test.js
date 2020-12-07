3410578654b43b80f01423c926af6d1e05f76eff
Raphael Caputo <faelelf2@gmail.com>
2020-12-06 23:02:26 -0300

    Adicionando subrequisitos e tentando o 5 de novo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,63 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Testando o arquivo Pokedex.js', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+    expect(screen.getByRole('heading', { name: /encountered pokémons/i }).tagName)
+      .toBe('H2');
+    expect(screen.getByRole('heading', { name: /encountered pokémons/i }))
+      .toBeInTheDocument();
+  });
+  describe('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo clicado',
+    () => {
+      test('O botão deve conter o texto Próximo pokémon;', () => {
+        renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />);
+        expect(screen.getByText(/próximo pokémon/i)).toBeInTheDocument();
+      });
+      test('Os próximos Pokémons da lista devem ser mostrados, ao clicar no botão',
+        () => {
+          const { getByText } = renderWithRouter(<Pokedex
+            pokemons={ pokemons }
+            isPokemonFavoriteById={ {} }
+          />);
+          expect(screen.getByText(/Pikachu/i)).toBeInTheDocument();
+          fireEvent.click(getByText(/próximo pokémon/i));
+          expect(screen.getByText(/Charmander/i)).toBeInTheDocument();
+          fireEvent.click(getByText(/próximo pokémon/i));
+          expect(screen.getByText(/Caterpie/i)).toBeInTheDocument();
+          fireEvent.click(getByText(/próximo pokémon/i));
+          expect(screen.getByText(/Ekans/i)).toBeInTheDocument();
+          fireEvent.click(getByText(/próximo pokémon/i));
+          expect(screen.getByText(/Alakazam/i)).toBeInTheDocument();
+          fireEvent.click(getByText(/próximo pokémon/i));
+        });
+      test('O primeiro Pokémon da lista deve ser mostrado ao clicar no botão',
+        () => {
+          renderWithRouter(<Pokedex
+            pokemons={ pokemons }
+            isPokemonFavoriteById={ {} }
+          />);
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          fireEvent.click(screen.getByText(/próximo pokémon/i));
+          expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+        });
+      test('Teste se é mostrado apenas um Pokémon por vez.', () => {
+        renderWithRouter(<Pokedex
+          pokemons={ pokemons }
+          isPokemonFavoriteById={ {} }
+        />);
+        expect(screen.getAllByTestId(/pokemon-name/i).length).toBe(1);
+      });
+    });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+import PokemonDetails from '../components';
+
+describe('Teste se as informações detalhadas do Pokémon selecionado são mostradas',
+  () => {
+    it('A página deve conter um texto <name> Details, onde <name> é o nome do Pokémon',
+      () => {
+
+      });
+  });
