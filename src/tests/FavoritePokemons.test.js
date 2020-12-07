e3efdc29170041143e9c5345ca769103bd9bfde2
Vitor Rodrigues <vitor_rc@outlook.com>
2020-12-07 15:06:27 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -16,3 +16,27 @@ test("is displayed on the screen 'No favorite pokemon'", () => {
   const noFavoriteText = getByText('No favorite pokemon found');
   expect(noFavoriteText).toBeInTheDocument();
 });
+
+test("is displayed on the screen all favorite pokemons card", () => {
+  const { getByText, getByLabelText, getByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const moreDetailsButton = getByText('More details');
+  fireEvent.click(moreDetailsButton)
+
+  const checkFavorite =  getByLabelText('Pokémon favoritado?');
+  fireEvent.click(checkFavorite);
+
+  const favoritePokemonsButton = getByText('Favorite Pokémons');
+  fireEvent.click(favoritePokemonsButton);
+
+  const pokemonName = getByTestId('pokemon-name');
+  const pokemonType = getByTestId('pokemonType');
+  const pokemonWeight = getByTestId('pokemon-weight');
+
+  expect(pokemonName).toHaveTextContent('Pikachu');
+  expect(pokemonType).toHaveTextContent('Electric');
+  expect(pokemonWeight).toHaveTextContent('6.0 kg');
+});
