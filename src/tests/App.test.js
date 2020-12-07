47b4322c59f0d9d180a02dd7d00791d0b98ce604
murilopereira006 <muriloedu006@gmail.com>
2020-12-07 17:45:00 -0300

    req 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('testing App.js', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const history = createMemoryHistory();
+    const { pathname } = history.location;
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(pathname).toBe('/');
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('test if clicking in Home the URL is redicecioned for /', () => {
+    const history = createMemoryHistory();
+    const { pathname } = history.location;
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    fireEvent.click(getByText(/Home/i));
+    expect(pathname).toBe('/');
+  })
+
+  test('test if clicking in About the URL is redicecioned for /about', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  })
+
+  test('test if clicking in Favorite Pokémons the URL is redicecioned for /favorites', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  })
+})
