67060e50f6c8bfff7f422641c0892e14cf1a4c34
richardfazeredo <richardcompletetour@gmail.com>
2020-12-07 16:02:40 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('testa se é exibido na tela o texto No "favorite pokemon found"', () => {
+  const { getByText } = render(<FavoritePokemons />);
+
+  const text = getByText(/No favorite pokemon found/i);
+  expect(text).toBeInTheDocument();
+});
