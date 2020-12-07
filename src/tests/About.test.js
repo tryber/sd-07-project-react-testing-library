53ca00ba3c9b34ed0b32faaac17045b6165e48be
richardfazeredo <richardcompletetour@gmail.com>
2020-12-07 15:52:37 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+test('testa se a página contém um heading com o texto About Pokédex', () => {
+  const { getByText, getByAltText } = renderWithRouter(<About />);
+
+  const heading = getByText(/About Pokédex/i);
+  expect(heading).toBeInTheDocument();
+
+  const img = getByAltText(/Pokédex/i);
+  expect(img).toBeInTheDocument();
+  expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
