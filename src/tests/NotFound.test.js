032c1c7f0bf592d89ffbd8bd6864799b00a9ca9f
marcelo ivan <sagacello@hotmail.com>
2020-12-07 10:52:13 -0300

    Quarto requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Testando about', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found 😭;', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/rota/errada');
+    const h2 = getByText(`Page requested ` + `not found`);
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Testa se a página tem a imagem', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+    history.push('/rota/errada');
+    const imagem = getByAltText(
+      'Pikachu crying because the page requested was not found'
+    );
+    expect(imagem.src).toEqual(
+      `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`
+    );
+  });
+});
