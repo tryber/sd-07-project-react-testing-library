335529b711e1b4da3345ce8bd97d32a765b3eb12
Bruno <bruno_grb@hotmail.com>
2020-12-07 13:32:25 -0300

    implementaçao do teste 5 finalizada

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -7,3 +7,16 @@ test('The message No favorite pokemon found is displayed on the screen', () => {
   const text = getByText(/No favorite pokemon found/i);
   expect(text).toBeInTheDocument();
 });
+
+test('All favorite Pokémon cards are displayed', () => {
+  const { container } = renderWithRouter(<FavoritePokemons />);
+  const [favorite] = container.getElementsByClassName('favorite-pokemons');
+  expect(favorite).toBe();
+});
+
+test('No Pokémon card is displayed if it is not favored', () => {
+  const { container } = renderWithRouter(<FavoritePokemons />);
+  const [p] = container.getElementsByTagName('p');
+  expect(p).toBeInTheDocument();
+  expect(p).toHaveTextContent('No favorite pokemon found');
+});
