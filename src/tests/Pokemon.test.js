5781d4c2fb05a4f23413d26169345779e51ea5e1
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-06 21:34:32 -0300

    Test 6 going on

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import RenderWithRouter from './RenderWithRouter';
-import { fireEvent } from '@testing-library/react';
+// import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import pokemons from '../data';
 
 describe('Test 6 - Pokemon.js', () => {
   it('', () => {
-    const { getByText } = RenderWithRouter(<App pokemons={ pokemons } />);
-    expect(getByText(pokemons[0].name)).toBeInTheDocument();
-});
+    const { getByTestId, getByRole } = RenderWithRouter(<App pokemons={ pokemons } />);
+    const pokemonName = getByTestId('pokemon-name');
+    const pokemonType = getByTestId('pokemonType');
+    const pokemonWeight = getByTestId('pokemon-weight');
+    const image = getByRole('img', { src: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png' });
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonWeight).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+  });
 });
