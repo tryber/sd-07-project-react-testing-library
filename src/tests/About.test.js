a1e3871237899b1c8be1da324f03372c977e160d
Ana Karine <anakarine.ti@gmail.com>
2020-12-07 17:35:36 -0300

    About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testando informações sobre a Pokédex', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    const { getByText, history } = renderWithRouter(<About />);
+    history.push('/about');
+    expect(getByText(/one can filter pokémons/i)).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto `About Pokédex`', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    expect(getByRole('heading')).toHaveTextContent(/About Pokédex/i);
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    // https://pt-br.reactjs.org/docs/testing-recipes.html
+    const { container } = renderWithRouter(<About />);
+    const paragraphs = container.querySelectorAll('p');
+    expect(paragraphs.length.toString()).toBe('2');
+  });
+
+  it('Teste se a página contém a imagem de uma Pokédex', () => {
+    const { container } = renderWithRouter(<About />);
+    const image = container.querySelector('img');
+    // https://github.com/solid/react-components/blob/master/test/components/Image-test.jsx
+    expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
