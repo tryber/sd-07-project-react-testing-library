0d38fd6c2e4332039b19044c68a216af5b6215e9
josielvs <josielvs@gmail.com>
2020-12-08 07:26:04 -0300

    Requisito 4 - Teste page NotFound.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+it('Verifica se há uma tag h2 com o texto: Page requested not found ', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+
+  const heading = getByText(/Page requested not found/);
+  expect(heading).toBeInTheDocument();
+});
+
+it('Verifica se página mostra a imagem', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const srcImgToFind = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const image = getAllByRole('img');
+
+  expect(image[1].src).toBe(srcImgToFind);
+});
