df19ec2ecbf2cde23497d6910e817387d9660ba8
josielvs <josielvs@gmail.com>
2020-12-07 22:59:25 -0300

    Requisito 1 - Teste page App. Refatorado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
-it('renders a reading with the text `Pokédex`', () => {
+test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -14,43 +14,50 @@ it('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
+it('Verifica se a página inicial reinderiza.', () => {
+  const { history } = renderWithRouter(<App />);
+
+  expect(history.location.pathname).toBe('/');
+});
+
 it('Verifica se no topo temos links de navegação fixados.', () => {
   const { getByText } = renderWithRouter(<App />);
-  const home = getByText(/home/i);
-  const about = getByText(/about/i);
-  const favorites = getByText(/Favorite Pokémons/i);
+  const home = getByText(/Home/);
+  const about = getByText(/About/);
+  const favorites = getByText(/Favorite Pokémons/);
 
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favorites).toBeInTheDocument();
+});
+
+it('Verifica se ao clicar no link HOME a página é reinderizada', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const home = getByText(/Home/);
+  expect(home).toBeInTheDocument();
   expect(home.href).toBe('http://localhost/');
+});
+
+it('Verifica se ao clicar no link ABOUT a página é redirecionada', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const about = getByText(/About/);
+  expect(about).toBeInTheDocument();
   expect(about.href).toBe('http://localhost/about');
+});
+
+it('Verifica se ao clicar no link Favorite a página é redirecionada', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const favorites = getByText(/Favorite Pokémon/);
+  expect(favorites).toBeInTheDocument();
   expect(favorites.href).toBe('http://localhost/favorites');
 });
-it('Verifica se ao clicar no link "HOME" a página é reinderizada', () => {
-  const { getByText, history } = renderWithRouter(<App />);
 
-  fireEvent.click(getByText(/Home/i));
-  const { pathname } = history.location;
-
-  expect(pathname).toBe('/');
-});
-it('Verifica se ao clicar no link "ABOUT" a página é redirecionada', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-
-  fireEvent.click(getByText(/About/i));
-  const { pathname } = history.location;
-
-  expect(pathname).toBe('/about');
-});
-it('Verifica se ao clicar no link "Favorite" a página é redirecionada', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-
-  fireEvent.click(getByText(/Favorite Pokémons/i));
-  const { pathname } = history.location;
-
-  expect(pathname).toBe('/favorites');
-});
-test('Verifica se ao entrar em uma URL desconhecida redireciona para Not Found', () => {
+it('Verifica se ao entrar em uma URL desconhecida redireciona para Not Found', () => {
   const { getByText, history } = renderWithRouter(<App />);
   history.push('/pagina/que-nao-existe/');
-  const noMatch = getByText(/Page requested not found/i);
+  const noMatch = getByText(/Page requested not found/);
   expect(noMatch).toBeInTheDocument();
 });
