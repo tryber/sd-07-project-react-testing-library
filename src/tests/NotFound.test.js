a7a9d6603dde25732f479d305946c240fb4fbed2
Muniz <vinicius.muniz.marin@gmail.com>
2020-12-08 15:22:41 -0300

    4. trocado metodo toBeInTheDocument para toBe

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -7,15 +7,15 @@ describe('4. Testando o arquivo NotFound.js', () => {
     const { getByRole } = renderWithRouter(<NotFound />);
 
     const h2 = getByRole('heading', { level: 2 });
-    expect(h2).toBeInTheDocument();
+    expect(h2.textContent).toBe('Page requested not found');
   });
 
   it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
-    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const { getByAltText } = renderWithRouter(<NotFound />);
 
-    const secondImage = 1;
-    const img = getAllByRole('img')[secondImage];
-
+    const img = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
     expect(img.src).toBe(
       'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
     );
