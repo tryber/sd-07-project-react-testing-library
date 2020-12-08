f439d4c7dc5ecc1c4a1d66f817a2608d5337e8ee
Muniz <vinicius.muniz.marin@gmail.com>
2020-12-08 14:32:24 -0300

    4. Testando o arquivo NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('4. Testando o arquivo NotFound.js', () => {
+  it('A página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+
+    const img = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(img).toBeInTheDocument();
+  });
+});
