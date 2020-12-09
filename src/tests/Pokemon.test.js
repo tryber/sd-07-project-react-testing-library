ac222286ed573543d5df60106c6b2ae9caa8923d
vitorAmorims <vtamorims@gmail.com>
2020-12-09 08:47:12 -0300

    Pokemon.test.js

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -58,21 +58,22 @@ describe('Teste se é renderizado um card com as informações de'
 });
 test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação'
 + 'para exibir detalhes deste Pokémon.'
-+ 'O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon exibido;', () => {
++ 'O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon'
++ 'exibido;', () => {
   const { container, getByText } = renderWithRouter(
     <Pokemon pokemon={ pokemon } />,
   );
   const link = getByText(/More details/);
   expect(link).toBeInTheDocument();
   const tagA = container.getElementsByTagName('a');
-  expect(tagA[0].getAttribute('href')).toBe('/pokemons/25')
+  expect(tagA[0].getAttribute('href')).toBe('/pokemons/25');
 });
 test('Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento'
 + 'da aplicação para a página de detalhes de Pokémon.', () => {
   const history = createMemoryHistory();
-  const { getByText, container } = render(
-    <Router history={history}>
-      <Pokemon pokemon={ pokemon } />
+  const { getByText } = render(
+    <Router history={ history }>
+      <Pokemon pokemon={ pokemon } />,
     </Router>
   );
   fireEvent.click(getByText(/More details/));
@@ -82,9 +83,9 @@ test('Teste se ao clicar no link de navegação do Pokémon, é feito o redireci
 test('Teste também se a URL exibida no navegador muda para /pokemon/<id>,'
 + 'onde <id> é o id do Pokémon cujos detalhes se deseja ver;', () => {
   const history = createMemoryHistory();
-  const { getByText, container } = render(
-    <Router history={history}>
-      <Pokemon pokemon={ pokemon } />
+  const { getByText } = render(
+    <Router history={ history }>
+      <Pokemon pokemon={ pokemon } />,
     </Router>
   );
   fireEvent.click(getByText(/More details/));
