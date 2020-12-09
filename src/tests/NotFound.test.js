5efa5ae82252413bf364f100181f212ceac8aa14
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-12-09 14:09:08 -0300

    Matando Mutante NotFound - 09/12/2020

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -7,15 +7,17 @@ afterEach(cleanup);
 
 describe('EX04 - Testando o arquivo NotFound.js', () => {
   test('heading h2 com o texto "Page requested not found 😭"', () => {
-    const { history, getByRole, getByAltText } = RenderWithRouter(<App />);
+    const { history, getByRole, getByAltText, getByText } = RenderWithRouter(<App />);
 
     history.push('/xablau');
 
     const h2Tag = getByRole('heading', { name: /Page requested/i });
+    const textNotFound = getByText(/not found/i);
     const img = getByAltText(/Pikachu crying/i);
 
     expect(h2Tag.tagName).toBe('H2');
     expect(h2Tag).toBeInTheDocument();
+    expect(textNotFound).toBeInTheDocument();
     expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
     expect(img).toBeInTheDocument();
   });
