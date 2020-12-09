3a8a9e3b0ad928b556df5e3914b07afafc91c4aa
moises <moisaant@gmail.com>
2020-12-09 12:36:12 -0300

    correcoes

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import pokemons from '../data'
+import pokemons from '../data';
 import PokemonDetails from '../components/PokemonDetails';
 import App from '../App';
 
 describe('Requirement 07', () => {
   test('1/3', () => {
-    renderWithRouter(<PokemonDetails match={ { params: { id: 143 } } } isPokemonFavoriteById={ false } pokemons={ pokemons } />);
+    renderWithRouter(
+      <PokemonDetails
+        match={ { params: { id: 143 } } }
+        isPokemonFavoriteById={ false }
+        pokemons={ pokemons }
+      />
+    );
 
     const titleSnorlaxDetails = screen.getByText(/snorlax details/i);
     const moreDetailsLink = screen.queryByText('More Details');
