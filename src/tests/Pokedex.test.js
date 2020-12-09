2d8332ba413283edeee4063f38980e82a555f774
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-12-08 22:49:19 -0300

    Zero to Hero: Requirement 5 (incomplete v0.2)

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -6,82 +6,101 @@ import Pokedex from '../components/Pokedex';
 afterEach(cleanup);
 
 describe('Requirement 5: Testing Pokedex.js', () => {
-  test.skip('if the text "Encountered pokémons" exists in a h2 tag', () => {
-    const fakeData = [
-      {
-        id: 143,
-        name: 'Snorlax',
-        type: 'Normal',
-        averageWeight: {
-          value: '460.0',
-          measurementUnit: 'kg',
-        },
-        image: 'https://cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png',
-        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
-        foundAt: [
-          {
-            location: 'Kanto Vermillion City',
-            map: 'https://cdn.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
-          },
-        ],
-        summary: 'It is hungry.',
+  const fakeData = [
+    {
+      id: 143,
+      name: 'Snorlax',
+      type: 'Normal',
+      averageWeight: {
+        value: '460.0',
+        measurementUnit: 'kg',
       },
-      {
-        id: 10,
-        name: 'Caterpie',
-        type: 'Bug',
-        averageWeight: {
-          value: '2.9',
-          measurementUnit: 'kg',
+      image: 'https://cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Vermillion City',
+          map: 'https://cdn.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
         },
-        image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
-        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
-        foundAt: [
-          {
-            location: 'Johto Route 30',
-            map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
-          },
-          {
-            location: 'Johto Route 31',
-            map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
-          },
-          {
-            location: 'Ilex Forest',
-            map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
-          },
-          {
-            location: 'Johto National Park',
-            map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
-          },
-        ],
-        summary: 'Well... It is a bug',
+      ],
+      summary: 'It is hungry.',
+    },
+    {
+      id: 10,
+      name: 'Caterpie',
+      type: 'Bug',
+      averageWeight: {
+        value: '2.9',
+        measurementUnit: 'kg',
       },
-      {
-        id: 151,
-        name: 'Mew',
-        type: 'Psychic',
-        averageWeight: {
-          value: '4.0',
-          measurementUnit: 'kg',
+      image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Johto Route 30',
+          map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
         },
-        image: 'https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png',
-        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
-        foundAt: [
-          {
-            location: 'Faraway Island',
-            map: 'https://cdn.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
-          },
-        ],
-        summary: 'It is kinda creepy',
+        {
+          location: 'Johto Route 31',
+          map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
+        },
+        {
+          location: 'Ilex Forest',
+          map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
+        },
+        {
+          location: 'Johto National Park',
+          map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
+        },
+      ],
+      summary: 'Well... It is a bug',
+    },
+    {
+      id: 151,
+      name: 'Mew',
+      type: 'Psychic',
+      averageWeight: {
+        value: '4.0',
+        measurementUnit: 'kg',
       },
-    ];
+      image: 'https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Faraway Island',
+          map: 'https://cdn.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
+        },
+      ],
+      summary: 'It is kinda creepy',
+    },
+    {
+      id: 65,
+      name: 'Alakazam',
+      type: 'Psychic',
+      averageWeight: {
+        value: '48.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Unova Accumula Town',
+          map: 'https://cdn.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png',
+        },
+      ],
+      summary: 'Closing both its eyes heightens all its other senses. This enables it to use its abilities to their extremes.',
+    },
+  ];
 
-    const fakeFavPokes = {
-      143: true,
-      10: false,
-      151: false,
-    };
+  const fakeFavPokes = {
+    143: true,
+    10: false,
+    151: false,
+    65: true,
+  };
 
+  test('if the text "Encountered pokémons" exists in a h2 tag', () => {
     const { getByText } = renderWithRouter(
       <Pokedex
         pokemons={ fakeData }
@@ -95,81 +114,6 @@ describe('Requirement 5: Testing Pokedex.js', () => {
 
   describe('When the button "Próximo pokémon" is clicked, goes to the next', () => {
     test('if the text in the button is "Próximo pokémon"', () => {
-      const fakeData = [
-        {
-          id: 143,
-          name: 'Snorlax',
-          type: 'Normal',
-          averageWeight: {
-            value: '460.0',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Kanto Vermillion City',
-              map: 'https://cdn.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
-            },
-          ],
-          summary: 'It is hungry.',
-        },
-        {
-          id: 10,
-          name: 'Caterpie',
-          type: 'Bug',
-          averageWeight: {
-            value: '2.9',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Johto Route 30',
-              map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
-            },
-            {
-              location: 'Johto Route 31',
-              map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
-            },
-            {
-              location: 'Ilex Forest',
-              map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
-            },
-            {
-              location: 'Johto National Park',
-              map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
-            },
-          ],
-          summary: 'Well... It is a bug',
-        },
-        {
-          id: 151,
-          name: 'Mew',
-          type: 'Psychic',
-          averageWeight: {
-            value: '4.0',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Faraway Island',
-              map: 'https://cdn.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
-            },
-          ],
-          summary: 'It is kinda creepy',
-        },
-      ];
-
-      const fakeFavPokes = {
-        143: true,
-        10: false,
-        151: false,
-      };
-
       const { getByText } = renderWithRouter(
         <Pokedex
           pokemons={ fakeData }
@@ -182,81 +126,6 @@ describe('Requirement 5: Testing Pokedex.js', () => {
     });
 
     test('if the next pokemon is shown', () => {
-      const fakeData = [
-        {
-          id: 143,
-          name: 'Snorlax',
-          type: 'Normal',
-          averageWeight: {
-            value: '460.0',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Kanto Vermillion City',
-              map: 'https://cdn.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
-            },
-          ],
-          summary: 'It is hungry.',
-        },
-        {
-          id: 10,
-          name: 'Caterpie',
-          type: 'Bug',
-          averageWeight: {
-            value: '2.9',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Johto Route 30',
-              map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
-            },
-            {
-              location: 'Johto Route 31',
-              map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
-            },
-            {
-              location: 'Ilex Forest',
-              map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
-            },
-            {
-              location: 'Johto National Park',
-              map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
-            },
-          ],
-          summary: 'Well... It is a bug',
-        },
-        {
-          id: 151,
-          name: 'Mew',
-          type: 'Psychic',
-          averageWeight: {
-            value: '4.0',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Faraway Island',
-              map: 'https://cdn.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
-            },
-          ],
-          summary: 'It is kinda creepy',
-        },
-      ];
-
-      const fakeFavPokes = {
-        143: true,
-        10: false,
-        151: false,
-      };
-
       const { getByText, getByTestId } = renderWithRouter(
         <Pokedex
           pokemons={ fakeData }
@@ -269,86 +138,10 @@ describe('Requirement 5: Testing Pokedex.js', () => {
 
       expect(pokemon).toHaveTextContent(fakeData[index].name);
       fireEvent.click(nextButton);
-      index += 1;
-      expect(pokemon).toHaveTextContent(fakeData[index].name);
+      expect(pokemon).toHaveTextContent(fakeData[index + 1].name);
     });
 
     test('if the first pokemon is shown after the last pokemon', () => {
-      const fakeData = [
-        {
-          id: 143,
-          name: 'Snorlax',
-          type: 'Normal',
-          averageWeight: {
-            value: '460.0',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Kanto Vermillion City',
-              map: 'https://cdn.bulbagarden.net/upload/5/54/Kanto_Vermilion_City_Map.png',
-            },
-          ],
-          summary: 'It is hungry.',
-        },
-        {
-          id: 10,
-          name: 'Caterpie',
-          type: 'Bug',
-          averageWeight: {
-            value: '2.9',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Johto Route 30',
-              map: 'https://cdn.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
-            },
-            {
-              location: 'Johto Route 31',
-              map: 'https://cdn.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
-            },
-            {
-              location: 'Ilex Forest',
-              map: 'https://cdn.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
-            },
-            {
-              location: 'Johto National Park',
-              map: 'https://cdn.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
-            },
-          ],
-          summary: 'Well... It is a bug',
-        },
-        {
-          id: 151,
-          name: 'Mew',
-          type: 'Psychic',
-          averageWeight: {
-            value: '4.0',
-            measurementUnit: 'kg',
-          },
-          image: 'https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png',
-          moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
-          foundAt: [
-            {
-              location: 'Faraway Island',
-              map: 'https://cdn.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
-            },
-          ],
-          summary: 'It is kinda creepy',
-        },
-      ];
-
-      const fakeFavPokes = {
-        143: true,
-        10: false,
-        151: false,
-      };
-
       const { getByText, getByTestId } = renderWithRouter(
         <Pokedex
           pokemons={ fakeData }
@@ -357,7 +150,7 @@ describe('Requirement 5: Testing Pokedex.js', () => {
       );
       const nextButton = getByText(/próximo pokémon/i);
 
-      for (const index = 0; index < fakeData.length; index += 1) {
+      for (let index = 0; index < fakeData.length; index += 1) {
         if (index === fakeData.length - 1) {
           fireEvent.click(nextButton);
           const pokemon = getByTestId('pokemon-name');
@@ -367,5 +160,38 @@ describe('Requirement 5: Testing Pokedex.js', () => {
         }
       }
     });
+  });
+
+  test('if only one pokemon is shown', () => {
+   const { getAllByTestId } = renderWithRouter(
+      <Pokedex
+        pokemons={ fakeData }
+        isPokemonFavoriteById={ fakeFavPokes }
+      />,
+    );
+
+    const pokemon = getAllByTestId('pokemon-name');
+    expect(pokemon.length).toBe(1);
+  });
+
+  describe('if the Pokedex has filter buttons', () => {
+    test('if all the pokemon have the same type after clicking', () => {
+      const { queryAllByTestId, getByRole } = renderWithRouter(
+        <Pokedex
+          pokemons={ fakeData }
+          isPokemonFavoriteById={ fakeFavPokes }
+        />,
+      );
+
+      const fakeType = 'Psychic';
+      const psychicButton = getByRole('button', { name: /psychic/i });
+      fireEvent.click(psychicButton);
+      const allPsychicPoke = queryAllByTestId('pokemonType');
+
+      const pokeType = allPsychicPoke.reduce(type => type.textContent === 'Psychics');
+      expect(pokeType).toHaveTextContent(fakeType);
+    });
+
+    // test('if the text in the button matches the type\'s name ', () => {});
   });
 });
