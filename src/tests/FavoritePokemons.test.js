b21c1d367bb61d0b202919478fb17e8974d238dd
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-12-10 15:44:31 -0300

    segundo requisito do teste FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import { fireEvent, cleanup } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
-// import App from '../App';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 test('if render `No favorite pokémon found` if favorite pokémons list is empty', () => {
@@ -9,54 +7,42 @@ test('if render `No favorite pokémon found` if favorite pokémons list is empty
   const notFound = getByText('No favorite pokemon found');
   expect(notFound).toBeInTheDocument();
 });
-/*
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText, history } = renderWithRouter(<App />);
-  const route = '/';
-  history.push(route);
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+
+test('if render all favorited pokemons', () => {
+  const fav = [
+    {
+      averageWeight: { measurementUnit: '', value: '' },
+      id: 1,
+      image: '',
+      name: '',
+      type: '',
+    },
+    {
+      averageWeight: { measurementUnit: '', value: '' },
+      id: 2,
+      image: '',
+      name: '',
+      type: '',
+    },
+    {
+      averageWeight: { measurementUnit: '', value: '' },
+      id: 3,
+      image: '',
+      name: '',
+      type: '',
+    },
+    {
+      averageWeight: { measurementUnit: '', value: '' },
+      id: 4,
+      image: '',
+      name: '',
+      type: '',
+    },
+  ];
+  const { getAllByAltText } = renderWithRouter(
+    <FavoritePokemons pokemons={ fav } />,
+  );
+  const expectedLength = 4;
+  const favoritePokemons = getAllByAltText(/is marked as favorite/i);
+  expect(favoritePokemons).toHaveLength(expectedLength);
 });
-
-describe('Verify if a nav bar exists in the main page.', () => {
-  afterEach(cleanup);
-  it('should exist in the document.', () => {
-    const { getByRole } = renderWithRouter(<App />);
-    const navBar = getByRole('navigation');
-    expect(navBar).toBeInTheDocument();
-  });
-  it('should have be three links inside, `Home`, `About` and `Favorite Pokémons`', () => {
-    const { getByRole } = renderWithRouter(<App />);
-    const navBar = getByRole('navigation');
-    const homeLink = navBar.firstElementChild;
-    const aboutLink = homeLink.nextElementSibling;
-    const favoritPokeLink = aboutLink.nextElementSibling;
-    expect(homeLink).toHaveTextContent('Home');
-    expect(aboutLink).toHaveTextContent('About');
-    expect(favoritPokeLink).toHaveTextContent('Favorite Pokémons');
-  });
-  it('should render `/` route when `Home` link is clicked.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    const homeLink = getByText(/Home/i);
-
-    fireEvent.click(homeLink);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
-  });
-  it('should render `/about` route when `About` link is clicked.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    const aboutLink = getByText(/About/i);
-
-    fireEvent.click(aboutLink);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
-  });
-  it('should render `/favorites` route when `Favorite Pokémons` link is clicked.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    const favoritePokeLink = getByText(/Favorite Pokémons/i);
-
-    fireEvent.click(favoritePokeLink);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/favorites');
-  });
-});
-*/
