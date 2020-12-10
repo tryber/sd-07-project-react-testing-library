719af41d8816c7ddf0e577652f1bf7ed9b9028b8
murilopereira006 <muriloedu006@gmail.com>
2020-12-09 22:34:26 -0300

    renderWithRouter e querySelector

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/dom';
-import renderWithRouter from '../components/renderWithRouter';
+import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import Router from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
 
 describe('testing About.js', () => {
+    
+    
     test('testing if the page has all infos about Pokedex', () => {
+    const { getByText, history } = renderWithRouter(<About />);
 
-    const history = createMemoryHistory();
-    const { getByText } = render(
-        <Router history={ history }>
-            <About />
-        </Router>
-    );
     const infosPokedex = getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i);
     expect(infosPokedex).toBeInTheDocument();
     });
 
-    test('', () => {
-        renderWithRouter(<About />);
-        const tag = document.querySelector('h2');
-        expect(tag.tagName).toBe('h2');
-        expect(tag.innerHTML).toBe('About Pokédex');
+    test('testing if About.js has a tag h2 with About Pokédex', () => {
+        const { container } = renderWithRouter(<About />);
+
+        const tag = container.querySelector('h2');
+        const tagConfirm = container.querySelector('h2').innerHTML;
+    expect(tagConfirm).toBe('About Pokédex');
     });
 });
