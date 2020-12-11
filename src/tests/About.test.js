5b1c24d6fdd06556e8a6699cfb951b4cd8f27b1d
murilopereira006 <muriloedu006@gmail.com>
2020-12-10 22:52:48 -0300

    req3 iniciado, deve ter formas melhoras de fazer estes testes...

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { getAllByAltText, render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom'
 import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
 
 describe('testing About.js', () => {
-    
-    
+
     test('testing if the page has all infos about Pokedex', () => {
-    const { getByText, history } = renderWithRouter(<About />);
+    const { getByText } = renderWithRouter(<About />);
 
     const infosPokedex = getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i);
     expect(infosPokedex).toBeInTheDocument();
     });
 
     test('testing if About.js has a tag h2 with About Pokédex', () => {
-        const { container, history } = renderWithRouter(<About />);
+        const { container } = renderWithRouter(<About />);
 
-        const tag = container.querySelector('h2');
-        const tagConfirm = container.querySelector('h2').innerHTML;
-        expect(tagConfirm).toBe('About Pokédex');
+        const tag = container.querySelector('h2').innerHTML;
+        expect(tag).toBe('About Pokédex');
     });
 
     test('verify if there is two paragraphs', () =>{
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, getByTitle, render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('testing About.js', () => {
+
+    test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
+        const history = createMemoryHistory();
+        const { getByText } = render(
+            <Router history={ history }>
+                <App />
+            </Router>,
+        );
+        fireEvent.click(getByText(/Favorite Pokémons/i));
+        const noFavoriteYet = getByText(/No favorite pokemon found/i);
+        expect(noFavoriteYet).toBeInTheDocument();
+    });
+
+    test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
+        const history = createMemoryHistory();
+        const { getByText } = render(
+            <Router history={ history }>
+                <App />
+            </Router>,
+        );
+        fireEvent.click(getByText(/Favorite Pokémons/i));
+        //clicar no pokemon
+        //clicar em favorito
+        //checar pokemons favoritos
+    });
+});
