9b95e57a32723d5353c78d68a25507e8a7f7d5e4
murilopereira006 <muriloedu006@gmail.com>
2020-12-10 21:30:14 -0300

    ESlint nao esta funcioando localmente :(

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { getAllByAltText, render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom'
 import About from '../components/About';
@@ -16,10 +16,26 @@ describe('testing About.js', () => {
     });
 
     test('testing if About.js has a tag h2 with About Pokédex', () => {
-        const { container } = renderWithRouter(<About />);
+        const { container, history } = renderWithRouter(<About />);
 
         const tag = container.querySelector('h2');
         const tagConfirm = container.querySelector('h2').innerHTML;
-    expect(tagConfirm).toBe('About Pokédex');
+        expect(tagConfirm).toBe('About Pokédex');
+    });
+
+    test('verify if there is two paragraphs', () =>{
+        const { getByTestId } = renderWithRouter(<About />);
+
+        const p1 = getByTestId(/paragraph1/);
+        const p2 = getByTestId(/paragraph2/);
+        expect(p1).toBeInTheDocument();
+        expect(p2).toBeInTheDocument();
+    });
+
+    test('test if /About has an especific image', () => {
+        const { getByAltText } = renderWithRouter(<About />);
+
+        const imageOfPokedex = getByAltText(/Pokédex/i);
+        expect(imageOfPokedex.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
     });
 });
