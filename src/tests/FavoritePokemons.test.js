e7219a2d73dd9e2e9b7a3650f5d441ea8d584467
Rita Jeveaux <ritajeveaux@gmail.com>
2020-12-11 10:18:49 -0300

    Requisito03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import data from '../data';
+
+describe('Testing the FavoritePokemons.js file', () => {
+  const poke = {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries.',
+  };
+  test('if the message `No favorite pokemon found` is displayed', () => {
+    const { getByText } = renderWithRouter(
+      <FavoritePokemons
+        pokemon={ poke }
+      />,
+    );
+    const text = getByText(/No favorite pokemon found/);
+    expect(text).toBeInTheDocument();
+  });
+  test('Test whether all favorite Pokémon cards are displayed', () => {
+    const favorite = ['Pikachu', 'Dragonair'];
+    const favoriteExpec = data.filter((pokemon) => favorite.includes(pokemon.name));
+    const favoriteExpecLength = 2;
+    expect(favoriteExpec.length).toBe(favoriteExpecLength);
+  });
+  test('Test if ** no ** Pokémon card is displayed, if it is not favored', () => {
+    const favoritePokemons = [];
+    const { queryByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ favoritePokemons } />,
+    );
+    const caterpie = queryByText('Caterpie');
+    expect(caterpie).toBeNull();
+  });
+});
