7049a6c895808426a0702afe465145986818b29d
murilopereira006 <muriloedu006@gmail.com>
2020-12-11 21:29:14 -0300

    refatorando req3

--- src/mocks/pokeMocks.js
+++ src/mocks/pokeMocks.js
@@ -4,22 +4,40 @@ const pokemons = [
     name: 'Pikachu',
     type: 'Electric',
     image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
   },
   {
     id: 10,
     name: 'Caterpie',
     type: 'Bug',
     image: 'https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png',
-  },{
+    averageWeight: {
+      value: '2.9',
+      measurementUnit: 'kg',
+    },
+  },
+  {
     id: 4,
     name: 'Charmander',
     type: 'Fire',
     image: 'https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
-  },{
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+  },
+  {
     id: 23,
     name: 'Ekans',
     type: 'Poison',
     image: 'https://cdn.bulbagarden.net/upload/1/18/Spr_5b_023.png',
+    averageWeight: {
+      value: '6.9',
+      measurementUnit: 'kg',
+    },
   },
 ];
 
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import { fireEvent, getByTitle, render } from '@testing-library/react';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import FavoritePokemons from '../components/FavoritePokemons';
-import favoritePokemons from '../mocks/pokeMocks';
+import favPoke from '../mocks/pokeMocks';
 import renderWithRouter from '../services/renderWithRouter';
 import { screen } from '@testing-library/react';
-import pokemons from '../data';
 import App from '../App';
 
 describe('testing About.js', () => {
 
     test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
-        const history = createMemoryHistory();
-        const { getByText } = render(
-            <Router history={ history }>
-                <App />
-            </Router>,
+        renderWithRouter(
+            <FavoritePokemons pokemons={ [] } />
         );
-        fireEvent.click(getByText(/Favorite Pokémons/i));
-        const noFavoriteYet = getByText(/No favorite pokemon found/i);
+        const noFavoriteYet = screen.getByText(/No favorite pokemon found/i);
         expect(noFavoriteYet).toBeInTheDocument();
     });
 
     test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
         renderWithRouter(
-            <FavoritePokemons history={ history } pokemons={ favoritePokemons } />,
-          );
-          favoritePokemons.forEach((favorite) => {
+            <FavoritePokemons history={ history } pokemons={ favPoke }/>,
+        );
+        favPoke.forEach((favorite) => {
             expect(screen.getByText(favorite.name)).toBeInTheDocument();
-          });
         });
-  });
+    });
+});
