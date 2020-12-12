beaf52f587ef838ff006b050b77ea10bcf5a1876
murilopereira006 <muriloedu006@gmail.com>
2020-12-11 21:58:09 -0300

    ESlint de novo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,34 +3,34 @@ import About from '../components/About';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('testing About.js', () => {
+  test('testing if the page has all infos about Pokedex', () => {
+  const { getByText } = renderWithRouter(<About />);
 
-    test('testing if the page has all infos about Pokedex', () => {
-    const { getByText } = renderWithRouter(<About />);
+  const infosPokedex = getByText
+    (/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i);
+  expect(infosPokedex).toBeInTheDocument();
+  });
 
-    const infosPokedex = getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i);
-    expect(infosPokedex).toBeInTheDocument();
-    });
+  test('testing if About.js has a tag h2 with About Pokédex', () => {
+    const { container } = renderWithRouter(<About />);
 
-    test('testing if About.js has a tag h2 with About Pokédex', () => {
-        const { container } = renderWithRouter(<About />);
+    const tag = container.querySelector('h2').innerHTML;
+    expect(tag).toBe('About Pokédex');
+  });
 
-        const tag = container.querySelector('h2').innerHTML;
-        expect(tag).toBe('About Pokédex');
-    });
+  test('verify if there is two paragraphs', () => {
+    const { getByTestId } = renderWithRouter(<About />);
 
-    test('verify if there is two paragraphs', () =>{
-        const { getByTestId } = renderWithRouter(<About />);
+    const p1 = getByTestId(/paragraph1/);
+    const p2 = getByTestId(/paragraph2/);
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
 
-        const p1 = getByTestId(/paragraph1/);
-        const p2 = getByTestId(/paragraph2/);
-        expect(p1).toBeInTheDocument();
-        expect(p2).toBeInTheDocument();
-    });
+  test('test if /About has an especific image', () => {
+    const { getByAltText } = renderWithRouter(<About />);
 
-    test('test if /About has an especific image', () => {
-        const { getByAltText } = renderWithRouter(<About />);
-
-        const imageOfPokedex = getByAltText(/Pokédex/i);
-        expect(imageOfPokedex.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-    });
+    const imageOfPokedex = getByAltText(/Pokédex/i);
+    expect(imageOfPokedex.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import favPoke from '../mocks/pokeMocks';
 import noFavPoke from '../mocks/noFavPokes';
@@ -6,30 +7,29 @@ import renderWithRouter from '../services/renderWithRouter';
 import { screen } from '@testing-library/react';
 
 describe('testing About.js', () => {
+  test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
+    renderWithRouter(
+      <FavoritePokemons pokemons={ [] } />,
+    );
+    const noFavoriteYet = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoriteYet).toBeInTheDocument();
+  });
 
-    test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
-        renderWithRouter(
-            <FavoritePokemons pokemons={ [] } />
-        );
-        const noFavoriteYet = screen.getByText(/No favorite pokemon found/i);
-        expect(noFavoriteYet).toBeInTheDocument();
+  test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
+    renderWithRouter(
+      <FavoritePokemons pokemons={ favPoke } />,
+    );
+    favPoke.forEach((favorite) => {
+      expect(screen.getByText(favorite.name)).toBeInTheDocument();
     });
+  });
 
-    test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
-        renderWithRouter(
-            <FavoritePokemons history={ history } pokemons={ favPoke }/>,
-        );
-        favPoke.forEach((favorite) => {
-            expect(screen.getByText(favorite.name)).toBeInTheDocument();
-        });
+  test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
+    renderWithRouter(
+      <FavoritePokemons pokemons={ favPoke } />,
+    );
+    noFavPoke.forEach((notFavorite) => {
+      expect(screen.queryByText(notFavorite.name)).not.toBeInTheDocument();
     });
-
-    test('test if for non pokemons favorited return "No favorite pokemon found"', () => {
-        renderWithRouter(
-            <FavoritePokemons history={ history } pokemons={ favPoke }/>,
-        );
-        noFavPoke.forEach((notFavorite) => {
-            expect(screen.queryByText(notFavorite.name)).not.toBeInTheDocument();
-          });
-    });
+  });
 });
