2dfff7494637618a754933f9244c18da3c942902
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-12 11:08:56 -0300

    Test 6 ready

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,9 +4,10 @@ import RenderWithRouter from './RenderWithRouter';
 import App from '../App';
 import Pokemon from '../components/Pokemon';
 import pokemons from '../data';
+import { fireEvent } from '@testing-library/react';
 
 describe('Test 6 - Pokemon.js', () => {
-  it('', () => {
+  it('Should to show all info about a pokemon', () => {
     const { getByTestId, getByRole } = RenderWithRouter(
       <App pokemons={ pokemons }>
         <Pokemon />
@@ -20,5 +21,22 @@ describe('Test 6 - Pokemon.js', () => {
     expect(pokemonType).toBeInTheDocument();
     expect(pokemonWeight).toBeInTheDocument();
     expect(image).toBeInTheDocument();
+  });
+  it('Should to contain a link with \'More details\'', () => {
+    const { getByRole, history } = RenderWithRouter(
+      <Pokemon pokemon={ pokemons[0] } />
+      );
+      const details = getByRole('link', { name: 'More details', src: `/pokemons/${pokemons[0].id}` });
+      expect(details).toBeInTheDocument();
+      fireEvent.click(details);
+      const { pathname } = history.location;
+      expect(pathname).toBe(`/pokemons/${pokemons[0].id}`);
+  });
+  it('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    const { getAllByRole } = RenderWithRouter(
+      <Pokemon pokemon={ pokemons[0] } isFavorite />,
+    );
+    const image = getAllByRole('img', { src: '/star-icon.svg' });
+    expect(image[1]).toBeInTheDocument();
   });
 });
