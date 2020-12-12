9e9cde070610140e3daf070f7edb64a55b725b50
murilopereira006 <muriloedu006@gmail.com>
2020-12-12 20:46:33 -0300

    req5 ok

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
-import { fireEvent, screen } from '@testing-library/react';
+import { fireEvent, getByText } from '@testing-library/react';
 import pokemons from '../data';
 
 describe('testing Pokedex.js', () => {
-  test('testing if the page has h2', () => {
+  test('test if the page has h2', () => {
     const { getByText } = renderWithRouter(<App />);
 
     const h2Tag = getByText(/Encountered pokémons/i);
@@ -13,7 +13,7 @@ describe('testing Pokedex.js', () => {
     expect(h2Tag.tagName).toBe('H2');
   });
 
-  test('testing if pokemons change clicking in the button', () => {
+  test('test if pokemons change clicking in the button', () => {
     const { getByText } = renderWithRouter(<App />);
 
     const pikachu = getByText(/Pikachu/i);
@@ -38,33 +38,97 @@ describe('testing Pokedex.js', () => {
 
     const Mew = getByText(/Mew/i);
     expect(Mew).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/i));
+
+    const Rapidash = getByText(/Rapidash/i);
+    expect(Rapidash).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/i));
+
+    const Snorlax = getByText(/Snorlax/i);
+    expect(Snorlax).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/i));
+
+    const Dragonair = getByText(/Dragonair/i);
+    expect(Dragonair).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/i));
+
+    const Pikachu = getByText(/Pikachu/i);
+    expect(Pikachu).toBeInTheDocument();
   });
 
   test('test if there is just 1 pokemon on the sceen', () => {
     const { getByText, container } = renderWithRouter(<App />);
 
     for(let index = 0; index < pokemons.length; index+=1) {
-      const imgOfPoke2 = container.querySelectorAll('img');
-      expect(imgOfPoke2.length.toString()).toBe('1');
+      const imgOfPoke = container.querySelectorAll('img');
+      expect(imgOfPoke.length.toString()).toBe('1');
       fireEvent.click(getByText(/Próximo pokémon/i));
     }
-    /*const imgOfPoke = container.querySelectorAll('img');
-    expect(imgOfPoke.length.toString()).toBe('1');
-
-    fireEvent.click(getByText(/Próximo pokémon/i));
-    const imgOfPoke2 = container.querySelectorAll('img');
-    expect(imgOfPoke2.length.toString()).toBe('1');
-
-    fireEvent.click(getByText(/Próximo pokémon/i));
-    const imgOfPoke3 = container.querySelectorAll('img');
-    expect(imgOfPoke3.length.toString()).toBe('1');*/
   });
 
-  test('', () => {});
+  test('test if there is filter buttons on Pokédex', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
 
-  test('', () => {});
+    const type = getByText('Psychic');
+    fireEvent.click(type);
+    const pokeType = getByTestId('pokemonType').innerHTML;
+    expect(pokeType).toBe('Psychic');
+  });
 
-  test('', () => {});
+  test('test if pokedex has filter buttons', () => {
+    const { getByText } = renderWithRouter(<App />);
 
-  test('', () => {});
+    const typeAll = getByText('All');
+    expect(typeAll).toBeInTheDocument();
+    fireEvent.click(typeAll);
+
+    const pikachu = getByText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/i));
+
+    const Charmander = getByText(/Charmander/i);
+    expect(Charmander).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/i));
+  });
+
+  test('test if type button is dinamic', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+
+    const filters = getAllByTestId('pokemon-type-button');
+    expect(filters.length.toString()).toBe('7');
+  });
+
+  test('test id porximo Pokemon button is desable', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const desableButton = getByText(/Próximo pokémon/i);
+    fireEvent.click(desableButton);
+
+    fireEvent.click(getByText(/Electric/i));
+    fireEvent.click(desableButton);
+    const pikachu = getByText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+
+    fireEvent.click(getByText(/All/i));
+
+    fireEvent.click(getByText(/Bug/i));
+    fireEvent.click(desableButton);
+    const Caterpie = getByText(/Caterpie/i);
+    expect(Caterpie).toBeInTheDocument();
+
+    fireEvent.click(getByText(/Poison/i));
+    fireEvent.click(desableButton);
+    const Ekans = getByText(/Ekans/i);
+    expect(Ekans).toBeInTheDocument();
+
+    fireEvent.click(getByText(/Normal/i));
+    fireEvent.click(desableButton);
+    const Snorlax = getByText(/Snorlax/i);
+    expect(Snorlax).toBeInTheDocument();
+
+    fireEvent.click(getByText(/Dragon/i));
+    fireEvent.click(desableButton);
+    const Dragonair = getByText(/Dragonair/i);
+    expect(Dragonair).toBeInTheDocument();
+  });
 });
