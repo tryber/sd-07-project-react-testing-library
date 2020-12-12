1ef6ee86c4328cf39a028d6c7facf9e8a7ecb135
Carol Andrade <a.carolg1@gmail.com>
2020-12-12 20:19:53 -0300

    requisito4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testing NotFound.js  - requirement4', () => {
+  it('Teste se página contém um h2 com o texto Page requested not found 😭', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Page requested not found 😭');
+    expect(heading.tagName).toBe('H2');
+  });
+
+  it('Teste se página mostra a imagem:', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const images = getAllByRole('img');
+    const matchImage = images.filter((image) => image.src === 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(matchImage.length).toBe(1);
+  });
+});
