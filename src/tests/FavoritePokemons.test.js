da48127309856c16c06c34334ecd463d275b3af3
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-12-12 14:39:48 -0300

    teste refatorado.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderWithRouter from './helpers/renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
 
 test('if render `No favorite pokémon found` if favorite pokémons list is empty', () => {
   const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
@@ -9,40 +10,18 @@ test('if render `No favorite pokémon found` if favorite pokémons list is empty
 });
 
 test('if render all favorited pokemons', () => {
-  const fav = [
-    {
-      averageWeight: { measurementUnit: '', value: '' },
-      id: 1,
-      image: '',
-      name: '',
-      type: '',
-    },
-    {
-      averageWeight: { measurementUnit: '', value: '' },
-      id: 2,
-      image: '',
-      name: '',
-      type: '',
-    },
-    {
-      averageWeight: { measurementUnit: '', value: '' },
-      id: 3,
-      image: '',
-      name: '',
-      type: '',
-    },
-    {
-      averageWeight: { measurementUnit: '', value: '' },
-      id: 4,
-      image: '',
-      name: '',
-      type: '',
-    },
-  ];
   const { getAllByAltText } = renderWithRouter(
-    <FavoritePokemons pokemons={ fav } />,
+    <FavoritePokemons pokemons={ pokemons } />,
   );
-  const expectedLength = 4;
+  const expectedLength = 9;
   const favoritePokemons = getAllByAltText(/is marked as favorite/i);
   expect(favoritePokemons).toHaveLength(expectedLength);
+});
+
+test('if nothing is rendered when pokemons is not favorited.', () => {
+  const { queryByAltText } = renderWithRouter(
+    <FavoritePokemons pokemons={ [] } />,
+  );
+  const favoritePokemons = queryByAltText(/is marked as favorite/i);
+  expect(favoritePokemons).toBeNull();
 });
