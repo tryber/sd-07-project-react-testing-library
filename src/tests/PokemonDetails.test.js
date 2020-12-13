d1a62e2cec9a3e2a497d306ef11842c0cc93e4a3
murilopereira006 <muriloedu006@gmail.com>
2020-12-13 00:34:16 -0300

    req7 parte 3

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,29 +1,41 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
 describe('test PokemonsDetails.js cards', () => {
   test('test if noma Details in on the screen', () => {
-    const { getByText } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/More details/i));
-    const nameDetail = getByText(/pikachu details/i);
+    renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/More details/i));
+    const nameDetail = screen.getByText(/pikachu details/i);
     expect(nameDetail).toBeInTheDocument();
+
+    const linkDetails = screen.queryByRole(/More details/i);
+    expect(linkDetails).not.toBeInTheDocument();
+
+  const heading = screen.getByText(/Summary/i);
+  expect(heading).toBeInTheDocument();
+
+  expect(screen.getByText(/This intelligent Pokémon roasts hard/i))
+    .toBeInTheDocument();
   });
 
   test('test if Game locations exist', () => {
-    const { getByText } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/More details/i));
-    const heading = getByText(/Game Locations of Pikachu/i);
+    renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/More details/i));
+    const heading = screen.getByText(/Game Locations of Pikachu/i);
     expect(heading).toBeInTheDocument();
   });
 
   test('name and map of localization', () => {
-    const { getAllByAltText, getByText } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/More details/i));
-    const map1Localization = getByText(/Kanto Viridian Forest/i);
-    const map2Localization = getByText(/Kanto Power Plant/i);
-    const mapsImages = getAllByAltText(/pikachu location/i);
+    renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText(/More details/i));
+    const map1Localization = screen.getByText(/Kanto Viridian Forest/i);
+    const map2Localization = screen.getByText(/Kanto Power Plant/i);
+    const mapsImages = screen.getAllByAltText(/pikachu location/i);
     expect(map1Localization && map2Localization).toBeInTheDocument();
     expect(mapsImages[0] && mapsImages[1]).toBeInTheDocument();
     expect(mapsImages[0].src).toBe('https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
@@ -31,16 +43,17 @@ describe('test PokemonsDetails.js cards', () => {
   });
 
   it('Test if you con favorite by Detaisl page', () => {
-    const { getByRole, getByLabelText } = renderWithRouter(<App />);
-    const details = getByRole('link', { name: 'More details' });
+    renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: 'More details' });
     fireEvent.click(details);
-    const checkbox = getByRole('checkbox');
+    const checkbox = screen.getByRole('checkbox');
     expect(checkbox).toBeInTheDocument();
     fireEvent.click(checkbox);
     expect(checkbox.checked).toBeTruthy();
     fireEvent.click(checkbox);
     expect(checkbox.checked).toBeFalsy();
-    const favorite = getByLabelText(/Pokémon favoritado?/i);
+    const favorite = screen.getByLabelText(/Pokémon favoritado?/i);
     expect(favorite).toBeInTheDocument();
   });
 });
