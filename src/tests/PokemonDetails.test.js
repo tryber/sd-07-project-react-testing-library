c1400f4251b46e8407ebfe49fe1417346a3748e1
murilopereira006 <muriloedu006@gmail.com>
2020-12-13 00:07:31 -0300

    req7 primeira tentativa

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,7 +1,33 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+
 describe('test PokemonsDetails.js cards', () => {
-  test('', () => {});
+  test('test if noma Details in on the screen', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i))
+    const nameDetail = getByText(/pikachu details/i);
+    expect(nameDetail).toBeInTheDocument();
+  });
 
-  test('', () => {});
+  test('test if Game locations exist', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i))
+    const heading = getByText(/Game Locations of Pikachu/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-  test('', () => {});
+  test('name and map of localization', () => {
+    const { getAllByAltText, getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i))
+    const map1Localization = getByText(/Kanto Viridian Forest/i);
+    const map2Localization = getByText(/Kanto Power Plant/i);
+    const mapsImages = getAllByAltText(/pikachu location/i);
+    expect(map1Localization && map2Localization).toBeInTheDocument();
+    expect(mapsImages[0] && mapsImages[1]).toBeInTheDocument();
+    expect(mapsImages[0].src).toBe('https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(mapsImages[1].src).toBe('https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
 });
