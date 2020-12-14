114434c4c95768318a2254500fc39e0dffca97fa
sttefanieMaciel <macielsttefanie@gmail.com>
2020-12-14 19:21:56 -0300

    corrigindo erros

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -23,4 +23,4 @@ describe('Testando o arquivo About.js', () => {
     const { getByRole } = RenderWithRouter(<About />);
     const image = getByRole('img');
     expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-  });
+  })
