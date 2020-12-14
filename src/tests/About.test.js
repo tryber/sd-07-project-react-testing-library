50ee8bb73c970bf5c85c85de2bf9de4d5615c050
sttefanieMaciel <macielsttefanie@gmail.com>
2020-12-14 19:08:37 -0300

    Primeiro e segundo req

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('Testando o arquivo About.js', () => {
+  test('A página contém as informações sobre a Pokédex.', () => {
+    const { getByText } = RenderWithRouter(<About />);
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+  test('A página contém um heading com o texto "About Pokédex."', () => {
+    const { getByRole } = RenderWithRouter(<About />);
+    const heading = getByRole('heading');
+    expect(heading.innerHTML).toBe('About Pokédex');
+  });
+  test('A página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getByText } = RenderWithRouter(<About />);
+    const text1 = getByText(/this application simulates a pokédex/i);
+    const text2 = getByText(/one can filter pokémons by type/i);
+    expect(text1).toBeInTheDocument();
+    expect(text2).toBeInTheDocument();
+  });
+  test('A página contém imagem de uma Pokédex.', () => {
+    const { getByRole } = RenderWithRouter(<About />);
+    const image = getByRole('img');
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import RenderWithRouter from './RenderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/']} >
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+describe('Testando o arquivo App.js', () => {
+  test('Pokédex é renderizada na rota `/`', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    expect(getByText(/Encountered pokémons/i)).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  test('', () => {
+    const { getByText } = RenderWithRouter(<App />);
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+  test('Ao clicar em "Home" é redirecionada para rora "/"', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  test('Ao clicar em "About" é redirecionada para rora "/about"', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  test('Ao clicar em "Favorite Pokémons" é redirecionada para rora "/favorites"', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 });
