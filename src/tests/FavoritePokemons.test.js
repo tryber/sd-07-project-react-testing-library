5f717336293f7c0030184362efae5bb7f511be39
Francisco <franciscoliratorres@gmail.com>
2020-12-14 11:05:29 -0300

    Add até pokedex

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 
 describe('testing about component', () => {
   test('if it contains information about `Pokédex`', () => {
@@ -10,31 +11,24 @@ describe('testing about component', () => {
     expect(noFavorite).toBeInTheDocument();
   });
 
-  test('if it contains information about `Pokédex`', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />);
-
-    const noFavorite = getByText(/No favorite pokemon found/i);
-    expect(noFavorite).toBeInTheDocument();
+  test('if it shows all favorite pokemons`', () => {
+    const favoritePok = [pokemons[0], pokemons[2]];
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ favoritePok } />);
+    favoritePok.forEach((favorite) => {
+      const pokName = getByText(favorite.name);
+      expect(pokName).toBeInTheDocument();
+    });
   });
-  // test('if it contains a heading', () => {
-  //   const { container } = renderWithRouter(<About />);
 
-  //   const title = container.querySelectorAll('h2');
-  //   expect(title.length).toBe(1);
-  // });
-
-  // test('if it contains two paragraphs', () => {
-  //   const { container } = renderWithRouter(<About />);
-
-  //   const paragraphs = container.querySelectorAll('p');
-  //   const numberOfParagraphs = 2;
-  //   expect(paragraphs.length).toBe(numberOfParagraphs);
-  // });
-
-  // test('if hs the correct URL', () => {
-  //   const { getByRole } = renderWithRouter(<About />);
-
-  //   const image = getByRole('img');
-  //   expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-  // });
+  test('if it doesnt show not favorite pokemons`', () => {
+    const favoritePok = [pokemons[0], pokemons[2]];
+    const notFavoritePok = [pokemons[1], pokemons[3]];
+    const { queryByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ favoritePok } />,
+    );
+    notFavoritePok.forEach((notFavorite) => {
+      const pokName = queryByText(notFavorite.name);
+      expect(pokName).not.toBeInTheDocument();
+    });
+  });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,56 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('testing Pokedex component', () => {
+  test('if it contains a h2 heading with the text Encountered pokémons', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />,
+    );
+
+    const title = getByText(/Encountered pokémons/i);
+    expect(title).toBeInTheDocument();
+  });
+
+  test('if it contains a heading', () => {
+    const { container } = renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />,
+    );
+    const title = container.querySelectorAll('h2');
+    expect(title.length).toBe(1);
+  });
+
+  test('if shows only one pokemon at time', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />,
+    );
+    const dataTestId = getAllByTestId('pokemon-name');
+    expect(dataTestId.length).toBe(1);
+  });
+
+  test('if has filter buttons', () => {
+    const { getAllByTestId, getByTestId } = renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />,
+    );
+    const dataTestId = getAllByTestId('pokemon-type-button');
+    const numberOfButtons = 7;
+    expect(dataTestId.length).toBe(numberOfButtons);
+
+    const dataTestIdTypeOf = getByTestId('pokemonType');
+    dataTestId.forEach((type) => {
+      fireEvent.click(type);
+      expect(dataTestIdTypeOf.textContent).toBe(type.textContent);
+    });
+  });
+
+  test('if has a button to reset the filters', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />,
+    );
+    const buttonContent = getByText(/All/i);
+    expect(buttonContent).toBeInTheDocument();
+    fireEvent.click(buttonContent);
+  });
+});
