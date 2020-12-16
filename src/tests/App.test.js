85d2bc97c93dd18a7f51995c81b2d24c2d3bdf33
Samanta Below <samanta.below@gmail.com>
2020-12-16 13:30:30 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testando o arquivo App.js', () => {
+  it('o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favoritePokemons = getByText('Favorite Pokémons');
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  })
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+  it('ao clicar no link Home da barra de navegação, redireciona pra página /home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    fireEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
+  it('ao clicar no link About da barra de navegação, redireciona pra página /about', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText('About');
+    fireEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  })
+
+  it('ao clicar no link Favorite Pokémons da barra de navegação, redireciona pra página /favorites', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoritePokemons = getByText('Favorite Pokémons');
+    fireEvent.click(favoritePokemons);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  })
+
+  it('ao entrar em uma URL desconhecida, redireciona pra página /not-found', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+    history.push('/not-found');
+    const noMatch = getByAltText('Pikachu crying because the page requested was not found');
+    expect(noMatch).toBeInTheDocument();
+  })
+})
+
