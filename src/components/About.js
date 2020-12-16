a41fdd9056689b307d9349e9a0dbf3259aec2434
vladimirrchagas <vladimirrchagas@gmail.com>
2020-12-16 16:18:16 -0300

    requisito 2

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ import './about.css';
 const About = () => (
   <section>
     <h2>{`About ` + `Pokédex`}</h2>
-    <section>
-      <p>
+    <section data-testid="pokedex-info">
+      <p data-testid="pokedex-content">
         This application simulates a Pokédex, a
         digital encliclopedia containing all Pokémons
       </p>
-      <p>One can filter Pokémons by type, and see more details for each one of them</p>
+      <p data-testid="pokedex-content">One can filter Pokémons by type, and see more details for each one of them</p>
       <img
         className="pokedex-image"
         src={`https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`}
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('If page have Pokedéx information', () => {
+  const { getByTestId } = render(<About />);
+  expect(getByTestId('pokedex-info')).toBeInTheDocument();
+});
+
+test('If page have a heading with text About Pokédex', () => {
+  const { getByRole } = render(<About />);
+  expect(getByRole('heading').textContent).toBe('About Pokédex');
+});
+
+test('If have two paragraphs with Pokédex content', () => {
+  const length = 2;
+  const { getAllByTestId } = render(<About />);
+  expect(getAllByTestId('pokedex-content').length).toBe(length);
+});
+
+test('If page have an image defined', () => {
+  const imgMockPath = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const { getByRole } = render(<About />);
+  expect(getByRole('img').src).toBe(imgMockPath);
+});
