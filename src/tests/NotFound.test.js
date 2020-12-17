0609b894d020d0f6ff86490e7d948370bb0bb40b
vladimirrchagas <vladimirrchagas@gmail.com>
2020-12-17 16:20:27 -0300

    mais uma tentativa

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import NotFound from '../components/NotFound';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
 
 test('If have text Page requested not found 😭 on the page', () => {
-  // const { getByRole } = render(<NotFound />);
-  // expect(getByRole('heading').textContent).toBe('Page requested not found 😭');
+  const { getByRole } = renderWithRouter(<NotFound />);
+  expect(getByRole('heading').textContent).toBe('Page requested not found 😭');
 });
-// scr test using cotainer
-// test('If have image according to requested', () => {
-//   const { container } = render(<NotFound />);
-//   const images = container.getElementsByTagName('img');
-//   expect(images[0].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
-// });
+
+test('If have image according to requested', () => {
+  const { container } = renderWithRouter(<NotFound />);
+  const images = container.getElementsByTagName('img');
+  expect(images[0].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
