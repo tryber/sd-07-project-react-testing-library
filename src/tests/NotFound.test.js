252d0b59f8e7a23ec1028dec27811e46520f6bd0
vladimirrchagas <vladimirrchagas@gmail.com>
2020-12-18 09:20:15 -0300

    tentando novamente solucionar requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import NotFound from '../components/NotFound';
-import renderWithRouter from '../renderWithRouter';
+import { render } from '@testing-library/react';
 
 test('If have text Page requested not found 😭 on the page', () => {
-  const { getByRole } = renderWithRouter(<NotFound />);
-  expect(getByRole('heading').textContent).toBe('Page requested not found 😭');
+  const { getByRole } = render(<NotFound />);
+  expect(getByRole('heading').textContent).toBe('Page requested not found 😭 ');
 });
 
 test('If have image according to requested', () => {
-  const { container } = renderWithRouter(<NotFound />);
+  const { container } = render(<NotFound />); 
   const images = container.getElementsByTagName('img');
   expect(images[0].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
