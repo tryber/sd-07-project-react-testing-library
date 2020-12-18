99972ad6ac689283c263ed5f09ab46fd54e320be
sttefanieMaciel <macielsttefanie@gmail.com>
2020-12-18 09:53:23 -0300

    corrigindo erros

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,11 +5,11 @@ import App from '../App';
 import RenderWithRouter from './RenderWithRouter';
 
 describe('Testando o arquivo FavoritePokemons.js', () => {
-  test('Se não há favoritos, exibe mensagem "No favorite pokemon found"', () =>{
+  test('Se não há favoritos, exibe mensagem "No favorite pokemon found"', () => {
     const { getByText } = RenderWithRouter(<FavoritePokemons />);
     expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
-test('é exibido todos os cards de pokémons favoritados.', () => {
+  test('é exibido todos os cards de pokémons favoritados.', () => {
     const { getByText } = RenderWithRouter(<App />);
     fireEvent.click(getByText(/More details/i));
     fireEvent.click(getByText(/pokémon favoritado?/i));
