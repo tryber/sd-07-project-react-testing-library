2cceae187cda714a5d3e6f524cc5376c533ca66e
jvitorof <j.vitoroflores@gmail.com>
2020-12-17 22:29:13 -0300

    Project resumed :rocket:

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import renderWithRouter from '../renderWhithRouter';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWhithRouter';
 import { About } from '../components';
 
 describe('testing About.js. Checks if:', () => {
   test('the page contains the informations about Pokedéx', () => {
     const { history } = renderWithRouter(<About />);
     history.push('/about');
-    const pathname = history.location.pathname;
+    const { pathname } = history.location;
     expect(pathname).toBe('/about');
   });
 
-  test("the page has a 'h2' heading with About Pokédex text", () => {
+  test('the page has a "h2" heading with About Pokédex text', () => {
     const { container, getByText, getByRole } = render(<About />);
     const heading = getByRole(/heading/i);
     expect(heading).toBeInTheDocument();
@@ -22,20 +22,21 @@ describe('testing About.js. Checks if:', () => {
   });
 
   test('the pages contains two paragraphs', () => {
+    const paragraphs = 2;
     const { container } = render(<About />);
     const tagsP = container.querySelectorAll('p');
-    expect(tagsP).toHaveLength(2);
+    expect(tagsP).toHaveLength(paragraphs);
   });
 
   test('the page has image informations', () => {
-    const { container, getByText, getByRole } = render(<About />);
+    const { container, getByRole } = render(<About />);
     const img = getByRole(/img/i);
     expect(img).toBeInTheDocument();
     const tagImg = container.querySelectorAll('img');
     expect(tagImg).toHaveLength(1);
     const imgSrc = container.querySelector('img').src;
     expect(imgSrc).toBe(
-      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
     );
   });
 });
