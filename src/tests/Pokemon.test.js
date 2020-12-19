f02c6b35424b44d3caac2fa6af8ef2ac334adc22
Lorena Góes <goesloren@gmail.com>
2020-12-19 17:30:14 -0300

    Pokemon.test file completed

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -63,4 +63,66 @@ describe('Testing Pokemon.js file:', () => {
         });
       });
     });
+  describe('Testing Pokemon card', () => {
+    it(`Must contain a navigation link to show details of this pokemon.
+    Link must have the url /pokemons/<id>`, () => {
+      renderWithRouter(<App />);
+
+      pokemons.forEach((pokemon) => {
+        const pokemonLink = screen.getByRole('link', { name: 'More details' });
+        expect(pokemonLink).toBeInTheDocument();
+        // Linha a seguir adaptada a partir da solução feita por Gabriel Deori:
+        expect(pokemonLink.pathname).toBe(`/pokemons/${pokemon.id}`);
+
+        const nextBtn = screen.getByTestId('next-pokemon');
+        fireEvent.click(nextBtn);
+      });
+    });
+  });
+
+  describe('Testing link redirection', () => {
+    it('Must be working correctly', () => {
+      const { history } = renderWithRouter(<App />);
+
+      const pokemonLink = screen.getByRole('link', { name: 'More details' });
+      const home = screen.getByText('Encountered pokémons');
+      expect(home).toBeInTheDocument();
+
+      fireEvent.click(pokemonLink);
+      const path = history.location.pathname;
+      expect(path).not.toBe(home);
+    });
+    it(`Must navigate to pokemon details page. Navigator
+    URL must be /pokemon/<id>`, () => {
+      const { history } = renderWithRouter(<App />);
+
+      const pokemonLink = screen.getByRole('link', { name: 'More details' });
+      const home = screen.getByText('Encountered pokémons');
+      expect(home).toBeInTheDocument();
+
+      fireEvent.click(pokemonLink);
+      const path = history.location.pathname;
+      expect(path).toBe(`/pokemons/${pokemons[0].id}`);
+    });
+  });
+  describe('Testing star icon on favorite pokemons', () => {
+    it(`Must have a image with the src attribute "/star-icon.svg" and
+    the alt attribute "<pokemon> is marked as favorite"`,
+    () => {
+      renderWithRouter(<App />);
+
+      const pokemonLink = screen.getByRole('link', { name: 'More details' });
+      fireEvent.click(pokemonLink);
+
+      let selectFavoritePokemon = screen.getByRole('checkbox', { checked: false });
+      expect(selectFavoritePokemon).toBeInTheDocument();
+      fireEvent.click(selectFavoritePokemon);
+      selectFavoritePokemon = screen.getByRole('checkbox', { checked: true });
+      expect(selectFavoritePokemon).toBeInTheDocument();
+
+      const starIcon = screen.getByAltText('Pikachu is marked as favorite');
+      expect(starIcon).toBeInTheDocument();
+      expect(starIcon.src).toMatch('/star-icon.svg');
+    });
+  });
 });
