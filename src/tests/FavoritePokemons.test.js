6087e7c8e8ac97f6e12614de3d20eec517840ed5
Filipi Firmino <contatoic3web@gmail.com>
2020-12-21 18:35:42 -0300

    Testing favorite pokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, getByLabelText } from '@testing-library/dom';
+import data from '../data';
+import App from '../App';
+import renderWhitRouter from '../RenderWhitRouter';
+
+describe('Testing favorite pokemons', () => {
+  it('Testing if the message No favorite pokemon found is displayed', () => {
+    const { getByText } = renderWhitRouter(<App />);
+    const favoritePokemons = getByText('/Favorite Pokemons/i');
+    fireEvent.click(favoritePokemons);
+
+    const noFavorite = getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  it('Test whether all favorite Pokémon cards are displayed ', () => {
+    const {
+      queryByText,
+      getByText,
+      getAllByTestId,
+      getByTestId,
+    } = renderWhitRouter(<App />);
+
+    const home = getByText(/Home/i);
+    fireEvent.click(home);
+
+    const buttons = getAllByTestId('pokemon-type-button');
+    const butonNumber = 1;
+    fireEvent.click(buttons[butonNumber]);
+
+    const moreDetail = getByText(/More details/i);
+    fireEvent(moreDetail);
+
+    const checkBox = getByLabelText('Pokemon favoritado?');
+    fireEvent.click(checkBox);
+
+    const pokemon = getByTestId('pokemon-name').textContent;
+
+    const favoreitePokemon = getByText(/Favorite Pokémons/i);
+    fireEvent.click(favoreitePokemon);
+
+    expect(getByText(pokemon)).toBeInTheDocument();
+
+    const otherPokemons = data.filter(
+      (pokemons) => pokemons.name !== pokemon,
+    );
+
+    otherPokemons.forEach((actual) => {
+      expect(queryByText(actual.name)).not.toBeInTheDocument();
+    });
+  });
+});
