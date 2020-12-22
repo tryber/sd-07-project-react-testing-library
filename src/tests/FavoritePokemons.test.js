b5556b8562a1d6c9381a6ba5667b8db77717b6a3
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-12-22 16:34:10 -0300

    Commit 0.1.7

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
 
 describe('Teste se a aplicação é redirecionada'
 + 'para a página de Pokémons Favoritados, na URL'
@@ -16,5 +18,38 @@ describe('Teste se a aplicação é redirecionada'
     expect(pathname).toBe('/favorites');
 
     expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+});
+
+describe('Testando o arquivo FavoritePokemons.js', () => {
+  it('Teste se é exibido na tela a mensagem No favorite'
+  + 'pokemon found, se a pessoa não tiver pokémons favoritos', () => {
+    const { getByText } = render(<FavoritePokemons />);
+
+    const favoriteText = getByText('No favorite pokemon found');
+
+    expect(favoriteText).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0], pokemons[1]] } />);
+
+    const { getByText } = render(<FavoritePokemons />);
+
+    const pokemonNameOne = getByText('Pikachu');
+    const pokemonNameTwo = getByText('Charmander');
+
+    expect(pokemonNameOne).toBeInTheDocument();
+    expect(pokemonNameTwo).toBeInTheDocument();
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0]] } />);
+
+    const { queryByText } = render(<FavoritePokemons />);
+
+    const pokemonNotFavoite = queryByText('Dragonair');
+
+    expect(pokemonNotFavoite).not.toBeInTheDocument();
   });
 });
