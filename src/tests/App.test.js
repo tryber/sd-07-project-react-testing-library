a67bc79e6c163607a6613b9be9b32c22b63a8d7c
Lucas Nonato <lucas.nonato1@hotmail.com>
2020-12-23 17:28:29 -0300

    iniciando projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,15 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
