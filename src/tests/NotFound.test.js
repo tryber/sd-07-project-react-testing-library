3eb2c98f5b8d9e9dfa08c906c309a8b7de10a158
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-12-24 17:57:59 -0300

    Requirement 4 - Testing NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Tests the elements of the NotFound.js component', () => {
+  it('tests the messege `Page requested not found`', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const title = getByText(/Page requested not found/);
+    expect(title).toBeInTheDocument();
+  });
+
+  it('tests whether the page shows the Pikachu image', () => {
+    const { container } = renderWithRouter(<NotFound />);
+    const image = container.querySelector('img');
+
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
