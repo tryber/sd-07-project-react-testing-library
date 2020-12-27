57f2c3a7b897627245ffb90e3f129e2430374227
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-12-27 15:07:49 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from '../renderWithRouter';
+
+test('', () => {
+
+  //     Teste se é exibido o próximo Pokémon da lista quando o botão Próximo pokémon é clicado.
+
+  // O botão deve conter o texto Próximo pokémon;
+
+  // Os próximos Pokémons da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão.
+
+  // O primeiro Pokémon da lista deve ser mostrado ao clicar no botão se estiver no último Pokémon da lista.
+
+});
