ed28a7200e188b66840fb8aefd2ac6098ad8efc2
Filipi Firmino <38562909+ic3web@users.noreply.github.com>
2020-12-28 11:14:15 -0300

    Fix error eslint

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import renderWhitRouter from '../RenderWhitRouter';
-import {NotFound} from '../components'
+import { NotFound } from '../components';
 
+describe('Testing not found page', () => {
+  test('Displays a "page request not found" text', () => {
+    const { getByText, getByRole } = renderWhitRouter(<NotFound />);
+    const heading = getByRole('heading');
+    expect(heading).toBeInTheDocument();
 
-describe ('Testing not found page', () => {
-    test ('Displays a "page request not found" text', () => {
-        const {getByText, getByRole} = renderWhitRouter(<NotFound/>);
-        const heading = getByRole('heading');
-        expect(heading).toBeInTheDocument();
+    const mesage = getByText(/Page requested not found/i);
+    expect(mesage).toBeInTheDocument();
+  });
 
-        const mesage = getByText(/Page requested not found/i);
-        expect(mesage).toBeInTheDocument();
-    });
+  test('Testing show picture in the page', () => {
+    const { getByRole, container } = renderWhitRouter(<NotFound />);
+    const image = getByRole('img',
+      { name: /Pikachu crying because the page requested was not found/i });
+    expect(image).toBeInTheDocument();
 
-    test('Testing show picture in the page', () => {
-        const {getByRole, container} = renderWhitRouter(<NotFound />);
-        const image = getByRole('img', { name:/Pikachu crying because the page requested was not found/i});
-        expect(image).toBeInTheDocument();
-
-        const ImageSrc = container.querySelector('.not-found-image').src;
-        expect(ImageSrc).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
-    });
+    const ImageSrc = container.querySelector('.not-found-image').src;
+    expect(ImageSrc).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
 });
