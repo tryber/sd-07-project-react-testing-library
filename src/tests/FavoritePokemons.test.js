a388792a028ba147498fc654222602706aead3bd
Alstent <rafaelasltent@gmail.com>
2020-12-31 00:49:43 +0100

    resolvendo code climate

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -15,16 +15,16 @@ describe('FavoritePokemons.js', () => {
   test('renders all favorite Pokemons', () => {
     const history = createMemoryHistory();
     const { getByText } = render(
-      <Router history={history}>
+      <Router history={ history }>
         <App />
-      </Router>
+      </Router>,
     );
-    
+
     fireEvent.click(getByText('More details'));
     fireEvent.click(getByText('Pokémon favoritado?'));
     fireEvent.click(getByText('Favorite Pokémons'));
 
-    const pokemons = document.getElementsByClassName('favorite-pokemons').length
+    const pokemons = document.getElementsByClassName('favorite-pokemons').length;
     const size = 1;
 
     expect(pokemons).toBe(size);
@@ -33,7 +33,7 @@ describe('FavoritePokemons.js', () => {
   test('renders no cards, if there is none favorite Pokemons', () => {
     render(<FavoritePokemons />);
 
-    const pokemons = document.getElementsByClassName('favorite-pokemons').length
+    const pokemons = document.getElementsByClassName('favorite-pokemons').length;
     const size = 0;
 
     expect(pokemons).toBe(size);
