4fd6b19ae05788e195c758f89e517ce1070aa074
Renato Silva <renatosdev@gmail.com>
2020-12-29 22:39:05 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -18,15 +18,16 @@ test('renders a reading with the text `Pokédex`', () => {
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries = { ['/'] } >
       <App />
     </MemoryRouter>,
   );
 
-   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
-test('check if at the top of the page you have the "Home, About, Favorite Polémons link"', () => {
+test('check if at the top of the page you have the "Home, About, Favorite Polémons link"',
+() => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -39,8 +40,8 @@ test('check if at the top of the page you have the "Home, About, Favorite Polém
 });
 
 test('navigate to home', () => {
-  const history = createMemoryHistory(); 
-  const { getByText } = render(
+  const history = createMemoryHistory();
+  const { getByText }=render(
     <Router history = { history }>
       <App />
     </Router>,
@@ -48,13 +49,13 @@ test('navigate to home', () => {
 
   expect(getByText('Home')).toBeInTheDocument();
   fireEvent.click(getByText(/Home/i));
-  const { pathname } = history.location
+  const { pathname } = history.location;
   expect(pathname).toBe('/')
 });
 
 test('navigate to About', () => {
-  const history = createMemoryHistory(); 
-  const { getByText } = render(
+  const history = createMemoryHistory();
+  const { getByText }=render(
     <Router history = { history }>
       <App />
     </Router>,
@@ -62,13 +63,13 @@ test('navigate to About', () => {
 
   expect(getByText('About')).toBeInTheDocument();
   fireEvent.click(getByText(/About/i));
-  const { pathname } = history.location
+  const { pathname } = history.location;
   expect(pathname).toBe('/about')
 });
 
 test('navigate to Favorite Pokémons', () => {
-  const history = createMemoryHistory(); 
-  const { getByText } = render(
+  const history = createMemoryHistory();
+  const { getByText }=render(
     <Router history = { history }>
       <App />
     </Router>,
@@ -76,13 +77,13 @@ test('navigate to Favorite Pokémons', () => {
 
   expect(getByText('Favorite Pokémons')).toBeInTheDocument();
   fireEvent.click(getByText(/Favorite Pokémons/i));
-  const { pathname } = history.location
-  expect(pathname).toBe('/favorites')
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
 });
 
 test('navigate to Not Found', () => {
-  const history = createMemoryHistory(); 
-  const { getByText } = render(
+  const history = createMemoryHistory();
+  const { getByText }=render(
     <Router history = { history }>
       <App />
     </Router>,
@@ -91,5 +92,4 @@ test('navigate to Not Found', () => {
   const route = '/teste';
   history.push(route);
   expect(getByText('Page requested not found')).toBeInTheDocument();
-  
 });
