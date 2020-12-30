8b2af444e1edc86fc6b6b6ffcd74bfa4e706bbb7
Alstent <rafaelasltent@gmail.com>
2020-12-31 00:12:40 +0100

    resolvendo code climate

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import About from '../components/About';
-import renderWithRouter from './renderWithRouter.js';
+import { render } from '@testing-library/react';
 
 describe('Testing About.js', () => {
   test('renders with the about page`s content', () => {
-    const { getByText } = renderWithRouter(<About />);
+    const { getByText } = render(<About />);
 
     expect(getByText(/This application simulates a Pokédex,/i)).toBeInTheDocument();
   });
 
   test('renders a h2 with the text `About Pokédex`', () => {
-    const { getByText } = renderWithRouter(<About />);
+    const { getByText } = render(<About />);
 
     expect(getByText('About Pokédex')).toBeInTheDocument();
   });
 
   test('renders 2 `p`', () => {
-    renderWithRouter(<About />);
+    render(<About />);
 
     const p = document.getElementsByTagName('p').length;
-    const size = 2
-    
+    const size = 2;
+
     expect(p).toBe(size);
   });
 
   test('renders a specific img', () => {
-    renderWithRouter(<About />);
+    render(<About />);
 
     const imgSrc = document.querySelector('img').src;
     const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
