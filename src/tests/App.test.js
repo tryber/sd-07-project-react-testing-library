f3fd54667a65f3aaf7c722f5e0810dc175648e06
marcos <marcos.1998226@gmail.com>
2020-12-31 15:38:56 -0300

    1-testa o caminho de URL /

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import {render} from '@testing-library/react';
-import {Router} from 'react-router-dom';
-import {createMemoryHistory} from 'history';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-    const history = createMemoryHistory();
-    const {getByText} = render (
-        <Router history={history}><App/></Router>,
-    );
-    const heading = getByText(/Pokédex/i);
-    expect(heading).toBeInTheDocument();
+  const history = createMemoryHistory();
+  const { getByText } = render(<Router history={ history }><App/></Router>,);
+  const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
 });
 
 test('shows thePokedex when the route is `/`', () => {
-    const history = createMemoryHistory();
-    const {getByText} = render (
-        <Router history={history}><App/></Router>,
-    );
-
-    const {pathname} = history.location;
-    expect(pathname).toBe('/');
-    const info = getByText(/Encountered pokémons/i);
-    expect(info).toBeInTheDocument();
+  const history = createMemoryHistory();
+  const { getByText } = render(<Router history={ history }><App/></Router>,);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+  const info = getByText(/Encountered pokémons/i);
+  expect(info).toBeInTheDocument();
 });
