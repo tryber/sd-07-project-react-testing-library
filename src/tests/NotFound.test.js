d3471958bf4e94473b212cc135d70fec40fdb3a2
Alstent <rafaelasltent@gmail.com>
2020-12-31 02:16:33 +0100

    resolvendo requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testing NotFound.js', () => {
+  test('renders h2 with `Page requested not found 😭`', () => {
+    const { getByText } = render(<NotFound />);
+
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
+
+  test('renders a specific img', () => {
+    render(<NotFound />);
+
+    const imgSrc = document.querySelector('img').src;
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imgSrc).toBe(src);
+  });
+});
