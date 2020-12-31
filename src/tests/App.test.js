a76fac747e560cf01cc4e387b9cc7054e2063d3a
marcos <marcos.1998226@gmail.com>
2020-12-31 16:29:40 -0300

    3-testa o caminho de URL /

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const history = createMemoryHistory();
-  const { getByText } = render(<Router history={ history }><App /></Router>);
+  const { getByText } = render(<MemoryRouter><App /></MemoryRouter>);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
 
-test('shows thePokedex when the route is `/`', () => {
+test('testa a url para home "/"', () => {
   const history = createMemoryHistory();
-  const { getByText } = render(<Router history={ history }><App /></Router>);
+  const { getByText } = render(<MemoryRouter history={ history }><App /></MemoryRouter>);
   const { pathname } = history.location;
   expect(pathname).toBe('/');
   const info = getByText(/Encountered pokémons/i);
