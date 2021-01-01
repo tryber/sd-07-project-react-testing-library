be8d70d49bd9cc16adb14d93f7e000e460893e58
marcos <marcos.1998226@gmail.com>
2021-01-01 16:21:21 -0300

    1-Pokemons details

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -29,26 +29,26 @@ describe('Teste se as informações detalhadas do Pokémon.', () => {
     fireEvent.click(getByText(/More details/i));
     const tagP = document.getElementsByTagName('p');
     expect(tagP[3].innerHTML)
-    .toBe('This intelligent Pokémon roasts hard berries with electricity '
+      .toBe('This intelligent Pokémon roasts hard berries with electricity '
     + 'to make them tender enough to eat.');
-    });
+  });
 });
 describe('teste seção com os mapas contendo as localizações do pokémon', () => {
-    it('deverá existir um heading h2 com o texto Game Locations of pokemon', () => {
-     const {getByText, getByRole} = renderWithRouter(<App />);
-     fireEvent.click(getByText(/More details/i));
-     const tagH2 = getByRole('heading', { name: /Game Locations/i });
-     expect(tagH2).toBeInTheDocument();
-     expect(tagH2.tagName).toBe('H2');
-});
-    it('Todas as localizações do Pokémon devem ser mostradas na seção de detalhes', () => {
+  it('deverá existir um heading h2 com o texto Game Locations of pokemon', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    const tagH2 = getByRole('heading', { name: /Game Locations/i });
+    expect(tagH2).toBeInTheDocument();
+    expect(tagH2.tagName).toBe('H2');
+  });
+  it('Todas as localizações do Pokémon devem ser mostradas na seção', () => {
     const { getByText } = renderWithRouter(<App />);
     fireEvent.click(getByText(/More details/i));
     const tagH2 = getByText(`Game Locations of ${pokemons[0].name}`);
     expect(tagH2.parentNode.tagName).toBe('SECTION');
-});
+  });
   it('Testa a localização e uma imagem do mapa em cada localização', () => {
-    const { getByText, getByRole} = renderWithRouter(<App />);
+    const { getByText, getByRole } = renderWithRouter(<App />);
     fireEvent.click(getByText(/More details/i));
     const tagH2 = getByRole('heading', { name: /Game Locations/i });
     const PokeDwell = tagH2.nextElementSibling;
@@ -62,8 +62,8 @@ describe('teste seção com os mapas contendo as localizações do pokémon', ()
       expect(img).toBeInTheDocument();
       expect(p).toBeInTheDocument();
     });
-});
-it('A imagem da localização deve ter um atributo src com a URL da localização', () => {
+  });
+  it('A imagem da localização deve ter um atributo src com a URL da localização', () => {
     const { getByText, getByRole } = renderWithRouter(<App />);
     fireEvent.click(getByText(/More details/i));
     const h2 = getByRole('heading', { name: /Game Locations/i });
@@ -74,8 +74,8 @@ it('A imagem da localização deve ter um atributo src com a URL da localizaçã
       expect(img.src).toBe(pokemons[0].foundAt[index].map);
     });
   });
-  it('A imagem da localização deve ter um atributo alt com o texto location do Pokémon', () => {
-   const { getByText, getByRole } = renderWithRouter(<App />);
+  it('Imagem da localização deve ter atributo alt com o texto location', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
     fireEvent.click(getByText(/More details/i));
     const h2 = getByRole('heading', { name: /Game Locations/i });
     const pokemonHabitat = h2.nextElementSibling;
@@ -88,23 +88,23 @@ it('A imagem da localização deve ter um atributo src com a URL da localizaçã
 });
 
 describe('Teste se o usuário pode favoritar um pokémon', () => {
-    it('A página deve exibir um checkbox que permite favoritar o Pokémon', () => {
-      const { getByText, getByRole } = renderWithRouter(<App />);
-      fireEvent.click(getByText(/More details/i));
-      const checkbox = getByRole('checkbox');
-      expect(checkbox).toBeInTheDocument();
-      expect(checkbox.type).toBe('checkbox');
-    });
-    it('Cliques alternados no checkbox devem adicionar e remover', () => {
-      const {getByAltText, getByText, getByRole } = renderWithRouter(<App />);
-      fireEvent.click(getByText(/More details/i));
-      const checkbox = getByRole('checkbox');
-      checkbox.checked && fireEvent.click(checkbox);
-      expect(checkbox.checked).toBeFalsy();
-      fireEvent.click(checkbox);
-      expect(checkbox.checked).toBeTruthy();
-      const star = getByAltText(`${pokemons[0].name} is marked as favorite`);
-      expect(star).toBeInTheDocument();
+  it('A página deve exibir um checkbox que permite favoritar o Pokémon', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    const checkbox = getByRole('checkbox');
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox.type).toBe('checkbox');
+  });
+  it('Cliques alternados no checkbox devem adicionar e remover', () => {
+    const { getByAltText, getByText, getByRole } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    const checkbox = getByRole('checkbox');
+    checkbox.checked && fireEvent.click(checkbox);
+    expect(checkbox.checked).toBeFalsy();
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toBeTruthy();
+    const star = getByAltText(`${pokemons[0].name} is marked as favorite`);
+    expect(star).toBeInTheDocument();
   });
   it('O label do checkbox deve conter o texto Pokémon favoritado?', () => {
     const { getByText, getByLabelText } = renderWithRouter(<App />);
