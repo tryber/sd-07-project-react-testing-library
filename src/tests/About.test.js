ae4b9a5c05ada2789bbeb360758beaf8b75baa59
Renato Silva <renatosdev@gmail.com>
2021-01-01 17:38:31 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { render, cleanup } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import About from '../components/About';
 import RenderWithRouter from './RenderWithRouter';
 
@@ -11,19 +9,14 @@ test('if the page contains information about Pokédex', () => {
   const { getByText } = RenderWithRouter(<About />);
   expect(
     getByText(
-      'This application simulates a Pokédex,' 
+      'This application simulates a Pokédex,'
       + 'a digital encliclopedia containing all Pokémons',
     ),
   ).toBeInTheDocument();
 });
 
 test('if the page contains an h2 heading with the text About Pokédex', () => {
-  const history = createMemoryHistory();
-  const { container } = render(
-    <Router history={ history }>
-      <About />,
-    </Router>
-  );
+  const { container } = RenderWithRouter(<About />);
   const h2 = container.querySelector('h2');
   expect(h2).toBeInTheDocument();
   // expect(getByText('About Pokédex').tagName).toBe('H2');
@@ -31,32 +24,22 @@ test('if the page contains an h2 heading with the text About Pokédex', () => {
 });
 
 test('if the page contains two paragraphs with text about Pokédex', () => {
-  const history = createMemoryHistory();
-  const { getByText } = render(
-    <Router history={ history }>
-      <About />,
-    </Router>
-  );
+  const { getByText } = RenderWithRouter(<About />);
   expect(
     getByText(
-      'This application simulates a Pokédex,' 
+      'This application simulates a Pokédex,'
       + 'a digital encliclopedia containing all Pokémons',
     ),
   ).toBeInTheDocument();
   expect(
     getByText(
       /One can filter Pokémons by type, and see more details for each one of them/i,
-    )
+    ),
   ).toBeInTheDocument();
 });
 
 test('if the page contains the following image of a Pokédex', () => {
-  const history = createMemoryHistory();
-  const { container } = render(
-    <Router history={ history }>
-      <About />,
-    </Router>
-  );
+  const { container } = RenderWithRouter(<About />);
   const img = container.querySelector('img');
   expect(img).toHaveAttribute('alt', 'Pokédex');
 });
