200a029d3fb58be07b9566d7cdfb814338fcbfe1
Bruno Nogueira <nugobruno@gmail.com>
2021-01-02 20:12:44 -0300

    Requirement 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -7,8 +7,8 @@ import pokemons from '../data';
 afterEach(cleanup);
 
 describe('fifth requirement', () => {
-  // Inspirado no projeto de Alexandre Faustino
-  const favoritePokemonList = {
+  // Referência: Alexandre Faustino
+  const isFavorite = {
     4: false,
     10: false,
     23: true,
@@ -18,68 +18,92 @@ describe('fifth requirement', () => {
     143: false,
     148: false,
     151: true,
-  };
-  it('should render an heading H2 with the text `Encountered pokémons`', () => {
-    const { getByText } = TestingRouter(
+  }
+  it('should render heading with the text Encountered pokémons', () => {
+    const { getByRole } = TestingRouter (
       <Pokedex
         pokemons={ pokemons }
-        isPokemonFavoriteById={ favoritePokemonList }
+        isPokemonFavoriteById={ isFavorite }
       />,
-    );
-    const pokedexText = getByText(/encountered pokémons/i);
-    expect(pokedexText).toBeInTheDocument();
+    )
+    const heading = getByRole('heading');
+    expect(heading).toBeInTheDocument();
   });
-
-  it('should render the next pokémon when press next button', () => {
-    // Inspirado em Alexandre Faustino
-    const { getByTestId, getByText } = TestingRouter(
+  it('should render render the next pokémon when clicking next buttom', () => {
+    const { getByTestId } = TestingRouter (
       <Pokedex
         pokemons={ pokemons }
-        isPokemonFavoriteById={ favoritePokemonList }
+        isPokemonFavoriteById={ isFavorite }
       />,
     );
     const nextBtn = getByTestId('next-pokemon');
+    const pokemonName = getByTestId('pokemon-name');
     expect(nextBtn.innerHTML).toBe('Próximo pokémon');
 
+    pokemons.forEach((pokemon) => {
+      expect(pokemonName.innerHTML).toBe(pokemon.name);
+      fireEvent.click(nextBtn);
+    })
+    expect(pokemonName.innerHTML).toBe('Pikachu');
+  });
+  it('should render just one pokémon at time', () => {
+    const { getAllByTestId } = TestingRouter (
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ isFavorite }
+      />,
+    );
+    const pokemonName = getAllByTestId('pokemon-name');
+    expect(pokemonName.length).toBe(1);
+  });
+  it('should render filter buttons', () => {
+    const { getAllByTestId, getByTestId } = TestingRouter (
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ isFavorite }
+      />,
+    );
+    const nextBtn = getByTestId('next-pokemon');
+    const pokemonType = getByTestId('pokemonType');
+    const filterBtns = getAllByTestId('pokemon-type-button');
+    filterBtns.forEach((type) => expect(type).toBeInTheDocument());
+    fireEvent.click(filterBtns[1]);
+    expect(pokemonType.innerHTML).toBe('Fire');
+    fireEvent.click(nextBtn);
+    expect(pokemonType.innerHTML).toBe('Fire');
+  });
+  it('should render reset button', () => {
+    // Referência: Alexandre Faustino
+    const { getByTestId, getByText, getAllByRole } = TestingRouter (
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ isFavorite }
+      />,
+    );
+    const nextBtn = getByTestId('next-pokemon');
     pokemons.forEach((pokemon) => {
       expect(getByText(pokemon.name)).toBeInTheDocument();
       fireEvent.click(nextBtn);
     });
-    expect(getByText(pokemons[0].name)).toBeInTheDocument();
+    const buttons = getAllByRole('button');
+    const allBtn = buttons[0];
+    expect(allBtn.innerHTML).toBe('All');
+    fireEvent.click(allBtn);
+    pokemons.forEach((pokemon) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(nextBtn);
+    });
   });
-
-  it('should render just one Pokémon at time', () => {
-    const { getAllByTestId } = TestingRouter(
+  it('shouldnt render next button if theres just one pokémon', () => {
+    const { getByText, getAllByRole } = TestingRouter (
       <Pokedex
         pokemons={ pokemons }
-        isPokemonFavoriteById={ favoritePokemonList }
+        isPokemonFavoriteById={ isFavorite }
       />,
     );
-    const pokemon = getAllByTestId('pokemon-name');
-    expect(pokemon.length).toBe(1);
-  });
-
-  it('should render filter buttons', () => {
-    const { getAllByTestId, getByText, getByTestId } = TestingRouter(
-      <Pokedex
-        pokemons={ pokemons }
-        isPokemonFavoriteById={ favoritePokemonList }
-      />,
-    );
-    const eletricBtn = (getAllByTestId('pokemon-type-button'))[0];
-    fireEvent.click(eletricBtn);
-    expect(getByText(/pikachu/i)).toBeInTheDocument();
-    expect(getByTestId('pokemonType').textContent).toBe('Electric');
-  });
-
-  it('should render All button', () => {
-    const { getByText } = TestingRouter(
-      <Pokedex
-        pokemons={ pokemons }
-        isPokemonFavoriteById={ favoritePokemonList }
-      />,
-    );
-    const allBtn = getByText(/all/i);
-    expect(allBtn).toBeInTheDocument();
+    const buttons = getAllByRole('button');
+    const nextPokemon = getByText(/próximo pokémon/i);
+    fireEvent.click(buttons[1]);
+    expect(nextPokemon.disabled).toBe(true);
   });
 });
