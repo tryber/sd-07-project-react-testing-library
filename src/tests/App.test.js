bd4df2a6ed4c755b425bf65270ae8e62964e0a37
Márcio Barroso <barrrosomarcio@gmail.com>
2021-01-02 19:22:51 -0300

    cc req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,7 +4,6 @@ import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('App.js Tests`', () => {
-
   it('renders a reading with the text `Pokédex`', () => {
     const { getByText } = renderWithRouter(<App />);
 
@@ -12,50 +11,48 @@ describe('App.js Tests`', () => {
     expect(heading).toBeInTheDocument();
   });
 
-  it('checks if the home have Home, About and Favorite links',() => {
+  it('checks if Home, About and Favorite links are rendered', () => {
     const { getByText } = renderWithRouter(<App />);
     const home = getByText(/Home/i);
     const about = getByText(/About/i);
     const favorite = getByText(/Favorite Pokémons/i);
-    
     expect(home).toBeInTheDocument();
     expect(about).toBeInTheDocument();
     expect(favorite).toBeInTheDocument();
-  })
+  });
 
-  it('Checks if by clicking in the "Home" link, the page is redirected to the url "/"', () => {
+  it('Checks if clicking "Home" link, redirectes to the url "/"', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    
     const home = getByText('Home');
     fireEvent.click(home);
-    const pathname = history.location.pathname;
+    const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
 
-  it('Checks if by clicking in the "About" link, the page is redirected to the url "/about"', () => {
+  it('Checks if clicking "About" link, redirects to the url "/about"', () => {
     const { getByText, history } = renderWithRouter(<App />);
 
     const about = getByText(/About/i);
     fireEvent.click(about);
-    const pathname = history.location.pathname;
+    const { pathname } = history.location;
     expect(pathname).toBe('/about');
   });
 
-  it('Checks if by clicking in the "Favorite Pokémons" link, the page is redirected to the url "/favorites"', () => {
+  it('Checks if clicking "Favorite Pokémons" link, redirects to the url "/favorites"', () => {
     const { getByText, history } = renderWithRouter(<App />);
 
     const favorites = getByText(/Favorite Pokémons/i);
     fireEvent.click(favorites);
-    const pathname = history.location.pathname;
+    const { pathname } = history.location;
     expect(pathname).toBe('/favorites');
   });
 
-  it('Checks if by chosing a unespecified pathname the page is redirected to NotFound', () => {
+  it('Checks if chosing a unespecified pathname redirects to NotFound',() => {
     const { getByAltText, history } = renderWithRouter(<App />);
     history.push('/marcio');
-    const notFound = getByAltText('Pikachu crying because the page requested was not found');
-    
+    const notFound = getByAltText(
+      'Pikachu crying because the page requested was not found'
+      ); 
     expect(notFound).toBeInTheDocument();
   });
-
 });
