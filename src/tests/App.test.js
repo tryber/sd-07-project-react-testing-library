60210b2b11741154155474929b88c7fa70884a7d
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-03 07:23:12 -0300

    App.js 1 e 2 teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+describe('Verificando o App.js', () => {
+  it('Verifica se mostra a PokeDex se a rota é /', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+  it('Verifica se a Pokedex possui os links de redirecionamento', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const about = getByText('About');
+    const home = getByText('Home');
+    const favPoke = getByText('Favorite Pokémons');
+    expect(about).toHaveTextContent('About');
+    expect(home).toHaveTextContent('Home');
+    expect(favPoke).toHaveTextContent('Favorite Pokémons');
+  });
 });
