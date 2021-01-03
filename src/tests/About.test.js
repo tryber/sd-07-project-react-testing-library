ea25a4de68b822fc71dc791cdb354b78e12d15c6
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-03 19:20:31 -0300

    testing if componet has a h2 header with About Pokedex text

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+
+describe('testing About.js component', () => {
+  test('page should contain pokédex info', () => {
+    const { getByText } = render(<About />);
+    expect(getByText('About Pokédex')).toBeInTheDocument();
+  });
+
+  it('should contain a h2 header with About Pokédex text', () => {
+    const { getByRole } = render(<About />);
+    expect(getByRole('heading', { level: 2 })).toBeInTheDocument();
+  });
+});
+
