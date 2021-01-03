ab59b237da1eec5f24d4c7685facb9ef47a9b2e7
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-03 20:34:28 -0300

    testing if page shows a not found message

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testing NotFound.js component', () => {
+  it('should contain a heading with Page requested not found text', () => {
+    const { getByText } = render(<NotFound />);
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+    expect(getByText('😭')).toBeInTheDocument();
+  });
+});
