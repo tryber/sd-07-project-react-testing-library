decd849bfd36b485b35033d2a64627c6f7b6f950
Anderson Santana <anderslash@gmail.com>
2021-01-03 15:15:43 -0300

    Implementaçao do RenderWithRouter no About.test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,41 +1,29 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
 
 describe('Testing About Pokedéx page', () => {
   it('contains info about the Pokédex', () => {
     const { getByText } = renderWithRouter(<About />);
-    const info = getByText(/This application simulates a Pokédex/i);
-    expect(info).toBeInTheDocument();
+    const expectedText = getByText(/This application simulates a Pokédex/i);
+    expect(expectedText).toBeInTheDocument();
   });
   it('contains a heading h2 with the text About Pokédex', () => {
-    const { getByRole } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>,
-    );
+    const { getByRole } = renderWithRouter(<About />);
+    const expectedText = /About Pokédex/i;
     const heading = getByRole('heading');
-    expect(heading).toHaveTextContent(/About Pokédex/i);
+    expect(heading).toHaveTextContent(expectedText);
   });
   it('contains two paragraphs with text about the Pokédex', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>,
-    );
-    const paragraph = container.getElementsByTagName('p');
+    const { container } = renderWithRouter(<About />);
     const expectedParagraphs = 2;
+    const paragraph = container.getElementsByTagName('p');
     expect(paragraph).toHaveLength(expectedParagraphs);
   });
   it('displays an image of a Pokédex', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>,
-    );
+    const { container } = renderWithRouter(<About />);
+    const expectedImageUrl = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const image = container.querySelector('img');
-    expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toHaveAttribute('src', expectedImageUrl);
   });
 });
