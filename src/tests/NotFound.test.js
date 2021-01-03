63cdbcc8e37182f562d07e5d86d2b3496032e217
Márcio Barroso <barrrosomarcio@gmail.com>
2021-01-02 21:28:00 -0300

    req notFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('NotFound.js Tests`', () => {
+  it('Checks if an H2 is rendered with a message', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/Marcio');
+    const heading = document.querySelector('h2');
+    const inner = 'Page requested not found<span role="img" aria-label="Crying emoji"> 😭</span>';
+    expect(heading).toBeInTheDocument();
+    expect(heading.innerHTML).toBe(inner);
+  });
+
+  it('Checks if a especific IMG is rendered', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+    history.push('/Marcio');
+    const img = getByAltText('Pikachu crying because the page requested was not found')
+    const source = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(source);
+  });
+});
