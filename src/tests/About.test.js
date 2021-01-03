698d48af8289a3bd218c61f876b2476867edc781
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-03 19:35:48 -0300

    lint for 2nd requirement

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import About from '../components/About';
 
-
 describe('testing About.js component', () => {
   test('page should contain pokédex info', () => {
     const { getByText } = render(<About />);
@@ -16,12 +15,12 @@ describe('testing About.js component', () => {
 
   it('should contain two paragraphs with Pokedex text', () => {
     const { getByText } = render(<About />);
-    const firstParagraph =
-    'This application simulates a Pokédex, a digital encliclopedia containing all Pokémons';
-    const secondParagraph =
-    'One can filter Pokémons by type, and see more details for each one of them';
-    expect(getByText(firstParagraph)).toBeInTheDocument();
-    expect(getByText(secondParagraph)).toBeInTheDocument();
+    const firstPPart1 = 'This application simulates a Pokédex,';
+    const firstPPart2 = ' a digital encliclopedia containing all Pokémons';
+    const secondPPart1 = 'One can filter Pokémons by type,'
+    const secondPPart2 = ' and see more details for each one of them';
+    expect(getByText(firstPPart1 + firstPPart2)).toBeInTheDocument();
+    expect(getByText(secondPPart1 + secondPPart2)).toBeInTheDocument();
   });
 
   it('should contain an image', () => {
@@ -30,4 +29,3 @@ describe('testing About.js component', () => {
     expect(img).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
-
