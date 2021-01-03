c378b8eb1b2abd686e110f0437ccf106659f4b77
CleberFontinele <cleberfontinele2387@gmail.com>
2021-01-03 18:24:08 -0300

    Testando o arquivo About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testando arquivo about', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    const message = getByText(/About Pokédex/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto /About Pokédex/', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading');
+    expect(heading.innerHTML).toBe('About Pokédex');
+  });
+});
