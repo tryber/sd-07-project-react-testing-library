0287a1fa19549fcc09f4683430d2520ab6caf35c
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-01-03 18:12:41 -0300

    lint requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -17,14 +17,15 @@ describe('Testando o arquivo About.js', () => {
 
   it('Testa se a página contêm dois parágrafos com texto sobre a Pokédex', () => {
     const { container } = render(<About />);
-    
-    expect(container.querySelectorAll('p').length).toBe(2);
+    const numberOfParagraphs = 2;
+
+    expect(container.querySelectorAll('p').length).toBe(numberOfParagraphs);
   });
 
   it('testa se a página contém a imagem de uma pokédex', () => {
     const { getByRole } = render(<About />);
     const image = getByRole('img');
-    
+
     expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
