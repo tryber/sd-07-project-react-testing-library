902267cf8fc9a307d26380422b1435f7cc82dcf2
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-03 15:42:29 -0300

    Trazendo codigo da documentaçao para verificar renderizaçao de uma rota

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,24 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { createMemoryHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router-dom'
+
+import '@testing-library/jest-dom/extend-expect'
+
+import { App, LocationDisplay } from './app'
+
+test('full app rendering/navigating', () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  )
+  // verify page content for expected route
+  // often you'd use a data-testid or role query, but this is also possible
+  expect(screen.getByText(/you are home/i)).toBeInTheDocument()
