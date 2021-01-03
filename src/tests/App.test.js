cf7d81833018b4d6afeff024195372b96927ef03
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-03 17:09:13 -0300

    Correçao final do teste da rota "/"

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { MemoryRouter, Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { screen } from '@testing-library/dom';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -15,15 +13,15 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('full app rendering/navigating', () => {
-  const history = createMemoryHistory();
-  render(
-    <Router history={ history }>
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
-    </Router>,
+    </MemoryRouter>,
   );
-  // verify page content for expected route
-  // often you'd use a data-testid or role query, but this is also possible
-  expect(screen.getByText(/Encountered/i)).toBeInTheDocument();
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
-// adaptado a partir de https://testing-library.com/docs/example-react-router/
+
+// teste extraído de https://github.com/tryber/sd-07-project-react-testing-library/blob/ff0583e67652b58b80b2a5932d67279018fcc1ab/src/tests/App.test.js
+// sobre MemoryRouter e initialEntries : https://reactrouter.com/web/api/MemoryRouter/initialentries-array
