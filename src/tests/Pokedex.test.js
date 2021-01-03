ee54438bf26d932e9775c90b76bfbecfea6863cf
Anderson Santana <anderslash@gmail.com>
2021-01-03 16:08:36 -0300

    Primeira parte do teste do Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Pokedéx component is rendering correctly', () => {
+  it('page displays a heading containing the text "Encountered pokémons"', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const expectedText = /Encountered pokémons/i;
+    expect(getAllByRole('heading')[1]).toHaveTextContent(expectedText);
+  });
+  it('renders only one pokémon in the page', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    expect(getAllByTestId('pokemon-name').length).toBe(1);
+  });
+});
+
+describe('Next pokemon is rendered when clicked in next button', () => {
+  it('button text is "Próximo pokémon', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const expectedButtonText = /Próximo pokémon/i;
+    const button = getByTestId('next-pokemon');
+    expect(button).toHaveTextContent(expectedButtonText);
+  });
+  it('if clicked in the button, the next pokemon is shown', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    const button = getByTestId('next-pokemon');
+    pokemons.forEach((pokemon) => {
+      const actualPokemon = getByText(pokemon.name);
+      fireEvent.click(button);
+      expect(actualPokemon).toBeInTheDocument();
+    });
+  });
+});
