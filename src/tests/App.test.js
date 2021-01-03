53576078b7013032cafa87beb69d052031d8b0c3
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-03 19:02:04 -0300

    lint for 1st requirement

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter';
 import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent, getByTestId } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -38,27 +38,27 @@ test('nav bar contains a certain group of links', () => {
 test('redirects to Home when clicking Home link in navbar', () => {
   const { getByText, history } = renderWithRouter(<App />);
   fireEvent.click(getByText('Home'));
-  const pathname = history.location.pathname;
+  const { pathname } = history.location;
   expect(pathname).toBe('/');
 });
 
 test('redirects to About page when clicking About link in navbar', () => {
   const { getByText, history } = renderWithRouter(<App />);
   fireEvent.click(getByText('About'));
-  const pathname = history.location.pathname;
+  const { pathname } = history.location;
   expect(pathname).toBe('/about');
 });
 
 test('redirects to favorites page when clicking Favorite Pokémons link in navbar', () => {
   const { getByText, history } = renderWithRouter(<App />);
   fireEvent.click(getByText('Favorite Pokémons'));
-  const pathname = history.location.pathname;
+  const { pathname } = history.location;
   expect(pathname).toBe('/favorites');
 });
 
 test('redirects to Not Found page when entering an invalid path', () => {
   const { getByText, history } = renderWithRouter(<App />);
-  history.push('/invalid-url')
-  const invalidPathname = getByText(`Page requested ` + `not found`);
-  expect(invalidPathname).toBeInTheDocument;
+  history.push('/invalid-url');
+  const invalidPathname = getByText('Page requested not found');
+  expect(invalidPathname).toBeInTheDocument();
 });
