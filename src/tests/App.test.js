e2fd5c78b9f018b0320b6e1793cebf62e5eb0ebc
Andre <dehhmenezes87@gmail.com>
2021-01-03 13:56:18 -0300

    Testando o arquivo App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import RenderWithRouter from '../RenderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testando o arquivo App.js', () => {
+  
+  it('Renderiza Pokédex na rota `/`', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
 
-test('Renderiza a Pokédex pela rota `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  it('O primeiro link deve possuir o texto Home com a URL `/`', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const home = getByText(/home/i);
+    fireEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('O segundo link deve possuir o texto About com a URL `/about`', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const about = getByText(/about/i);
+    fireEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('O terceiro link deve possuir o texto Favorite com a URL `/favorites`', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const favorite = getByText(/favorite/i);
+    fireEvent.click(favorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Renderizado a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const notFound = 'pagina-nao-encontrada';
+    history.push(notFound);
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
 });
