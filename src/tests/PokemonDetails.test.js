84cd62cbbf8bf27680ca638575f6a6d45c512470
Bruno Nogueira <nugobruno@gmail.com>
2021-01-03 01:36:09 -0300

    Requirement 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,48 +1,52 @@
-test('', () => {});
-// import React from 'react';
-// import { cleanup, fireEvent, getByRole, queryByAltText } from '@testing-library/react';
-// import App from '../App';
-// import TestingRouter from '../components/TestingRouter';
-// import pokemons from '../data';
-// import PokemonDetails from '../components/PokemonDetails';
+import React from 'react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import PokemonDetails from '../components/PokemonDetails';
+import pokemons from '../data';
+import TestingRouter from '../components/TestingRouter';
 
-// afterEach(cleanup);
+afterEach(cleanup);
 
-// describe('seventh requirement', () => {
-//   it('should render detail informations of a given Pokémon', () => {
-//     // referência: Alexandre Faustino
-//     const { getByText, queryByRole } = TestingRouter(<PokemonDetails
-//       isPokemonFavoriteById={ { 25: true } }
-//       match={ { params: { id: '25' } } }
-//       pokemons={ pokemons }
-//       onUpdateFavoritePokemons={ () => {} }
-//     />);
-//     const pokemonName = getByText(/pikachu details/i);
-//     const detailsBtn = queryByRole('url');
-//     const heading = getByText('Summary');
-//     const summary = getByText(
-//       'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.'
-//     );
-
-//     expect(pokemonName).toBeInTheDocument();
-//     expect(detailsBtn).not.toBeInTheDocument();
-//     expect(heading.tagName).toBe('H2');
-//     expect(summary).toBeInTheDocument();
-//   });
-
-//   it('should render a map section of a given Pokémon', () => {
-//     const { getByText, getByRole, queryAllByAltText } = TestingRouter(<PokemonDetails
-//       isPokemonFavoriteById={ { 25: true } }
-//       match={ { params: { id: '25' } } }
-//       pokemons={ pokemons }
-//       onUpdateFavoritePokemons={ () => {} }
-//     />);
-//     const pokemonLocation = getByText(/game locations of Pikachu/i);
-//     const imagesMaps = queryAllByAltText(/Pikachu location/i);
-//     const detailSec = getByRole('pokemon-details');
-
-//     expect(pokemonLocation).toBeInTheDocument();
-//     expect(pokemonLocation.tagName).toBe('H2');
-//     console.log(detailSec);
-//   })
-// });
+describe('seventh requirement', () => {
+  it('should render detail informations of a given Pokémon', () => {
+    // referência: Alexandre Faustino
+    const { getByText, queryByRole } = TestingRouter(<PokemonDetails
+      isPokemonFavoriteById={ { 25: true } }
+      match={ { params: { id: '25' } } }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => {} }
+    />);
+    expect(getByText('Pikachu Details')).toBeInTheDocument();
+    expect(queryByRole('link')).not.toBeInTheDocument();
+    expect((getByText('Summary')).tagName).toBe('H2');
+    expect(getByText(pokemons[0].summary)).toBeInTheDocument();
+  });
+  it('should render map section', () => {
+    const { getByText, getAllByAltText } = TestingRouter(<PokemonDetails
+      isPokemonFavoriteById={ { 25: true } }
+      match={ { params: { id: '25' } } }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => {} }
+    />);
+    const heading = getByText('Game Locations of Pikachu');
+    expect(heading).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+    const maps = getAllByAltText('Pikachu location');
+    const mapImages = pokemons[0].foundAt.map((image) => image.map);
+    maps.forEach((map, index) => {
+      expect(map).toBeInTheDocument();
+      expect(map.src).toBe(mapImages[index]);
+    });
+  });
+  it('should be possible to favorite the pokemon', () => {
+    const { getByLabelText, queryByAltText } = TestingRouter(<PokemonDetails
+      isPokemonFavoriteById={ { 25: false } }
+      match={ { params: { id: '25' } } }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => {} }
+    />);
+    const checkbox = getByLabelText(`Pokémon favoritado?`);
+    expect(checkbox).toBeInTheDocument();
+    fireEvent.change(checkbox, { target: { checked: true } });
+    expect(checkbox).toBeChecked();
+  });
+});
