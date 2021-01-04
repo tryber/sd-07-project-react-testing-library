8f6c0a2b9b2a7d7f4040613b7a9f896e58d876f0
CleberFontinele <cleberfontinele2387@gmail.com>
2021-01-03 21:00:05 -0300

    Testando o arquivo About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -15,4 +15,20 @@ describe('Testando arquivo about', () => {
     const heading = getByRole('heading');
     expect(heading.innerHTML).toBe('About Pokédex');
   });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const p1 = /This application simulates a Pokédex/i;
+    const p2 = /One can filter Pokémons by type/i;
+
+    expect(getByText(p1)).toBeInTheDocument();
+    expect(getByText(p2)).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a imagem de uma pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
