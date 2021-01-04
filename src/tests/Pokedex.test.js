f89299c86c6075a04d16fca476d6f0608a0adcd8
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-01-03 22:22:49 -0300

    retirada do fireEvent

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
