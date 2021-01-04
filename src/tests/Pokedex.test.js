e96802f0e36f070ab37777045db4e3a2ac8d024f
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-01-03 22:20:21 -0300

    Resolução requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+
+const two = 2;
 
 test('A página contém um heading h2 com o texto Encountered pokémons', () => {
   const { getByText } = renderWithRouter(<App />);
@@ -55,6 +58,65 @@ test('A Pokedéx tem os botões de filtro', () => {
       userEvent.click(nextPokemonButton);
       const secondPokemon = getByTestId('pokemon-name');
       expect(firstPokemon !== secondPokemon);
+    }
+  });
+});
+
+test('A Pokédex contém um botão para resetar o filtro', () => {
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+
+  const allButton = getByText('All');
+  expect(allButton).toBeInTheDocument();
+
+  let firstPokemon = getByTestId('pokemon-name').innerHTML;
+  const nextPokemonButton = getByText('Próximo pokémon');
+  userEvent.click(nextPokemonButton);
+  let secondPokemon = getByTestId('pokemon-name').innerHTML;
+  while (firstPokemon !== 'Dragonair') {
+    expect(firstPokemon === secondPokemon).toBe(false);
+    firstPokemon = secondPokemon;
+    userEvent.click(nextPokemonButton);
+    secondPokemon = getByTestId('pokemon-name').innerHTML;
+  }
+  expect(secondPokemon).toBe('Pikachu');
+});
+
+test('é criado, dinamicamente, um botão de filtro para cada tipo de Pokémon', () => {
+  const { getAllByText, getByText, getByTestId } = renderWithRouter(<App />);
+
+  let currentPokemon = getByText('Pikachu').innerHTML;
+  const nextPokemonBtn = getByText('Próximo pokémon');
+  let pokemonTypeArray = getAllByText('Electric');
+  expect(pokemonTypeArray.length).toBe(two);
+
+  userEvent.click(nextPokemonBtn);
+
+  while (currentPokemon !== 'Pikachu') {
+    currentPokemon = getByTestId('pokemon-name').innerHTML;
+    pokemonTypeArray = getAllByText(getByTestId('pokemonType').innerHTML);
+    expect(pokemonTypeArray.length).toBe(two);
+    userEvent.click(nextPokemonBtn);
+  }
+});
+
+test('O botão próximo pokemon é desabilitado quando só houver um pokémon do tipo', () => {
+  const { getByText, getAllByTestId } = renderWithRouter(<App />);
+
+  const buttonNxtPokemon = getByText('Próximo pokémon');
+  const pokemonTypes = getAllByTestId('pokemon-type-button');
+
+  pokemonTypes.forEach((type) => {
+    userEvent.click(type);
+    const typeText = type.innerHTML;
+
+    if (
+      typeText === 'Electric'
+      || typeText === 'Bug'
+      || typeText === 'Poison'
+      || typeText === 'Normal'
+      || typeText === 'Dragon '
+    ) {
+      expect(buttonNxtPokemon.disabled).toBe(true);
     }
   });
 });
