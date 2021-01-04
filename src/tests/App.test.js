1b859148d4a0cc49f06030d74f264711bf50361c
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-01-04 09:29:08 -0300

    Finalizando requisito 1, testando o arquivo App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -27,5 +27,40 @@ describe('Testando o componente App', () => {
     expect(about).toBeInTheDocument();
     const favoritePokemons = getByText(/Favorite Pokémons/i);
     expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  test('By clicking on "Home" a page is redirected to "/"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const linkHome = getByText(/Home/i);
+    fireEvent.click(linkHome);
+    const path = history.location.pathname;
+    expect(path).toBe('/');
+  });
+
+  test('By clicking on "About" a page is redirected to "/about"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const linkAbout = getByText(/About/i);
+    fireEvent.click(linkAbout);
+    const path = history.location.pathname;
+    expect(path).toBe('/about');
+  });
+
+  test('By clicking on "Favorite Pokémons" a page is redirected to "/favorites', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const linkFavorites = getByText(/Favorite Pokémons/i);
+    fireEvent.click(linkFavorites);
+    const path = history.location.pathname;
+    expect(path).toBe('/favorites');
+  });
+
+  test('When typing unknown path, redirects to page "Not Found"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/naoencontrado');
+    const notFound = getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
   });
 });
