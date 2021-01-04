f5f34dfee8d919efbbf3a930d0799a7fc62a3e86
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-01-04 08:36:28 -0300

    Testando se existe os Links 'Home, Abaut, Favorite Pokemons

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,31 @@
 import React from 'react';
+// import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-describe('Tests the App screen', () => {
+describe('Testando o componente App', () => {
   test('renders a reading with the text `Pokédex`', () => {
-    const { getByText, history } = renderWithRouter(<App />);
+    const { getByText } = renderWithRouter(<App />);
 
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
+  });
 
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
+  test('If the Pokedex is rendered in the path "/"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const path = history.location.pathname;
+    expect(path).toBe('/');
+  });
+
+  test('If the Links exist "Home", "About", "Favorite Pokémons"', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+    const about = getByText(/About/i);
+    expect(about).toBeInTheDocument();
+    const favoritePokemons = getByText(/Favorite Pokémons/i);
+    expect(favoritePokemons).toBeInTheDocument();
   });
 });
