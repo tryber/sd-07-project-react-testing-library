9e45dc1fc05568933b3dd506a7f8df7703c2ba5e
Willian <willian.carrato@gmail.com>
2021-01-04 01:59:14 -0300

    corrigindo eslint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('testing About.js component', () => {
+  test('page should contain pokédex info', () => {
+    const { getByText } = render(<About />);
+    expect(getByText('About Pokédex')).toBeInTheDocument();
+  });
+
+  it('should contain a h2 header with About Pokédex text', () => {
+    const { getByRole } = render(<About />);
+    expect(getByRole('heading', { level: 2 })).toBeInTheDocument();
+  });
+
+  it('should contain two paragraphs with Pokedex text', () => {
+    const { getByText } = render(<About />);
+    const Part1 = 'This application simulates a Pokédex,';
+    const Part2 = ' a digital encliclopedia containing all Pokémons';
+    const second1 = 'One can filter Pokémons by type,';
+    const second2 = ' and see more details for each one of them';
+    expect(getByText(Part1 + Part2)).toBeInTheDocument();
+    expect(getByText(second1 + second2)).toBeInTheDocument();
+  });
+
+  it('should contain an image', () => {
+    const { getByRole } = render(<About />);
+    const img = getByRole('img').src;
+    expect(img).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
+import App from '../App';
+
+test('Page without favorites must have the message', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/favorites'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const noFavorites = getByText('No favorite pokemon found');
+  expect(noFavorites).toBeInTheDocument();
+});
+
+test('All favorite pokémon should appear', () => {
+  const { getByText, getByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  fireEvent.click(getByText('More details'));
+  fireEvent.click(getByText('Pokémon favoritado?'));
+  fireEvent.click(getByText('Favorite Pokémons'));
+  const fav = getByTestId('pokemon-name').textContent;
+  expect(fav).toBe('Pikachu');
+});
+
+test('No selected pokemons shouldnt appear', () => {
+  const { getByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const fav = getByTestId('pokemon-name').textContent;
+  expect(fav).not.toBe('Charmander');
+});
