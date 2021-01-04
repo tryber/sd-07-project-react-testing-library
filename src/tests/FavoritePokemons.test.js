31ca838c94e460f48eb27f630df99a19381c06fe
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-01-04 13:52:08 -0300

    Requisito 3: Implementando testes no arquivo 'FavoritePokemons'

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testando o arquivo "FavoritePokemon"', () => {
+  test('Teste se é exibida a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const texto = getByText(/No favorite pokemon found/i);
+    expect(texto).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { container } = renderWithRouter(<FavoritePokemons />);
+    const [fav] = container.getElementsByClassName('favorite-pokemon');
+    expect(fav).toBe();
+  });
+
+  test('Verifica se a pagina About renderiza dois <p>', () => {
+    const { container } = renderWithRouter(<FavoritePokemons />);
+    const [p] = container.getElementsByTagName('p');
+    expect(p).toBeInTheDocument();
+    expect(p).toHaveTextContent('No favorite pokemon found');
+  });
+});
