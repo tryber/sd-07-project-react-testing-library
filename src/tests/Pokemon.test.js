a5f7c24475704cafc45afaeeb06ff1a3abdbc9c9
Renato Silva <renatosdev@gmail.com>
2021-01-03 23:10:20 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
+import { cleanup, screen, fireEvent } from '@testing-library/react';
 import Pokemon from '../components/Pokemon';
 import RenderWithRouter from './RenderWithRouter';
+import data from '../data';
 
 afterEach(cleanup);
 
-const pokemon = {
-  id: 25,
-  name: 'pikachu',
-  type: 'electric',
-  averageWeight: { value: '6', measurementUnit: 'kg' },
-  image: 'pikachu-image',
-  foundAt: [{ location: 'locale', map: 'anywhere' }],
-  summary: 'A pokemon',
-};
-  {
-    id: 2,
-    averageWeight: { value: '95', measurementUnit: 'kg' },
-    name: 'Rapidash',
-    type: 'Fire',
+const pokemon = data[6];
+const Render = (favorite = false) => (
+  RenderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ favorite } />)
+);
+
+test('if a card is rendered with the information of a certain Pokémon', () => {
+  const { container } = Render();
+  expect(screen.getByText(/Rapidash/i)).toBeInTheDocument();
+  expect(screen.getByText(/Fire/i)).toBeInTheDocument();
+  expect(screen.getByText(/Average weight: 95.0 kg/i)).toBeInTheDocument();
+  const image = container.querySelector('img');
+  expect(image).toHaveAttribute(
+    'src',
+    'https://cdn.bulbagarden.net/upload/5/58/Spr_5b_078.png',
+  );
+  expect(image).toHaveAttribute('alt', 'Rapidash sprite');
+});
+
+test(
+  'if the Pokémon card indicated on the Pokédex'
+    + 'contains a navigation link to display details of this Pokémon',
+  () => {
+    const { getByText, history } = Render();
+    const link = getByText('More details');
+    expect(history.location.pathname).toBe('/');
+    fireEvent.click(link);
+    expect(history.location.pathname).toBe('/pokemons/78');
   },
-];
+);
+
+test('if there is a star icon on favorite Pokémon', () => {
+  const { container } = Render(true);
+  const icon = container.querySelector('.favorite-icon');
+  expect(icon).toHaveAttribute('src', '/star-icon.svg');
+  expect(icon).toHaveAttribute('alt', 'Rapidash is marked as favorite');
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen, fireEvent } from '@testing-library/react';
+import PokemonDetails from '../components/PokemonDetails';
+import RenderWithRouter from './RenderWithRouter';
+import data from '../data';
+
+afterEach(cleanup);
+
+const pokemon = data[6];
+const Render = () => (
+  RenderWithRouter(
+  <PokemonDetails 
+    pokemons={ [pokemon] }
+    isPokemonFavoriteById={ { 78: false } }
+    onUpdateFavoritePokemons={ () => {} }
+    match={ { params: { id: '78' } } }
+  />
+)
+);
+
+test ('whether detailed information about the selected'
+  + 'Pokémon is shown on the screen', () => {
+    const { } = Render();
+});
