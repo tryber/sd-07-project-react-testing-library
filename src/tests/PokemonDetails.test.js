60b5273d6a9a81cfc5dd3c026553faf56dafa928
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-01-04 00:54:11 -0300

    test 7 - part 1

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('Test 7 - PokemonDetails.js', () => {
+  it('Should show details about a especifc pokemon', () => {
+    const { getByText } = RenderWithRouter(
+      <App>
+        <Pokedex pokemons={ pokemons } />
+      </App>,
+    );
+    const moreDetails = getByText('More details');
+    fireEvent.click(moreDetails);
+    const pokeName = pokemons[0].name;
+    expect(getByText(pokeName)).toBeInTheDocument();
+    expect(moreDetails).not.toBeInTheDocument();
+    const heading = document.querySelectorAll('h2');
+    expect(heading[1]).toBeInTheDocument();
+    expect(heading[1].innerHTML).toBe('Summary');
+    const description = 'This intelligent Pokémon roasts hard berries'
+    + ' with electricity to make them tender enough to eat.';
+    const paragraph = document.querySelectorAll('p');
+    expect(paragraph[3].innerHTML).toBe(description);
+  });
+});
