4db5a8e1742858d20925cf4f109c6dc1dc7bb99b
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-01-04 12:39:11 -0300

    kajdlkj

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import renderWithRouter from '../renderWithRouter';
-import Pokedex from '../components/Pokedex';
+// import React from 'react';
+// import renderWithRouter from '../renderWithRouter';
+// import Pokedex from '../components/Pokedex';
 
-describe('Testing the Pokedex.js file', () => {
-  it('if the page contains an h2 heading with the text Encountered Pokémon', () => {
-    
-  })
-})
+// describe('Testing the Pokedex.js file', () => {
+//   it('if the page contains an h2 heading with the text Encountered Pokémon', () => {
+//     const { getByText } = render
+//   })
+// })
