5da59dbfee58bae2526511cbde6c5eb17a71d36b
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-03 21:18:47 -0300

    testing next pokemon button

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data'
+
+describe('testing Pokedex.js component', () => {
+  it('should contain heading with Encountered Pokemons text', () => {
+    const pokemon = [pokemons[0]];
+    const { getByRole } = renderWithRouter(<Pokedex
+      pokemons={ pokemon }
+      isPokemonFavoriteById={ { 25: false } }
+    />);
+    expect(getByRole('heading', { level: 2 })).toHaveTextContent('Encountered pokémons');
+  });
+
+  it('should contain a button with próximo pokémon text', () => {
+    const pokemon = [pokemons[0]];
+    const { getByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemon }
+      isPokemonFavoriteById={ { 25: false } }
+    />);
+    expect(getByTestId('next-pokemon')).toHaveTextContent('Próximo pokémon');
+  });
+
+  it('should show next pokemon when clicking next button', () => {
+    const pokemon = [pokemons[0], pokemons[1]];
+    const { getByTestId, getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemon }
+      isPokemonFavoriteById={ { 25: false, 4: false } }
+    />);
+    fireEvent.click(getByTestId('next-pokemon'));
+    expect(getByText('Charmander')).toBeInTheDocument();
+  });
+});
