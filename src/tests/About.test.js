f780cf5c464935b699525bd3918167dd57b56e08
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-03 21:30:28 -0300

    Modificaçoes em About.test para resolver o problema "Unable to find an element with the text: /About Pokédex/i. This could be because the text is broken up by multiple elements. In this case, you can provide a function for your text matcher to make your matcher more flexible."

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testando  o link /about', () => {
+  it('A página deve conter as informações sobre a Pokédex', () => {
+    render(<About />);
+    const aboutText = screen.getByText(/This application simulates a Pokédex/i);
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  it('A página deve conter um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const h2 = screen.getByText(/About Pokédex/i);
+    expect(h2).toBeInTheDocument();
+    expect(h2.tagName).toBe('H2');
+  });
+});
