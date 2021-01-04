f3cbe3a64d488e4f61a944580063d7b130d36b22
Willian <willian.carrato@gmail.com>
2021-01-04 00:16:34 -0300

    finalizando RTL, verificando codeclimate

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Requirement 1', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('contains links to Home, About and Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  it('must be redirected to the correct path when the link is clicked', () => {
+    const { history } = renderWithRouter(<App />);
+
+    fireEvent.click(screen.getByText('Home'));
+    expect(history.location.pathname).toBe('/');
+
+    fireEvent.click(screen.getByText('About'));
+    expect(history.location.pathname).toBe('/about');
+
+    fireEvent.click(screen.getByText('Favorite Pokémons'));
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('must be redirected to the `Not Found` page when entering an unknown URL', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagenotfound/');
+    const combine = getByText(/Page requested not found/i);
+    expect(combine).toBeInTheDocument();
+  });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Requirement 3', () => {
+  it('should display No Favoreite pokemon if no fav pokemon', () => {
+    render(<FavoritePokemons />);
+    expect(screen.getByText('No favorite pokemon found'));
+  });
+
+  it('displays all fav pokemons cards', () => {
+    const favoritePokemon = [pokemons[0], pokemons[1]];
+    renderWithRouter(<FavoritePokemons pokemons={favoritePokemon} />);
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    expect(screen.getByText('Charmander')).toBeInTheDocument();
+  });
+
+  it('displays no cards when pokemon isnt favorite', () => {
+    const favoritePokemon = [];
+    renderWithRouter(<FavoritePokemons pokemons={favoritePokemon} />);
+    expect(screen.queryByText('Pikachu')).not.toBeInTheDocument();
+    expect(screen.queryByText('Charmander')).not.toBeInTheDocument();
+  });
+});
