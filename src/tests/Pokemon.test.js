69a49f250bba82f16be941c5f277638ab7492f28
Renato Silva <renatosdev@gmail.com>
2021-01-03 22:06:31 -0300

    Requisito 3

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import Pokemon from '../components/Pokemon';
+import RenderWithRouter from './RenderWithRouter';
+
+afterEach(cleanup);
+
+const pokemon = {
+  id: 25,
+  name: 'pikachu',
+  type: 'electric',
+  averageWeight: { value: '6', measurementUnit: 'kg' },
+  image: 'pikachu-image',
+  foundAt: [{ location: 'locale', map: 'anywhere' }],
+  summary: 'A pokemon',
+};
+  {
+    id: 2,
+    averageWeight: { value: '95', measurementUnit: 'kg' },
+    name: 'Rapidash',
+    type: 'Fire',
+  },
+];
