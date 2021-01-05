7805fbe1d2aa7a60489f4baf45df72724e9f5ae8
Iury Nathan <iurynathan@gmail.com>
2021-01-05 16:18:22 -0300

    vmaos la

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { fireEvent, render } from '@testing-library/react';
-import renderWithRouter from '../renderWithRouter.js';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
