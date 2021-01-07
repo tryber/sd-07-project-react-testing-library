6f7ac9ffa16e99946c59567761167656cbcd880a
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2021-01-07 16:24:08 -0300

    Nicholas - Iniciando o projeto. VQV

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />)
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
