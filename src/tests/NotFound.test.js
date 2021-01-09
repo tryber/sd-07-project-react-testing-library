6177fd98f249fd82bda2ebf5637e992927bef6a7
Lets <letsferreiratx@gmail.com>
2021-01-09 18:29:18 -0300

    notFound test ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+test('heading text', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+
+  const heading = getByText(/Page requested not found/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('not found image', () => {
+  const { getByTestId } = renderWithRouter(<NotFound />);
+
+  const image = getByTestId('not-found-image');
+  expect(image.src).toBe(
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  );
+});
