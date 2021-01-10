5840c54e124b4f530fbde84c1d6aa32bf73f0e34
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-01-10 17:24:04 -0300

    App.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -21,4 +21,50 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('shows the Pokédex when the route is `About`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/about'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('shows the Pokédex when the route is `Favorite Pokémons`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/favorites'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Favorite pokémons')).toBeInTheDocument();
+});
+
+test('text of links contents `Home`, `About`, `Favorite Pokémons` respectivamente',
+  () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const links = getAllByRole('link');
+    expect(links[0].innerHTML).toBe('Home');
+    expect(links[1].innerHTML).toBe('About');
+    expect(links[2].innerHTML).toBe('Favorite Pokémons');
+  });
+
+test('if click in the Home link go to a URL `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/favorites'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  fireEvent.click(getByText('Home'));
+  const heading = getByText('Pokédex');
+  expect(heading).toBeInTheDocument();
 });
