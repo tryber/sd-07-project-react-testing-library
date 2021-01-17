e9010db2da61879831e506debe03a21a84722834
Eduardo Pizorno <pizorno@gmail.com>
2021-01-17 19:59:29 -0300

    App.test - Refeito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import RenderWithRaouter from './RenderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Tests-1 App.js', () => {
+  it('should render initial page Pokédex', () => {
+    const { getByText } = RenderWithRouter(<App />);
+    const head = getByText(/Pokédex/i);
+    expect(head).toBeInTheDocument();
+  });
+  
+  it('should have a text home', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const home = getByText('Home');
+    fireEvent.click(home);
+    const { pathname } = history.location;
+    expect(home).toBeInTheDocument();
+    expect(pathname).toBe('/');
+  });
+
+  it('should have a text About', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const about = getByText('About');
+    fireEvent.click(about);
+    const { pathname } = history.location;
+    expect(about).toBeInTheDocument();
+    expect(pathname).toBe('/about');
+  });
+
+  it('should have a text Favorite Pokémons', () => {
+    const { getByText, history } = RenderWithRouter(<App />);
+    const favorites = getByText('Favorite Pokémons');
+    fireEvent.click(favorites);
+    const { pathname } = history.location;
+    expect(favorites).toBeInTheDocument();
+    expect(pathname).toBe('/favorites:w');
+  });
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  expect(getByText('Encountered polémons')).toBeInTheDocument();
-});
 
-test('first link navigation is Home', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  expect(getByText('Home')).toBeInTheDocument();
-});
-
-test('second link navigation is About', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  expect(getByText('About')).toBeInTheDocument();
-});
