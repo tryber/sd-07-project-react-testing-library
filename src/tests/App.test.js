6a7347cc9c5a91919ba1d29fc921d0a0960b8fa4
Andre <dehhmenezes87@gmail.com>
2021-01-19 19:02:01 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testando o arquivo App.js', () => {
+  it('Verifica se renderiza a Pokédex na rota `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const title = getByText(/Encountered pokémons/);
+    const { pathname } = history.location;
+    expect(title).toBeInTheDocument();
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se há um conjunto de links de navegação', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/);
+    const about = getByText(/About/);
+    const favoritePokemons = getByText(/Favorite Pokémons/);
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  it('O link `Home` redirecionar para a rota `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const link = getByText(/Home/i);
+    const { pathname } = history.location;
+    userEvent.click(link);
+    expect(link).toBeInTheDocument();
+    expect(pathname).toBe('/');
+  });
+
+  it('O link `About` redirecionar para a rota `/about`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const link = getByText(/About/i);
+    userEvent.click(link);
+    const { pathname } = history.location;
+    expect(link).toBeInTheDocument();
+    expect(pathname).toBe('/about');
+  });
+
+  it('O link `Favorite Pokémons` redirecionar para a rota `/favorites`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const link = getByText(/Favorite Pokémons/i);
+    userEvent.click(link);
+    const { pathname } = history.location;
+    expect(link).toBeInTheDocument();
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('A página `Not Found` é renderizada quando acessado um caminho inexistente', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const link = 'digimon';
+    history.push(link);
+    const notFound = getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
 });
