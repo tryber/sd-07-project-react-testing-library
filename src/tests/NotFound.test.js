57e02aded0832459ca884d6416a1403f2835f41c
Andre <dehhmenezes87@gmail.com>
2021-01-20 12:10:12 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando o arquivo `NotFound.js`', () => {
+  it('Verifica se há um título `h2` com o texto `Page requested not found`', () => {
+    const { getByRole, getByText } = renderWithRouter(<NotFound />);
+    const title = getByRole('heading');
+    const text = getByText('Page requested not found');
+    expect(title).toBeInTheDocument();
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Verfica se há ima imagem de um Pikachu chorando', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const images = getAllByRole('img');
+    const pikachuCryingGif = images.find((image) => image.src === 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(pikachuCryingGif).toBeInTheDocument();
+  });
+});
