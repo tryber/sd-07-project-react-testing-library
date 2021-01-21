828d0dac6e0ffc3868e3a15700f0e7a62747ff05
MarceloBorges <melombm@hotmail.com>
2021-01-21 13:19:54 -0500

    primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../RenderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,53 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Testando o arquivo App.js', () => {
+  it('O topo da aplicação deve conter um conjunto fixo de links de navegação', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favoritePokemons = getByText('Favorite Pokémons');
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  it('Quando clicar no link Home, o app é redirecionado para a URL /', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    fireEvent.click(home);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+  });
+
+  it('Quando clicar no link About, o app é redirecionado para a URL /about', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText('About');
+    fireEvent.click(about);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+  });
+
+  it('ao clicar em Favorite Pokémons, o app é redirecionado para a URL /favorites', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoritePokemons = getByText('Favorite Pokémons');
+    fireEvent.click(favoritePokemons);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('a página Not Found deve aparecer ao entrar em uma URL desconhecida', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+
+    history.push('/pagina-nao-existente');
+    const
+      noMatch = getByAltText('Pikachu crying because the page requested was not found');
+    expect(noMatch).toBeInTheDocument();
+  });
 });
