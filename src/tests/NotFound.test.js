07439b523a026a7c760a826ad0edc0dda382dae8
Arthur de Sa <arthurdesa94@gmail.com>
2021-01-21 16:41:57 -0300

    sending the NotFound tests again

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Page must have a heading h2 with text Page requested not found', () => {
+  const history = createMemoryHistory();
+  const { getByRole } = render(
+    <Router history={history}>
+      <NotFound />
+    </Router>,
+  );
+  const h2 = getByRole('heading');
+  expect(h2).toBeInTheDocument();
+  expect(h2.textContent).toBe('Page requested not found');
+  expect(h2.tagName).toBe('H2');
+});
+
+test('Page must show the following img https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const history = createMemoryHistory();
+  const { getByAltText } = render(
+    <Router history={history}>
+      <NotFound />
+    </Router>,
+  );
+  const image = getByAltText(/Pikachu crying because the page requested was not found/i);
+  expect(image).toBeInTheDocument();
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
