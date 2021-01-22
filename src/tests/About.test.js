c42a483ed6bec06671cb23557621561a115112a9
Márcio Barroso <barrrosomarcio@gmail.com>
2021-01-22 20:31:44 -0300

    about.tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('About.js Tests`', () => {
+  it('Checks if "About Pokédex" info apears', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const heading = getByText(/About Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Checks if a H2 tag with text "About Pokédex" is rendered', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const title = getByText('About Pokédex');
+    expect(title).toBeInTheDocument();
+    expect(title.outerHTML).toBe('<h2>About Pokédex</h2>');
+  });
+
+  it('Checks 2 <p> are rendered', () => {
+    const { container, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const title = container.querySelectorAll('p');
+    expect(title.length.toString()).toBe('2');
+  });
+
+  it('Checks if a especific IMG is rendered', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const img = getByAltText('Pokédex');
+    const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(src);
+  });
+});
