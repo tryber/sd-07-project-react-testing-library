d183b0b3537ddd7df518e4ddeffed51e3b1241ed
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-01-22 16:29:41 -0300

    NotFound tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+  () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/error'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Page requested not found/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+test('Teste se página contém um heading h2',
+  () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={ ['/error'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const title = container.querySelector('h2');
+    expect(title).toBeInTheDocument();
+  });
+
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  () => {
+    const { getByAltText } = render(
+      <MemoryRouter initialEntries={ ['/error'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    const imageUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image.src).toBe(imageUrl);
+  });
