9eac2baa5f3db856e57b620033b3082ba5457950
BrunoNogueiraBruno <nugobruno@gmail.com>
2021-01-22 14:43:56 -0300

    4th Req

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('4th Req. | NotFound.js', () => {
+  it('should render title', () => {
+    const { getByText } = render(<NotFound />);
+    const title = getByText(/Page requested not found/i);
+    const emoji = getByText('😭');
+    expect(title).toBeInTheDocument();
+    expect(title.tagName).toBe('H2');
+    expect(emoji).toBeInTheDocument();
+  });
+  it('should render image', () => {
+    const { getByAltText } = render(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
