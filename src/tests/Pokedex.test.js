6ba4ede4f88def31e136dafc6c20c2a63200e6d7
MarceloBorges <melombm@hotmail.com>
2021-01-22 10:49:03 -0500

    requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+describe('Testando o arquivo Pokedex', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+
+  });
+  it('', () => {
+
+  });
+});
