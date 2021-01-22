957440ae3279c92913f47078b8301ca8b7067b7b
Márcio Barroso <barrrosomarcio@gmail.com>
2021-01-22 20:25:11 -0300

    app.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-  
+describe('App.js Tests`', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
   it('checks if Home, About and Favorite links are rendered', () => {
     const { getByText } = renderWithRouter(<App />);
     const home = getByText(/Home/i);
