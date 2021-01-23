8e600d368bedfc67077ce64fc06d9e6c09b80fb4
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-01-23 15:48:06 -0300

    dsadasda

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,24 +4,27 @@ import Pokedex from '../components/Pokedex';
 import Pokemon from '../data';
 
 describe('Testing the Pokedex.js file', () => {
+
   test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+
     const { getByRole } = renderWithRouter(<Pokedex
       pokemons={ pokemon }
       isPokemonFavoriteById={ { 25: false } }
     />);
     expect(getByRole('heading', { level: 2 })).toBeInTheDocument();
-  });
+});
 
-  test('Teste se é exibido o próximo Pokémon da lista', () => {
+test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo pokémon é clicado.', () => {
+
     const pokemon = [Pokemon[0]];
     const { getByTestId } = renderWithRouter(<Pokedex
       pokemons={ pokemon }
       isPokemonFavoriteById={ { 25: false } }
     />);
     expect(getByTestId('next-pokemon')).toHaveTextContent('Próximo pokémon');
-  });
+});
 
   test('Teste se é mostrado apenas um Pokémon por vez', () => {
 
-  });
+});
 });
