cb4dacf6a89cf2656b15712349ffe552ca849a5b
spawnred <bruno.spawnhell@gmail.com>
2021-01-23 14:19:03 -0300

    item 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -9,10 +9,10 @@ const isPokemonFavoriteById = {
   4: false,
   10: false,
   23: false,
-  25: false,
+  25: true,
   65: false,
   78: false,
-  143: true,
+  143: false,
   148: false,
   151: false,
 };
