4798c58a5d1179b763211236784e79a8c89c2b22
spawnred <bruno.spawnhell@gmail.com>
2021-01-23 00:55:10 -0300

    item 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -13,9 +13,10 @@ test('se a página contém um heading h2 c/ texto About Pokedéx', () => {
   const h2 = getByRole('heading', { name: 'About Pokédex' });
   expect(h2.tagName).toBe('H2');
 });
+
 test('se a página contém da Pokédex', () => {
   const { getByRole } = render(<About />);
-  const link = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+  const link = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
   const img = getByRole('img');
   expect(img.src).toBe(link);
 });
