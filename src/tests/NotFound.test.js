aeb83793c2933a49268d13900b4f4950dd786fc4
ErlingSJunior <erlingjunior@gmail.com>
2021-01-24 18:49:18 -0300

    requisito 4 pagina not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Requisito 4 Not Found Page', () => {
+  it('Verifica se a pagina contem "h2" Page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+    const tag = getByRole('heading', { name: /page requested not found Crying emoji/i });
+    expect(tag.tagName).toBe('H2');
+  });
+
+  it('Verifica se a página mostra a imagem', () => {
+    const { getByAltText } = render(<NotFound />);
+    const linkImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = getByAltText('Pikachu crying because the page requested was not found');
+    expect(img.src).toBe(linkImage);
+  });
+});
