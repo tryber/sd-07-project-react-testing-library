2b07d7061b83c6f033d82226730642f88d8bcd87
IzeldaValeriano <zel.valeriano@gmail.com>
2021-01-25 14:41:44 -0300

    segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,13 +1,24 @@
-/* import React from 'react';
-import { cleanup } from '@testing-library/react';
-import App from '../App';
+import React from 'react';
+import { render } from '@testing-library/react';
 import About from '../components/About';
 
-afterEach(cleanup);
+describe('Requisito 2', () => {
+  it('Verifica se a página contém informações sobre a Pokédex', () => {
+    const { getByText } = render(<About />);
+    const pokedex = getByText(/digital encliclopedia containing all/);
+    expect(pokedex).toBeInTheDocument();
+  });
 
-describe('', () => {
-  it('', () => {
+  it('Verifica se a página contém um heading <h2> com o texto "About Pokédex"', () => {
+    const { getByRole } = render(<About />);
+    const headingH2 = getByRole('heading', { name: 'About Pokédex' });
+    expect(headingH2.tagName).toBe('H2');
+  });
 
+  it('Verifica se a página contém a seguinte imagem de uma "Pokédex"', () => {
+    const { getByRole } = render(<About />);
+    const linkImage = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = getByRole('img');
+    expect(img.src).toBe(linkImage);
   });
 });
- */
