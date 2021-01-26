7005a26911bdca6f5d47e5fedb58d86cfa768d58
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-01-26 13:04:14 -0300

    requisito 5 só gzuis na causa

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -10,7 +10,7 @@ describe('Testing the Pokedex.js file', () => {
       pokemons={ Pokemon }
       isPokemonFavoriteById={ { 25: false } }
     />);
-    expect(getByRole('heading', { level: 2 })).toBeInTheDocument();
+    expect(getByRole('heading', { name: 'Encountered pokémons' })).toBeInTheDocument();
   });
 
   test('Teste se é exibido o próximo Pokémon da lista', () => {
@@ -36,9 +36,37 @@ describe('Testing the Pokedex.js file', () => {
     />);
 
     const pokemon = getAllByTestId('pokemon-name');
-    expect(pokemon.length).toBe(1);
+    expect(pokemon).toHaveLength(1);
   });
   test('Teste se a Pokédex tem os botões de filtro.', () => {
+    const pokemon = [Pokemon[0], Pokemon[1], Pokemon[6]];
+    const { getByText, getAllByTestId, getByRole } = renderWithRouter(<Pokedex
+      pokemons={ pokemon }
+      isPokemonFavoriteById={ { 25: false, 4: false, 78: false } }
+    />);
 
+    const typeButton = getAllByTestId('pokemon-type-button');
+    const fireFilter = getByRole('button', { name: 'Fire' });
+
+    const pokemonLength = 2;
+
+    fireEvent.click(fireFilter);
+    expect(typeButton).toHaveLength(pokemonLength);
+    expect(getByText('Charmander')).toBeInTheDocument();
+  });
+
+  test('Testa se a Pokédex contém um botão para resetar o filtro', () => {
+    const pokemons = [Pokemon[0], Pokemon[1]];
+    const { getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ { 25: false, 4: false } }
+    />);
+
+    const allBtn = getByText('All');
+    expect(allBtn).toBeInTheDocument();
+
+    fireEvent.click(allBtn);
+    const pokemonLength = 2;
+    expect(pokemons.length).toEqual(pokemonLength);
   });
 });
