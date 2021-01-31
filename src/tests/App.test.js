a50f5d88e092a67df5e7a9564614465ef2804bb8
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-01-31 11:27:10 -0300

    Req 1 - Lint 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -16,7 +16,7 @@ test('renders a reading with the text `Pokédex`', () => {
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
@@ -28,4 +28,44 @@ test('testa o texto Home', () => {
   const { getByText } = renderWithRouter(<App />);
   const home = getByText('Home');
   expect(home).toBeInTheDocument();
+});
+
+test('testa o texto About', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const about = getByText('About');
+  expect(about).toBeInTheDocument();
+});
+
+test('testa o texto Favorite Pokémons', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const favorite = getByText('Favorite Pokémons');
+  expect(favorite).toBeInTheDocument();
+});
+
+test('se a aplicação é redirecionada para a página inicial', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Home/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('se a aplicação é redirecionada para a página About', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/About/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('se a aplicação é redirecionada para a página favoritos', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('se a aplicação é redirecionada para a página 404', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/issonaoexiste/');
+  const erro404 = getByText(/Page requested not found/i);
+  expect(erro404).toBeInTheDocument();
 });
