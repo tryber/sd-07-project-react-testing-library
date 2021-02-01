63cc0facfacb4f5eea93ca2a8d960828d3dc6940
Anderson Meira <contatodeanderson@gmail.com>
2021-01-31 21:51:45 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,20 +1,31 @@
-/*
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './RenderWithRouter';
-import About from '../components/About';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Requisito 3: Testando o arquivo FavoritePokemons.js', () => {
   it('Testa se é exibido na tela a mensagem No favorite pokemon found.', () => {
-    expect(true).toBeTruthy();
+    const { queryByText } = renderWithRouter(<FavoritePokemons />);
+
+    const noFavoriteMessage = queryByText(/No favorite pokemon found/i);
+    // console.log(noFavoriteMessage);
+    expect(noFavoriteMessage).toBeInTheDocument();
   });
 
   it('Testa se é exibido todos os cards de pokémons favoritados.', () => {
-    expect(true).toBeTruthy();
+    const { queryByText, getByAltText } = renderWithRouter(<App />);
+    fireEvent.click(queryByText(/More details/i));
+    fireEvent.click(queryByText(/Pokémon favoritado?/i));
+    const favoritePokemonImage = getByAltText(/is marked as favorite/i);
+
+    expect(favoritePokemonImage).toBeInTheDocument();
   });
 
   it('Testa se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
-    expect(true).toBeTruthy();
+    const { queryByText } = renderWithRouter(<FavoritePokemons />);
+    const noFavoriteMessage = queryByText(/No favorite pokemon found/i);
+
+    expect(noFavoriteMessage).toBeDefined();
   });
 });
-*/
-test('', () => {});
