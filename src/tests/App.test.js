fb5e523eff4d06cce6ef1326c54992fb96851542
André Ferreira <andrefpires1@gmail.com>
2021-02-01 02:25:20 -0300

    Iniciando projeto :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App.js', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 });
