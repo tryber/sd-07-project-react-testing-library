f903bd0d475d5828d0c210380ad12908c0b6a3ab
Anderson Meira <contatodeanderson@gmail.com>
2021-01-31 22:11:59 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,16 +1,21 @@
-/*
 import React from 'react';
 import renderWithRouter from './RenderWithRouter';
-import About from '../components/About';
+import NotFound from '../components/NotFound';
 
 describe('Requisito 4: Testando o arquivo NotFound.js', () => {
   it('Testa se página contém um heading h2 com o texto Page requested not found', () => {
-    expect(true).toBeTruthy();
+    const { queryByText } = renderWithRouter(<NotFound />);
+    const notFoundTitle = queryByText(/Page requested not found/i);
+
+    expect(notFoundTitle.tagName).toBe('H2');
   });
 
   it('Testa se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
-    expect(true).toBeTruthy();
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const altText = 'Pikachu crying because the page requested was not found';
+    const imageURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const favoritePokemonImage = getByAltText(altText);
+
+    expect(favoritePokemonImage.src).toBe(imageURL);
   });
 });
-*/
-test('', () => {});
