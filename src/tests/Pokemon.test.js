053296f2a64fc97ab40c7c78aadbe93815dbbacd
Anderson Meira <contatodeanderson@gmail.com>
2021-02-01 12:17:27 -0300

    Requisito 6 - incompleto

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,38 +1,56 @@
-/*
-
-6. Testando o arquivo Pokemon.js
-
-  * Teste se é renderizado um card com as informações de determinado pokémon.
-
-    - O nome correto do Pokémon deve ser mostrado na tela;
-
-    - O tipo correto do pokémon deve ser mostrado na tela.
-
-    - O peso médio do pokémon deve ser exibido com um texto no formato Average weight: <value> <measurementUnit>; onde <value> e <measurementUnit> são, respectivamente, o peso médio do pokémon e sua unidade de medida.
-
-    - A imagem do Pokémon deve ser exibida. Ela deve conter um atributo src com a URL da imagem e um atributo alt com o texto <name> sprite, onde <name> é o nome do pokémon;
-
-  * Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes deste Pokémon. O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon exibido;
-
-  * Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento da aplicação para a página de detalhes de Pokémon.
-
-  * Teste também se a URL exibida no navegador muda para /pokemon/<id>, onde <id> é o id do Pokémon cujos detalhes se deseja ver;
-
-  * Teste se existe um ícone de estrela nos Pokémons favoritados.
-
-    - O ícone deve ser uma imagem com o atributo src contendo o caminho /star-icon.svg;
-
-    - A imagem deve ter o atributo alt igual a <pokemon> is marked as favorite, onde <pokemon> é o nome do Pokémon exibido.
-
 import React from 'react';
 import renderWithRouter from './RenderWithRouter';
-import About from '../components/About';
+import Pokemon from '../components/Pokemon';
+import pokeData from '../data';
 
-describe('Requisito : ', () => {
-  it('', () => {
+describe('Requisito 6: Testando o arquivo Pokemon.js', () => {
+  it('Testa se é renderizado um card com as informações de determinado pokémon', () => {
+    const { queryByText, getByAltText, getAllByRole } = renderWithRouter(
+      <Pokemon pokemon={ pokeData[0] } isFavorite />,
+    );
+
+    const pokeName = queryByText(/Pikachu/i);
+    const pokeType = queryByText(/Electric/i);
+    const pokeWeight = queryByText(/Average weight: 6.0 kg/i);
+    const favoritePokemonImage = getByAltText(/is marked as favorite/i);
+    const imageElement = getAllByRole('img');
+    const imageURL = 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+
+    expect(pokeName).toBeInTheDocument();
+    expect(pokeType).toBeInTheDocument();
+    expect(pokeWeight).toBeInTheDocument();
+    expect(favoritePokemonImage).toBeInTheDocument();
+    expect(imageElement[0].src).toBe(imageURL);
+    expect(imageElement[0].alt).toBe('Pikachu sprite');
+  });
+
+  it('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação', () => {
+    const { queryByText } = renderWithRouter(
+      <Pokemon pokemon={ pokeData[0] } isFavorite />,
+    );
+
+    const detailsButton = queryByText(/More details/i);
+    expect(detailsButton.tagName).toBe('A');
+    expect(detailsButton).toHaveAttribute('href', '/pokemons/25'); // Source: https://github.com/testing-library/jest-dom#tohaveattribute
+  });
+
+  it('Testa se ao clicar no link de navegação é feito o redirecionamento', () => {
+    const { queryByText, history } = renderWithRouter(
+      <Pokemon pokemon={ pokeData[0] } isFavorite />,
+    );
+  });
+
+  it('Testa se a URL exibida no navegador muda para /pokemon/id com os detalhes', () => {
     expect(true).toBeTruthy();
   });
-});
 
-*/
-test('', () => {});
+  it('Testa se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { getByAltText } = renderWithRouter(
+      <Pokemon pokemon={ pokeData[0] } isFavorite />,
+    );
+    const imageURL = '/star-icon.svg';
+    const favoritePokemonImage = getByAltText(/is marked as favorite/i);
+
+    expect(favoritePokemonImage).toHaveAttribute('src', imageURL);
+  });
+});
