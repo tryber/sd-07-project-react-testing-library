cc591f52f4fc0ce1b6e4851652b08e697e7989cf
André Ferreira <andrefpires1@gmail.com>
2021-02-01 18:45:27 -0300

    Requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,80 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('App.js', () => {
-  test('renders a reading with the text `Pokédex`', () => {
-    const { getByText } = renderWithRouter(<App />);
+  it('deve renderizar o componente `Pokédex` quando a URL for `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
 
+    history.push('/');
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
+    expect(heading.textContent).toBe('Pokédex');
+  });
+
+  it(
+    'deve renderizar uma barra de navegação com os links: Home, About e FavoritePokémons',
+    () => {
+      const { getByText } = renderWithRouter(<App />);
+      const linkHome = getByText(/home/i);
+      const linkAbout = getByText(/about/i);
+      const linkFavoritePokemons = getByText(/favorite pokémon/i);
+
+      expect(linkHome).toBeInTheDocument();
+      expect(linkHome.textContent).toBe('Home');
+
+      expect(linkAbout).toBeInTheDocument();
+      expect(linkAbout.textContent).toBe('About');
+
+      expect(linkFavoritePokemons).toBeInTheDocument();
+      expect(linkFavoritePokemons.textContent).toBe('Favorite Pokémons');
+    },
+  );
+
+  it(
+    `o link 'Home' da barra de navegação deve redirecionar para a URL '/'
+    quando clicado`,
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const linkHome = getByText(/home/i);
+      fireEvent.click(linkHome);
+
+      const { pathName } = history.location;
+      expect(pathName).toBe('/');
+    },
+  );
+
+  it(
+    `o link 'About' da barra de navegação deve redirecionar para a URL '/about'
+    quando clicado`,
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const linkAbout = getByText(/about/i);
+      fireEvent.click(linkAbout);
+
+      const { pathName } = history.location;
+      expect(pathName).toBe('/about');
+    },
+  );
+
+  it(
+    `o link 'Favorite Pokémons' da barra de navegação deve redirecionar para a URL
+    '/favorite' quando clicado`,
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      fireEvent.click(getByText(/favorite pokémons/i));
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    },
+  );
+
+  it('ao entrar em uma url desconhecida deve renderizar not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/notFound');
+    const notFoundTitle = getByText(/page requested not found/i);
+    expect(notFoundTitle).toBeInTheDocument();
   });
 });
