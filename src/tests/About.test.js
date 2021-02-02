613daf8e5793ec40c98cfe7887f673767be46071
André Ferreira <andrefpires1@gmail.com>
2021-02-01 22:59:50 -0300

    Requisito 2 - Componente About | Mudando seletores para passar no lint -,-

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,17 +5,19 @@ import App from '../App';
 
 describe('About.js', () => {
   it('deve renderizar informações sobre a Pokédex', () => {
-    const { getByText, getByAltText, getAllByText } = renderWithRouter(<App />);
+    const { getByText, getByAltText } = renderWithRouter(<App />);
     const buttonAbout = getByText(/about/i);
     fireEvent.click(buttonAbout);
 
     const imgAtribute = getByAltText(/pokédex/i);
     const pokedexTitle = getByText(/about pokédex/i);
-    const aboutPokedexparagraphs = getAllByText(/(pokédex, a|pokémons by)/i);
+    const paragraphOne = getByText(/(pokémons by)/i);
+    const paragraphTwo = getByText(/(pokédex, a)/i);
 
     expect(imgAtribute.alt).toBe('Pokédex');
     expect(pokedexTitle.textContent).toBe('About Pokédex');
-    expect(aboutPokedexparagraphs.length).toBe(2);
+    expect(paragraphOne.textContent).toMatch('One can filter Pokémons');
+    expect(paragraphTwo.textContent).toMatch('This application simulates');
     expect(imgAtribute.src).toBe(
       'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
     );
