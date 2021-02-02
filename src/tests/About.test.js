5287fcc0ac99fc566a4b7507c440c8714ac42d7f
André Ferreira <andrefpires1@gmail.com>
2021-02-01 22:51:50 -0300

    Requisito 2 - Componente About | Mudando para arquivo separado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('About.js', () => {
+  it('deve renderizar informações sobre a Pokédex', () => {
+    const { getByText, getByAltText, getAllByText } = renderWithRouter(<App />);
+    const buttonAbout = getByText(/about/i);
+    fireEvent.click(buttonAbout);
+
+    const imgAtribute = getByAltText(/pokédex/i);
+    const pokedexTitle = getByText(/about pokédex/i);
+    const aboutPokedexparagraphs = getAllByText(/(pokédex, a|pokémons by)/i);
+
+    expect(imgAtribute.alt).toBe('Pokédex');
+    expect(pokedexTitle.textContent).toBe('About Pokédex');
+    expect(aboutPokedexparagraphs.length).toBe(2);
+    expect(imgAtribute.src).toBe(
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
